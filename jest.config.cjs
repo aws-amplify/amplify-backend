1c2a723cba8e94c4b00ca01d1523ed335489aaab
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-04-17 12:40:04 -0500

    chore: use node built-in testing (#6)

--- jest.config.cjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.js']
-};
--- src/hello_world.test.ts
+++ src/hello_world.test.ts
@@ -1,8 +1,55 @@
-import { test, jest, expect } from '@jest/globals';
+import { test, mock, describe } from 'node:test';
+import assert from 'node:assert';
 import { helloWorld } from './hello_world.js';
 
-test('The program properly greets the world', () => {
-  const greeter = jest.fn();
-  helloWorld(greeter);
-  expect(greeter).toHaveBeenCalledWith('Hello, world.');
+describe('Mocking functions', () => {
+  describe('parameter assertions', () => {
+    /**
+     * @see https://nodejs.org/docs/latest-v18.x/api/assert.html#new-assertcalltracker
+     */
+    test('The program properly greets the world, verified by a CallTracker', () => {
+      const tracker = new assert.CallTracker();
+      const greeter = () => null;
+      const greeterTracker = tracker.calls(greeter, 1);
+      helloWorld(greeterTracker);
+      assert.deepStrictEqual(tracker.getCalls(greeterTracker), [
+        { thisArg: undefined, arguments: ['Hello, world.'] },
+      ]);
+
+      tracker.verify();
+    });
+
+    /**
+     * @see https://nodejs.org/docs/latest-v18.x/api/test.html#mocking
+     */
+    test('The program properly greets the world, verified by a MockFunction', () => {
+      const greeter = mock.fn();
+      helloWorld(greeter);
+      console.log(greeter.mock.calls);
+      assert.deepStrictEqual(greeter.mock.calls[0].arguments, [
+        'Hello, world.',
+      ]);
+      assert.equal(greeter.mock.callCount(), 1);
+    });
+  });
+  describe('validate errors', () => {
+    test('Throws a TypeError if the greeter is null', () => {
+      const exercise = () => helloWorld(null as any);
+      assert.throws(exercise, TypeError);
+    });
+  });
+  describe('table tests', () => {
+    type TestCase = {
+      message: string;
+    };
+
+    const cases: TestCase[] = [{ message: 'Hello, world.' }];
+    for (const testCase of cases) {
+      test(`The message "${testCase.message}" is shown`, () => {
+        const mockFn = mock.fn();
+        helloWorld(mockFn);
+        assert.equal(mockFn.mock.calls[0].arguments[0], testCase.message);
+      });
+    }
+  });
 });
