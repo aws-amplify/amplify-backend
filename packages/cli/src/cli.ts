7aa9a720c4551eae6c67ca700120097d907f6b81
Edward Foyle <foyleef@amazon.com>
2023-04-18 08:22:36 -0700

    chore: add TypeDoc generation (#8)

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -2,6 +2,9 @@
 
 import { helloWorld } from '@aws-amplify/lib-synth';
 
+/**
+ * Entry point
+ */
 const main = () => {
   helloWorld(console.log);
 };
--- packages/lib-synth/src/hello_world.ts
+++ packages/lib-synth/src/hello_world.ts
@@ -1,4 +1,9 @@
 export type Greeter = (greeting: string) => void;
+
+/**
+ * Hello world function
+ * @param greeter how to greet
+ */
 export function helloWorld(greeter: Greeter) {
   greeter('Hello, world.');
 }
