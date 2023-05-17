a57db2bd03d6dd50a5fdb9d8fa267f6b944949f6
Edward Foyle <foyleef@amazon.com>
2023-05-17 14:16:15 -0700

    add tsdoc

--- packages/core-types/src/construct_shared_types.ts
+++ packages/core-types/src/construct_shared_types.ts
@@ -1,5 +1,8 @@
 import { IFunction } from 'aws-cdk-lib/aws-lambda';
 
+/**
+ * Functional interface for Constructs that can attach Lambda function handlers to a service event
+ */
 export type EventHandlerSetter<EventName> = {
   setEventHandler(eventName: EventName, handler: IFunction): void;
 };
