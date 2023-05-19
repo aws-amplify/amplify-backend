dd9a494b9afd6df549369c65e10404a64583e634
Kamil Sobol <sobkamil@amazon.com>
2023-05-19 16:35:27 -0700

    fix pr size check

--- scripts/check_pr_size.ts
+++ scripts/check_pr_size.ts
@@ -12,6 +12,9 @@ const MAX_LINES_REMOVED = 200;
  */
 const main = async () => {
   let baseRef = process.argv[2];
+  console.log(baseRef);
+  const headRef = process.argv[3];
+  console.log(headRef);
   if (baseRef === undefined) {
     console.log('No base ref specified. Defaulting to "main"');
     console.log(
@@ -26,13 +29,13 @@ const main = async () => {
     'diff',
     '--name-only',
     baseRef,
-    'HEAD',
+    headRef,
   ]);
   const diffFileList = filenameDiffOutput.toString().split('\n');
   const filteredList = diffFileList.filter((file) => !EXCLUDE.includes(file));
 
   // now run diff --shortstat on the filtered list of files
-  const shortStatArgs = ['diff', '--shortstat', baseRef, 'HEAD', '--'];
+  const shortStatArgs = ['diff', '--shortstat', baseRef, headRef, '--'];
   shortStatArgs.push(...filteredList);
 
   // shortStatOutput is a string like "3 files changed, 1 insertion(+), 3 deletions(-)"
