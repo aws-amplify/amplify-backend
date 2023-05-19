3a46ea58b8e09d653aefbb401b8a0b8ff3ea85a2
Edward Foyle <foyleef@amazon.com>
2023-05-19 13:30:14 -0700

    update ts docs

--- packages/plugin-types/src/construct_cache.ts
+++ packages/plugin-types/src/construct_cache.ts
@@ -2,7 +2,7 @@ import { ConstructInitializer } from './construct_initializer.js';
 import { Construct } from 'constructs';
 
 /**
- * Vends Constructs based on a token and an initializer function
+ * Vends Constructs based on an initializer function
  */
 export type ConstructCache = {
   getOrCompute(initializer: ConstructInitializer<Construct>): Construct;
