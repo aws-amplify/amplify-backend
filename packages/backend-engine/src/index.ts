9fe5786f04434c51650c3255ebe109f25ac2e0f8
Edward Foyle <foyleef@amazon.com>
2023-05-19 15:00:47 -0700

    update api extract

--- packages/backend-engine/src/index.ts
+++ packages/backend-engine/src/index.ts
@@ -1,2 +1,2 @@
 export { SingletonConstructCache } from './singleton_construct_cache.js';
-export { NestedStackResolver } from './nested_stack_resolver.js';
+export { NestedStackResolver, StackResolver } from './nested_stack_resolver.js';
