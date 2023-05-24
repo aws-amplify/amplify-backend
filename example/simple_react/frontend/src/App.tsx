283053711233d655ce4663c27807053ef87b273e
Kamil Sobol <sobkamil@amazon.com>
2023-05-24 13:11:01 -0700

    new naming

--- example/simple_react/frontend/src/App.tsx
+++ example/simple_react/frontend/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { schema } from '../../backend/schema';
-import awsExports from '../../backend/aws-exports';
+import amplifyConfiguration from '../../backend/amplifyconfiguration';
 import { inferModelType } from '@aws-amplify/type-beast';
 
-schema.client.configure(awsExports);
+schema.client.configure(amplifyConfiguration);
 
 type Todo = inferModelType<typeof schema.client.models.Todo>;
 
