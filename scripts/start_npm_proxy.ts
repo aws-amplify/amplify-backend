e96d27bd692dc3fd9a01d0e2d492c705fb892bcb
Edward Foyle <foyleef@amazon.com>
2023-05-26 17:52:09 -0700

    chore: add npm proxy start and stop commands

---
+++ scripts/start_npm_proxy.ts
@@ -0,0 +1,51 @@
+import { execa, execaCommand } from 'execa';
+import * as fs from 'fs-extra';
+
+const EXPECTED_URL = 'http://localhost:4873';
+const LOG_FILE = 'verdaccio-logs.txt';
+const STARTUP_TIMEOUT_MS = 1000;
+
+/**
+ * Starts [Verdaccio](https://verdaccio.org/) in a background process.
+ * This script will exit while the background server continues to run.
+ * To stop the Verdaccio server run `npm run npm-proxy:stop`
+ *
+ * Also points the local npm config to point to the proxy server.
+ */
+const main = async () => {
+  if (await fs.exists(LOG_FILE)) {
+    await fs.unlink(LOG_FILE);
+  }
+  // start the server in a detached process
+  await execaCommand(`npx verdaccio -c verdaccio.config.yaml &>${LOG_FILE} &`, {
+    shell: 'bash',
+  });
+
+  // give the server a chance to start up
+  await new Promise((resolve) => setTimeout(resolve, STARTUP_TIMEOUT_MS));
+
+  const npmProxyLogs = await fs.readFile(LOG_FILE, 'utf-8');
+
+  if (npmProxyLogs.includes('EADDRINUSE')) {
+    throw new Error(
+      'Failed to start npm proxy. Port is already in use. Do you ned to run `npm run npm-proxy:stop` first?'
+    );
+  }
+
+  // when the server is ready a line like "http address - http://localhost:4873/ - verdaccio/5.24.1" is printed
+  if (!npmProxyLogs.includes('http address')) {
+    throw new Error(
+      `Failed to start npm proxy within the timeout. Check the logs in ${LOG_FILE}`
+    );
+  }
+
+  console.log(`Local npm proxy running at ${EXPECTED_URL}.`);
+
+  execa('npm', ['config', 'set', 'registry', EXPECTED_URL]);
+  console.log(`Set npm registry to ${EXPECTED_URL}`);
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
---
+++ scripts/stop_npm_proxy.ts
@@ -0,0 +1,24 @@
+import { execa, execaCommand } from 'execa';
+
+const VERDACCIO_PORT = 4873;
+const NPM_REGISTRY = 'https://registry.npmjs.org/';
+
+/**
+ * Kills the process that is listening on VERDACCIO_PORT
+ * Resets npm registry config back to the npm registry
+ */
+const main = async () => {
+  // returns the process id of the process listening on the specified port
+  const lsofResult = await execaCommand(
+    `lsof -n -t -iTCP:${VERDACCIO_PORT} -sTCP:LISTEN`
+  );
+  const pid = Number.parseInt(lsofResult.stdout.toString());
+  process.kill(pid);
+
+  await execa('npm', ['config', 'set', 'registry', NPM_REGISTRY]);
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
