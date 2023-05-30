214b99bd0be9eb78d0981aec70f9bdf1d4601488
Edward Foyle <foyleef@amazon.com>
2023-05-30 10:58:42 -0700

    local publish scripts (#22)

---
+++ scripts/publish_snapshot.ts
@@ -0,0 +1,26 @@
+import { execa } from 'execa';
+
+const main = async () => {
+  const tagName = process.argv[2];
+  if (tagName === undefined) {
+    throw new Error(
+      `Specify a tag name for the snapshot publish as the first and only argument`
+    );
+  }
+  await execa('changeset', ['version', '--snapshot', tagName], {
+    stdio: 'inherit',
+  });
+  // To change the registry that changeset publishes to, set the `npm_config_registry` environment variable
+  await execa(
+    'changeset',
+    ['publish', '--snapshot', '--no-git-tag', '--tag', tagName],
+    {
+      stdio: 'inherit',
+    }
+  );
+};
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
