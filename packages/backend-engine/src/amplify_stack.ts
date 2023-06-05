444557c9856d27bffdf77cadbbcea3f81c4cfdab
Edward Foyle <foyleef@amazon.com>
2023-06-05 15:15:21 -0700

    simplify logic

--- packages/backend-engine/src/amplify_stack.ts
+++ packages/backend-engine/src/amplify_stack.ts
@@ -10,18 +10,13 @@ export class AmplifyStack extends Stack {
   getLogicalId(element: CfnElement): string {
     // Nested stack logical IDs have a redundant structure of <name>NestedStack<name>NestedStackResource<hash>
     // This rewrites the nested stack logical ID to <name><hash>
-    if (element.node.id.includes('NestedStackResource')) {
-      const defaultId = super.getLogicalId(element);
-      const match =
-        /(?<name>.*)NestedStack.+NestedStackResource(?<hash>.*)/.exec(
-          defaultId
-        );
-      if (Object.keys(match?.groups || {}).length !== 2) {
-        // if something goes wrong with this regex, bail out and return the original logical id
-        return defaultId;
-      }
+    const defaultId = super.getLogicalId(element);
+    const match = /(?<name>.*)NestedStack.+NestedStackResource(?<hash>.*)/.exec(
+      defaultId
+    );
+    if (Object.keys(match?.groups || {}).length === 2) {
       return `${match?.groups?.name}${match?.groups?.hash}`;
     }
-    return super.getLogicalId(element);
+    return defaultId;
   }
 }
