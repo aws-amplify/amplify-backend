314d706ee1b23e7f137b52d5dc013635bd525d68
Edward Foyle <foyleef@amazon.com>
2023-06-09 12:13:28 -0700

    storage sets bucketName in output

--- packages/storage-construct/src/construct.test.ts
+++ packages/storage-construct/src/construct.test.ts
@@ -1,7 +1,10 @@
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import { AmplifyStorage } from './construct.js';
 import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
+import { OutputStorageStrategy } from '@aws-amplify/plugin-types';
+import assert from 'node:assert';
+import packageJson from '#package.json';
 
 describe('AmplifyStorage', () => {
   it('creates a bucket', () => {
@@ -20,6 +23,26 @@ describe('AmplifyStorage', () => {
     template.resourceCountIs('AWS::S3::Bucket', 1);
     template.hasResourceProperties('AWS::S3::Bucket', {
       VersioningConfiguration: { Status: 'Enabled' },
+    });
+  });
+  describe('storeOutput', () => {
+    it('stores output using the provided strategy', () => {
+      const app = new App();
+      const stack = new Stack(app);
+      const storageConstruct = new AmplifyStorage(stack, 'test', {});
+
+      const storeOutputMock = mock.fn();
+      const storageStrategy: OutputStorageStrategy = {
+        storeOutput: storeOutputMock,
+      };
+      storageConstruct.storeOutput(storageStrategy);
+
+      const storeOutputArgs = storeOutputMock.mock.calls[0].arguments;
+      assert.equal(storeOutputArgs.length, 3);
+      assert.equal(storeOutputArgs[0], packageJson.name);
+      assert.equal(storeOutputArgs[1], packageJson.version);
+      assert.equal(Object.keys(storeOutputArgs[2]).length, 1);
+      assert.equal(Object.keys(storeOutputArgs[2])[0], 'bucketName');
     });
   });
 });
--- packages/storage-factory/src/factory.test.ts
+++ packages/storage-factory/src/factory.test.ts
@@ -1,12 +1,14 @@
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import { AmplifyStorageFactory } from './factory.js';
 import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import {
   NestedStackResolver,
   SingletonConstructCache,
+  StackMetadataOutputStorageStrategy,
 } from '@aws-amplify/backend-engine';
 import assert from 'node:assert';
+import { OutputStorageStrategy } from '@aws-amplify/plugin-types';
 
 describe('AmplifyStorageFactory', () => {
   it('returns singleton instance', () => {
@@ -19,8 +21,16 @@ describe('AmplifyStorageFactory', () => {
       new NestedStackResolver(stack)
     );
 
-    const instance1 = storageFactory.getInstance(constructCache);
-    const instance2 = storageFactory.getInstance(constructCache);
+    const outputStorageStrategy = new StackMetadataOutputStorageStrategy(stack);
+
+    const instance1 = storageFactory.getInstance(
+      constructCache,
+      outputStorageStrategy
+    );
+    const instance2 = storageFactory.getInstance(
+      constructCache,
+      outputStorageStrategy
+    );
 
     assert.strictEqual(instance1, instance2);
   });
@@ -35,10 +45,36 @@ describe('AmplifyStorageFactory', () => {
       new NestedStackResolver(stack)
     );
 
-    const storageConstruct = storageFactory.getInstance(backendBuildState);
+    const outputStorageStrategy = new StackMetadataOutputStorageStrategy(stack);
+
+    const storageConstruct = storageFactory.getInstance(
+      backendBuildState,
+      outputStorageStrategy
+    );
 
     const template = Template.fromStack(Stack.of(storageConstruct));
 
     template.resourceCountIs('AWS::S3::Bucket', 1);
+  });
+
+  it('sets output in storage strategy', () => {
+    const storageFactory = new AmplifyStorageFactory({});
+
+    const app = new App();
+    const stack = new Stack(app);
+
+    const backendBuildState = new SingletonConstructCache(
+      new NestedStackResolver(stack)
+    );
+
+    const storeOutputMock = mock.fn();
+
+    const outputStorageStrategy: OutputStorageStrategy = {
+      storeOutput: storeOutputMock,
+    };
+
+    storageFactory.getInstance(backendBuildState, outputStorageStrategy);
+
+    assert.strictEqual(storeOutputMock.mock.callCount(), 1);
   });
 });
