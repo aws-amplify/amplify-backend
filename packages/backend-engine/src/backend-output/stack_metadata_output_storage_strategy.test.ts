f7a3b725e14f85c74e752ab4c06bf09f64619ead
Edward Foyle <foyleef@amazon.com>
2023-06-13 16:27:40 -0700

    fix tests

--- packages/backend-engine/src/backend-output/stack_metadata_output_storage_strategy.test.ts
+++ packages/backend-engine/src/backend-output/stack_metadata_output_storage_strategy.test.ts
@@ -2,11 +2,7 @@ import { describe, it } from 'node:test';
 import { StackMetadataOutputStorageStrategy } from './stack_metadata_output_storage_strategy.js';
 import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { AmplifyStack } from '../amplify_stack.js';
-import {
-  ProjectEnvironmentIdentifier,
-  stackMetadataSchema,
-} from '@aws-amplify/primitives';
+import { stackMetadataSchema } from './backend_output.js';
 
 describe('StackMetadataOutputStorageStrategy', () => {
   describe('storeOutput', () => {
@@ -41,22 +37,6 @@ describe('StackMetadataOutputStorageStrategy', () => {
       const template = Template.fromStack(stack);
       // successfully parsing the metadata means it validated against the schema
       stackMetadataSchema.parse(template.toJSON().Metadata);
-    });
-
-    it('sets SSM parameter of stack name if initialized with an AmplifyStack', () => {
-      const app = new App();
-      const stack = new AmplifyStack(app, 'test-stack', {
-        projectEnvironmentIdentifier: new ProjectEnvironmentIdentifier(
-          'testProjName',
-          'testEnvName'
-        ),
-      });
-      new StackMetadataOutputStorageStrategy(stack);
-      const template = Template.fromStack(stack);
-      template.hasResourceProperties('AWS::SSM::Parameter', {
-        Name: '/amplify/testProjName/testEnvName/outputStackName',
-        Value: 'testProjName-testEnvName',
-      });
     });
   });
 });
--- packages/backend-engine/src/generate-client-config/generate_client_config.test.ts
+++ packages/backend-engine/src/generate-client-config/generate_client_config.test.ts
@@ -1,14 +1,13 @@
 import { describe, it } from 'node:test';
 import { fromIni } from '@aws-sdk/credential-providers';
 import { generateClientConfig } from './generate_client_config.js';
-import { ProjectEnvironmentIdentifier } from '@aws-amplify/primitives';
 
 describe('generateClientConfig', () => {
   // TODO remove this
   it.skip('gets output from stack', async () => {
-    await generateClientConfig(
-      fromIni(),
-      new ProjectEnvironmentIdentifier('samsaraDemo', 'foyleefSandbox')
-    );
+    await generateClientConfig(fromIni(), {
+      projectName: 'samsaraDemo',
+      environmentName: 'foyleefSandbox',
+    });
   });
 });
