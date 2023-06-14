56f8ed25728e12aa3fd484ded992819a726f6ae9
Edward Foyle <foyleef@amazon.com>
2023-06-13 17:07:19 -0700

    add some comments

--- packages/backend-engine/src/backend-output/backend_output_schemas.ts
+++ packages/backend-engine/src/backend-output/backend_output_schemas.ts
@@ -1,12 +1,25 @@
 import { z } from 'zod';
 
+/**
+ * Schema for a single block of output written by a construct
+ */
 export const outputEntrySchema = z.object({
   constructVersion: z.string(),
   stackOutputs: z.array(z.string()),
 });
 
+/**
+ * Inferred type from outputEntrySchema
+ */
 export type OutputEntry = z.infer<typeof outputEntrySchema>;
 
+/**
+ * Schema for the record of all output written by all constructs
+ * The keys of the record are the package names of the constructs
+ */
 export const backendOutputSchema = z.record(outputEntrySchema);
 
+/**
+ * Inferred type from backendOutputSchema
+ */
 export type BackendOutput = z.infer<typeof backendOutputSchema>;
