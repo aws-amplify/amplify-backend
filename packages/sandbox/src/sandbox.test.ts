dd8d0e7da888c67ac4d7f6ce4d0f6519d3bb216f
Amplifiyer <51211245+Amplifiyer@users.noreply.github.com>
2023-06-28 16:20:22 -0700

    feat: sandbox using parcelWatcher + CDK deploy (#64)
    * feat: sandbox using parcelWatcher + CDK deploy
    
    * update lint
    
    * PR Updates
    
    * PR Updates
    
    * PR Updates

---
+++ packages/sandbox/src/sandbox.test.ts
@@ -0,0 +1,102 @@
+import { afterEach, beforeEach, describe, it, mock } from 'node:test';
+import watcher from '@parcel/watcher';
+import { Sandbox } from './sandbox.js';
+import assert from 'node:assert';
+import util from 'node:util';
+
+// exec mocks
+const invokeCDKExecMock = mock.fn();
+invokeCDKExecMock.mock.mockImplementation(() => {
+  return { stdout: '', stderr: '' };
+});
+mock.method(util, 'promisify', () => {
+  return invokeCDKExecMock;
+});
+
+// Watcher mocks
+const unsubscribeMockFn = mock.fn();
+const subscribeMock = mock.method(watcher, 'subscribe', async () => {
+  return { unsubscribe: unsubscribeMockFn };
+});
+let fileChangeEventActualFn: watcher.SubscribeCallback;
+
+describe('Sandbox', () => {
+  // class under test
+  let sandboxInstance: Sandbox;
+
+  /**
+   * For each test we start the sandbox and hence file watcher and get hold of
+   * file change event function which tests can simulate by calling as desired.
+   */
+  beforeEach(async () => {
+    invokeCDKExecMock.mock.resetCalls();
+    sandboxInstance = new Sandbox();
+    await sandboxInstance.start();
+    if (
+      subscribeMock.mock.calls[0].arguments[1] &&
+      typeof subscribeMock.mock.calls[0].arguments[1] === 'function'
+    ) {
+      fileChangeEventActualFn = subscribeMock.mock.calls[0].arguments[1];
+    }
+  });
+
+  afterEach(async () => {
+    await sandboxInstance.stop();
+  });
+
+  it('calls CDK once when a file change is present', async () => {
+    await fileChangeEventActualFn(null, [
+      { type: 'update', path: 'foo/test1.ts' },
+    ]);
+    assert.strictEqual(invokeCDKExecMock.mock.callCount(), 1);
+  });
+
+  it('calls CDK once when multiple file changes are present', async () => {
+    await fileChangeEventActualFn(null, [
+      { type: 'update', path: 'foo/test2.ts' },
+      { type: 'create', path: 'foo/test3.ts' },
+    ]);
+    assert.strictEqual(invokeCDKExecMock.mock.callCount(), 1);
+  });
+
+  it('calls CDK once when multiple file changes are within few milliseconds (debounce)', async () => {
+    // Not awaiting for this file event to be processed and submitting another one right away
+    fileChangeEventActualFn(null, [{ type: 'update', path: 'foo/test4.ts' }]);
+    await fileChangeEventActualFn(null, [
+      { type: 'update', path: 'foo/test4.ts' },
+    ]);
+    assert.strictEqual(invokeCDKExecMock.mock.callCount(), 1);
+  });
+
+  it('waits for file changes after completing a deployment and deploys again', async () => {
+    await fileChangeEventActualFn(null, [
+      { type: 'update', path: 'foo/test5.ts' },
+    ]);
+    await fileChangeEventActualFn(null, [
+      { type: 'update', path: 'foo/test6.ts' },
+    ]);
+    assert.strictEqual(invokeCDKExecMock.mock.callCount(), 2);
+  });
+
+  it('queues deployment if a file change is detected during an ongoing', async () => {
+    // Mimic cdk taking 200 ms.
+    invokeCDKExecMock.mock.mockImplementationOnce(async () => {
+      await new Promise((res) => setTimeout(res, 200));
+      return { stdout: '', stderr: '' };
+    });
+
+    // Not awaiting so we can push another file change while deployment is ongoing
+    fileChangeEventActualFn(null, [{ type: 'update', path: 'foo/test7.ts' }]);
+
+    // Get over debounce so that the next deployment is considered valid
+    await new Promise((res) => setTimeout(res, 100));
+
+    // second file change while the previous one is 'ongoing'
+    fileChangeEventActualFn(null, [{ type: 'update', path: 'foo/test8.ts' }]);
+
+    // Wait sufficient time for both deployments to have finished before we count number of cdk calls.
+    await new Promise((res) => setTimeout(res, 500));
+
+    assert.strictEqual(invokeCDKExecMock.mock.callCount(), 2);
+  });
+});
---
+++ packages/sandbox/src/sandbox.ts
@@ -0,0 +1,127 @@
+import debounce from 'debounce-promise';
+import parcelWatcher, { subscribe } from '@parcel/watcher';
+import child_process from 'node:child_process';
+import util from 'node:util';
+
+/**
+ * Main class for Sandbox. Runs a file watcher and deploys using cdk
+ */
+export class Sandbox {
+  private watcherSubscription: Awaited<ReturnType<typeof subscribe>>;
+  /**
+   * Creates a watcher process for this instance
+   */
+  constructor() {
+    process.once('SIGINT', this.stop);
+    process.once('SIGTERM', this.stop);
+  }
+
+  /**
+   * Starts the sandbox
+   */
+  public async start() {
+    console.debug(`[Sandbox] Initializing...`);
+    // Since 'cdk deploy' is a relatively slow operation for a 'watch' process,
+    // introduce a concurrency latch that tracks the state.
+    // This way, if file change events arrive when a 'cdk deploy' is still executing,
+    // we will batch them, and trigger another 'cdk deploy' after the current one finishes,
+    // making sure 'cdk deploy's  always execute one at a time.
+    // Here's a diagram showing the state transitions:
+
+    // --------    file changed     --------------    file changed     --------------  file changed
+    // |      | ------------------> |            | ------------------> |            | --------------|
+    // | open |                     | deploying  |                     |   queued   |               |
+    // |      | <------------------ |            | <------------------ |            | <-------------|
+    // --------  'cdk deploy' done  --------------  'cdk deploy' done  --------------
+
+    let latch: 'open' | 'deploying' | 'queued' = 'open';
+
+    const deployAndWatch = debounce(async () => {
+      latch = 'deploying';
+      await this.invokeCDKDeployWithDebounce();
+
+      // If latch is still 'deploying' after the 'await', that's fine,
+      // but if it's 'queued', that means we need to deploy again
+      while ((latch as 'deploying' | 'queued') === 'queued') {
+        // TypeScript doesn't realize latch can change between 'awaits' ¯\_(ツ)_/¯,
+        // and thinks the above 'while' condition is always 'false' without the cast
+        latch = 'deploying';
+        console.log(
+          "[Sandbox] Detected file changes while previous deployment was in progress. Invoking 'sandbox' again"
+        );
+        await this.invokeCDKDeployWithDebounce();
+      }
+      latch = 'open';
+      this.emitWatching();
+    });
+
+    this.watcherSubscription = await parcelWatcher.subscribe(
+      process.cwd(),
+      async (_, events) => {
+        // it doesn't matter which file changed, we are just using events to log the filenames. We deploy full state.
+        await Promise.all(
+          events.map(({ type: eventName, path }) => {
+            console.log(
+              `[Sandbox] Triggered due to a file ${eventName} event: ${path}`
+            );
+          })
+        );
+        if (latch === 'open') {
+          await deployAndWatch();
+        } else {
+          // this means latch is either 'deploying' or 'queued'
+          latch = 'queued';
+          console.log(
+            '[Sandbox] Previous deployment is still in progress. ' +
+              'Will queue for another deployment after this one finishes'
+          );
+        }
+      },
+      { ignore: ['cdk.out'] }
+    );
+
+    this.emitWatching();
+  }
+
+  /**
+   * Stops watching for file changes
+   */
+  public async stop() {
+    console.debug(`[Sandbox] Shutting down`);
+    await this.watcherSubscription.unsubscribe();
+  }
+
+  /**
+   * Function that deploys backend resources using CDK.
+   * Debounce is added in case multiple duplicate events are received.
+   */
+  private invokeCDKDeployWithDebounce = debounce(async (): Promise<void> => {
+    const execPromisified = util.promisify(child_process.execFile);
+
+    console.debug(`[Sandbox] Executing cdk deploy`);
+    const { stdout, stderr } = await execPromisified(
+      'npx',
+      [
+        'cdk',
+        'deploy',
+        '--app',
+        "'npx tsx index.ts'",
+        '--hotswap-fallback',
+        '--method=direct',
+      ],
+      { shell: true }
+    );
+
+    if (stderr) {
+      console.error(stderr);
+    }
+    console.log(stdout);
+  }, 100);
+
+  /**
+   * Just a shorthand console log to indicate whenever watcher is going idle
+   */
+  private emitWatching() {
+    console.log(`[Sandbox] Watching for file changes...`);
+  }
+}
