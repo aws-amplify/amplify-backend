2b59b5e72f577bf8d1fc6a99ea1b7b20910fa859
Praveen Gupta <pravgupt@amazon.com>
2023-06-29 01:08:15 +0200

    PR Updates

--- packages/sandbox/src/sandbox.ts
+++ packages/sandbox/src/sandbox.ts
@@ -7,107 +7,88 @@ import util from 'node:util';
  * Main class for Sandbox. Runs a file watcher and deploys using cdk
  */
 export class Sandbox {
-  private readonly watchProcess: SandboxWatchProcess;
-
+  private watcherSubscription: Awaited<ReturnType<typeof subscribe>>;
   /**
    * Creates a watcher process for this instance
    */
   constructor() {
-    this.watchProcess = this.createWatchProcess();
-    process.once('SIGINT', this.watchProcess.stop);
-    process.once('SIGTERM', this.watchProcess.stop);
+    process.once('SIGINT', this.stop);
+    process.once('SIGTERM', this.stop);
   }
 
   /**
    * Starts the sandbox
    */
   public async start() {
-    await this.watchProcess.start();
+    console.debug(`[Sandbox] Initializing...`);
+    // Since 'cdk deploy' is a relatively slow operation for a 'watch' process,
+    // introduce a concurrency latch that tracks the state.
+    // This way, if file change events arrive when a 'cdk deploy' is still executing,
+    // we will batch them, and trigger another 'cdk deploy' after the current one finishes,
+    // making sure 'cdk deploy's  always execute one at a time.
+    // Here's a diagram showing the state transitions:
+
+    // --------    file changed     --------------    file changed     --------------  file changed
+    // |      | ------------------> |            | ------------------> |            | --------------|
+    // | open |                     | deploying  |                     |   queued   |               |
+    // |      | <------------------ |            | <------------------ |            | <-------------|
+    // --------  'cdk deploy' done  --------------  'cdk deploy' done  --------------
+
+    let latch: 'open' | 'deploying' | 'queued' = 'open';
+
+    const deployAndWatch = debounce(async () => {
+      latch = 'deploying';
+      await this.invokeCDKDeployWithDebounce();
+
+      // If latch is still 'deploying' after the 'await', that's fine,
+      // but if it's 'queued', that means we need to deploy again
+      while ((latch as 'deploying' | 'queued') === 'queued') {
+        // TypeScript doesn't realize latch can change between 'awaits' ¯\_(ツ)_/¯,
+        // and thinks the above 'while' condition is always 'false' without the cast
+        latch = 'deploying';
+        console.log(
+          "[Sandbox] Detected file changes while previous deployment was in progress. Invoking 'sandbox' again"
+        );
+        await this.invokeCDKDeployWithDebounce();
+      }
+      latch = 'open';
+      this.emitWatching();
+    });
+
+    this.watcherSubscription = await parcelWatcher.subscribe(
+      process.cwd(),
+      async (_, events) => {
+        // it doesn't matter which file changed, we are just using events to log the filenames. We deploy full state.
+        await Promise.all(
+          events.map(({ type: eventName, path }) => {
+            console.log(
+              `[Sandbox] Triggered due to a file ${eventName} event: ${path}`
+            );
+          })
+        );
+        if (latch === 'open') {
+          await deployAndWatch();
+        } else {
+          // this means latch is either 'deploying' or 'queued'
+          latch = 'queued';
+          console.log(
+            '[Sandbox] Previous deployment is still in progress. ' +
+              'Will queue for another deployment after this one finishes'
+          );
+        }
+      },
+      { ignore: ['cdk.out'] }
+    );
+
+    this.emitWatching();
   }
 
   /**
    * Stops watching for file changes
    */
   public async stop() {
-    await this.watchProcess.stop();
-  }
-
-  /**
-   * Creates a file watcher and subscribes file change events to invoke cdk deploy
-   */
-  private createWatchProcess(): SandboxWatchProcess {
-    console.debug(`[Sandbox] Initializing...`);
-
-    let watcherSubscription: Awaited<ReturnType<typeof subscribe>>;
-    const watchProcess = {
-      start: async () => {
-        // Since 'cdk deploy' is a relatively slow operation for a 'watch' process,
-        // introduce a concurrency latch that tracks the state.
-        // This way, if file change events arrive when a 'cdk deploy' is still executing,
-        // we will batch them, and trigger another 'cdk deploy' after the current one finishes,
-        // making sure 'cdk deploy's  always execute one at a time.
-        // Here's a diagram showing the state transitions:
-
-        // --------    file changed     --------------    file changed     --------------  file changed
-        // |      | ------------------> |            | ------------------> |            | --------------|
-        // | open |                     | deploying  |                     |   queued   |               |
-        // |      | <------------------ |            | <------------------ |            | <-------------|
-        // --------  'cdk deploy' done  --------------  'cdk deploy' done  --------------
-
-        let latch: 'open' | 'deploying' | 'queued' = 'open';
-
-        const deployAndWatch = debounce(async () => {
-          latch = 'deploying';
-          await this.invokeCDKDeployWithDebounce();
-
-          // If latch is still 'deploying' after the 'await', that's fine,
-          // but if it's 'queued', that means we need to deploy again
-          while ((latch as 'deploying' | 'queued') === 'queued') {
-            // TypeScript doesn't realize latch can change between 'awaits' ¯\_(ツ)_/¯,
-            // and thinks the above 'while' condition is always 'false' without the cast
-            latch = 'deploying';
-            console.log(
-              "[Sandbox] Detected file changes while previous deployment was in progress. Invoking 'sandbox' again"
-            );
-            await this.invokeCDKDeployWithDebounce();
-          }
-          latch = 'open';
-          this.emitWatching();
-        });
-
-        watcherSubscription = await parcelWatcher.subscribe(
-          process.cwd(),
-          async (_, events) => {
-            // it doesn't matter which file changed, we are just using events to log the filenames. We deploy full state.
-            await Promise.all(
-              events.map(({ type: eventName, path }) => {
-                console.log(
-                  `[Sandbox] Triggered due to a file ${eventName} event: ${path}`
-                );
-              })
-            );
-            if (latch === 'open') {
-              await deployAndWatch();
-            } else {
-              // this means latch is either 'deploying' or 'queued'
-              latch = 'queued';
-              console.log(
-                '[Sandbox] Previous deployment is still in progress. ' +
-                  'Will queue for another deployment after this one finishes'
-              );
-            }
-          },
-          { ignore: ['cdk.out'] }
-        );
-
-        this.emitWatching();
-      },
-      stop: async () => {
-        console.debug(`[Sandbox] Shutting down`);
-        await watcherSubscription.unsubscribe();
-      },
-    };
-    return watchProcess;
+    console.debug(`[Sandbox] Shutting down`);
+    await this.watcherSubscription.unsubscribe();
   }
 
   /**
@@ -119,8 +100,9 @@ export class Sandbox {
 
     console.debug(`[Sandbox] Executing cdk deploy`);
     const { stdout, stderr } = await execPromisified(
-      'npx cdk',
+      'npx',
       [
+        'cdk',
         'deploy',
         '--app',
         "'npx tsx index.ts'",
@@ -140,11 +122,6 @@ export class Sandbox {
    * Just a shorthand console log to indicate whenever watcher is going idle
    */
   private emitWatching() {
-    console.log(`[Sandbox] Watching for changes...`);
+    console.log(`[Sandbox] Watching for file changes...`);
   }
 }
-
-type SandboxWatchProcess = {
-  start: () => Promise<void>;
-  stop: () => Promise<void>;
-};
