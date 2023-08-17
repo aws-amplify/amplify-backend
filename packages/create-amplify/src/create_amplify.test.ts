9a27d5e25fe51c31f2393fac3ed33864ac31d697
Edward Foyle <foyleef@amazon.com>
2023-08-16 18:46:19 -0700

    try this

--- packages/create-amplify/src/create_amplify.test.ts
+++ packages/create-amplify/src/create_amplify.test.ts
@@ -12,87 +12,78 @@ import assert from 'assert';
  *
  * TODO: we may want to pull this test into our e2e suite once we have that setup
  */
-describe(
-  'create-amplify script',
-  {
-    timeout: 1000 * 60 * 5, // 5 minutes
-    concurrency: 1,
-  },
-  () => {
-    before(async () => {
-      // start a local npm proxy and publish the current codebase to the proxy
-      await execa('npm', ['run', 'clean:npm-proxy'], { stdio: 'inherit' });
-      await execa('npm', ['run', 'vend'], { stdio: 'inherit' });
+describe('create-amplify script', async () => {
+  before(async () => {
+    // start a local npm proxy and publish the current codebase to the proxy
+    await execa('npm', ['run', 'clean:npm-proxy'], { stdio: 'inherit' });
+    await execa('npm', ['run', 'vend'], { stdio: 'inherit' });
 
-      // nuke the npx cache to ensure we are installing packages from the npm proxy
-      const { stdout } = await execa('npm', ['config', 'get', 'cache']);
-      const npxCacheLocation = path.join(stdout.toString().trim(), '_npx');
+    // nuke the npx cache to ensure we are installing packages from the npm proxy
+    const { stdout } = await execa('npm', ['config', 'get', 'cache']);
+    const npxCacheLocation = path.join(stdout.toString().trim(), '_npx');
 
-      if (fs.existsSync(npxCacheLocation)) {
-        await fsp.rm(npxCacheLocation, { recursive: true });
-      }
+    if (fs.existsSync(npxCacheLocation)) {
+      await fsp.rm(npxCacheLocation, { recursive: true });
+    }
+  });
+
+  after(async () => {
+    // stop the npm proxy
+    await execa('npm', ['run', 'stop:npm-proxy'], { stdio: 'inherit' });
+  });
+
+  let tempDir: string;
+  beforeEach(async () => {
+    tempDir = await fsp.mkdtemp(path.join(os.tmpdir(), 'test-create-amplify'));
+  });
+
+  afterEach(async () => {
+    await fsp.rm(tempDir, { recursive: true });
+  });
+
+  it('installs expected packages and scaffolds expected files', async () => {
+    // the --yes flag here is to bypass a npm prompt to install the create-amplify package, it's not a prompt we control
+    await execa('npm', ['create', 'amplify', '--yes'], {
+      cwd: tempDir,
+      stdio: 'inherit',
     });
+    const packageJsonPath = path.resolve(tempDir, 'package.json');
+    const packageJsonObject = JSON.parse(
+      await fsp.readFile(packageJsonPath, 'utf-8')
+    );
+    assert.deepStrictEqual(
+      Object.keys(packageJsonObject.devDependencies).sort(),
+      [
+        '@aws-amplify/backend',
+        '@aws-amplify/backend-auth',
+        '@aws-amplify/backend-graphql',
+        '@aws-amplify/cli',
+        'aws-amplify',
+      ]
+    );
 
-    after(async () => {
-      // stop the npm proxy
-      await execa('npm', ['run', 'stop:npm-proxy'], { stdio: 'inherit' });
+    const dirContent = await fsp.readdir(path.join(tempDir, 'amplify'));
+    assert.deepStrictEqual(dirContent.sort(), [
+      'auth.ts',
+      'backend.ts',
+      'data.ts',
+    ]);
+  });
+
+  it('fails fast if amplify directory is not empty', async () => {
+    const amplifyDirPath = path.join(tempDir, 'amplify');
+    await fsp.mkdir(amplifyDirPath, { recursive: true });
+    await fsp.writeFile(
+      path.join(amplifyDirPath, 'placeholder.txt'),
+      'some content'
+    );
+
+    // the --yes flag here is to bypass a npm prompt to install the create-amplify package, it's not a prompt we control
+    const result = await execa('npm', ['create', 'amplify', '--yes'], {
+      cwd: tempDir,
+      stdio: 'inherit',
+      reject: false,
     });
-
-    let tempDir: string;
-    beforeEach(async () => {
-      tempDir = await fsp.mkdtemp(
-        path.join(os.tmpdir(), 'test-create-amplify')
-      );
-    });
-
-    afterEach(async () => {
-      await fsp.rm(tempDir, { recursive: true });
-    });
-
-    it('installs expected packages and scaffolds expected files', async () => {
-      // the --yes flag here is to bypass a npm prompt to install the create-amplify package, it's not a prompt we control
-      await execa('npm', ['create', 'amplify', '--yes'], {
-        cwd: tempDir,
-        stdio: 'inherit',
-      });
-      const packageJsonPath = path.resolve(tempDir, 'package.json');
-      const packageJsonObject = JSON.parse(
-        await fsp.readFile(packageJsonPath, 'utf-8')
-      );
-      assert.deepStrictEqual(
-        Object.keys(packageJsonObject.devDependencies).sort(),
-        [
-          '@aws-amplify/backend',
-          '@aws-amplify/backend-auth',
-          '@aws-amplify/backend-graphql',
-          '@aws-amplify/cli',
-          'aws-amplify',
-        ]
-      );
-
-      const dirContent = await fsp.readdir(path.join(tempDir, 'amplify'));
-      assert.deepStrictEqual(dirContent.sort(), [
-        'auth.ts',
-        'backend.ts',
-        'data.ts',
-      ]);
-    });
-
-    it('fails fast if amplify directory is not empty', async () => {
-      const amplifyDirPath = path.join(tempDir, 'amplify');
-      await fsp.mkdir(amplifyDirPath, { recursive: true });
-      await fsp.writeFile(
-        path.join(amplifyDirPath, 'placeholder.txt'),
-        'some content'
-      );
-
-      // the --yes flag here is to bypass a npm prompt to install the create-amplify package, it's not a prompt we control
-      const result = await execa('npm', ['create', 'amplify', '--yes'], {
-        cwd: tempDir,
-        stdio: 'inherit',
-        reject: false,
-      });
-      assert.equal(result.exitCode, 1);
-    });
-  }
-);
+    assert.equal(result.exitCode, 1);
+  });
+});
