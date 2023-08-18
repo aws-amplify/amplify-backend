7296e9d9ab9fa63fc4b4620933026377e7f5c850
Edward Foyle <foyleef@amazon.com>
2023-08-18 16:15:02 -0700

    changeset to bump patch versions (#140)

--- scripts/publish_snapshot.ts
+++ scripts/publish_snapshot.ts
@@ -6,6 +6,17 @@ if (tagName === undefined) {
     `Specify a tag name for the snapshot publish as the first and only argument`
   );
 }
+
+const result = await execa('git', ['diff', '--quiet'], { reject: false });
+if (result.exitCode !== 0) {
+  // non-zero exit code indicates working tree is dirty
+  throw new Error(
+    `Detected a dirty working tree. Commit or stash changes before publishing a snapshot`
+  );
+}
+
+// this command will write staged changesets into changelog files and update version
+// this is reverted at the end of this script
 await execa('changeset', ['version', '--snapshot', tagName], {
   stdio: 'inherit',
 });
@@ -17,3 +28,6 @@ await execa(
     stdio: 'inherit',
   }
 );
+
+// this is safe because the script ensures the working tree is clean before starting
+await execa('git', ['reset', '--hard']);
