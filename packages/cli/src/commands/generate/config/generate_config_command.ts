c97be05ea32e2eace592daf54646e8729dfc13c6
Spencer Stolworthy <sstol@amazon.com>
2023-08-30 14:21:48 -0700

    Extract write config (#179)
    * fix: add generateClientConfig method to adapter class

--- packages/cli/src/commands/generate/config/generate_config_command.ts
+++ packages/cli/src/commands/generate/config/generate_config_command.ts
@@ -60,7 +60,6 @@ export class GenerateConfigCommand
     } else {
       targetPath = path.resolve(process.cwd(), 'amplifyconfiguration.js');
     }
-
     await this.clientConfigGenerator.generateClientConfigToFile(
       backendIdentifier,
       targetPath
