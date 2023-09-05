a0c24a6d5fde2a22fab7117f8b2009f08196e584
Edward Foyle <foyleef@amazon.com>
2023-09-05 15:53:55 -0700

    fix types

--- packages/integration-tests/src/short_uuid.ts
+++ packages/integration-tests/src/short_uuid.ts
@@ -3,4 +3,12 @@ import { randomUUID } from 'crypto';
 /**
  * Generate a short UUID
  */
-export const shortUuid = () => randomUUID().split('-').at(-1);
+export const shortUuid = (): string => {
+  const result = randomUUID().split('-').at(-1);
+  if (typeof result !== 'string') {
+    throw new Error(
+      'short uuid is not a string. Something has gone terribly wrong'
+    );
+  }
+  return result;
+};
