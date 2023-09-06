4e4c490151f318953670cae8d8bdb44994b02c45
Edward Foyle <foyleef@amazon.com>
2023-09-06 13:55:34 -0700

    revert package update

--- packages/backend-storage/src/factory.ts
+++ packages/backend-storage/src/factory.ts
@@ -22,7 +22,6 @@ export class AmplifyStorageFactory implements ConstructFactory<AmplifyStorage> {
    * Set the properties that will be used to initialize the bucket
    */
   constructor(private readonly props: AmplifyStorageProps) {
-    // test update package
     this.importStack = new Error().stack;
   }
 
