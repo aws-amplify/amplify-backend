407a09ffc1c2beb89f23e98188ce10bda8037b54
Hoang Nguyen <115044274+hoangnbn@users.noreply.github.com>
2023-09-19 09:03:20 -0700

    Implement Samsara secret. (#156)
    * Samsara secret implementation
    
    * Rebase, add unit test, and make secret() works
    
    * Add to-do note to implement secret inheritance for CI/CD
    
    * Fix linting and API extractor error
    
    * Create lambda-backed custom resource to retrieve backend parameters
    
    * Reuse secret fetcher lambda, renaming, and some unit tests
    
    * Change input of backend secret to take in uniqueBackendIdentifier, plus some minor changes
    
    * 1. Move secret lib to a separate backend-secret pkg
    2. Refactor secret custom resource and update backend-secret API input
    3. Renaming and minor changes.
    
    * Refactor/renaming some code and update backend-secret pkg
    
    * Add version to backend:secret()
    
    * Remove BackendSecretResolver and relevant code
    
    * Update package-lock.json
    
    * Add a changeset file
    
    * Add grantable and nodejs to lint spellcheck skipword. Fix changeset file.
    
    * Update backend-graphql/API.md
    
    * Update backend-secret API
    
    ---------
    
    Co-authored-by: Edward Foyle <foyleef@amazon.com>
    Co-authored-by: Hoang Nguyen <hoangaz@amazon.com>

---
+++ packages/backend/src/engine/backend-secret/backend_secret_fetcher_provider_factory.ts
@@ -0,0 +1,55 @@
+import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { UniqueBackendIdentifier } from '@aws-amplify/plugin-types';
+import { Duration } from 'aws-cdk-lib';
+import * as path from 'path';
+import { Runtime as LambdaRuntime } from 'aws-cdk-lib/aws-lambda';
+import { Provider } from 'aws-cdk-lib/custom-resources';
+import { fileURLToPath } from 'url';
+import { SecretClient } from '@aws-amplify/backend-secret';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const resourcesRoot = path.normalize(path.join(dirname, 'lambda'));
+const backendSecretLambdaFilePath = path.join(
+  resourcesRoot,
+  'backend_secret_fetcher.js'
+);
+
+/**
+ * The factory to create secret-fetcher provider.
+ */
+export class BackendSecretFetcherProviderFactory {
+  /**
+   * Creates a secret-fetcher provider factory.
+   */
+  constructor(private readonly secretClient: SecretClient) {}
+
+  /**
+   * Returns a resource provider if it exists in the input scope. Otherwise,
+   * creates a new provider.
+   */
+  getOrCreateInstance = (
+    scope: Construct,
+    providerId: string,
+    backendIdentifier: UniqueBackendIdentifier
+  ) => {
+    const provider = scope.node.tryFindChild(providerId) as Provider;
+    if (provider) {
+      return provider;
+    }
+
+    const secretLambda = new NodejsFunction(scope, `${providerId}Lambda`, {
+      runtime: LambdaRuntime.NODEJS_18_X,
+      timeout: Duration.seconds(10),
+      entry: backendSecretLambdaFilePath,
+      handler: 'handler',
+    });
+
+    this.secretClient.grantPermission(secretLambda, backendIdentifier, ['GET']);
+
+    return new Provider(scope, providerId, {
+      onEventHandler: secretLambda,
+    });
+  };
+}
