bdb2f655d598d1a3ed91741c03cc49816f12197f
Spencer Stolworthy <sstol@amazon.com>
2023-09-20 01:47:50 -0600

    lint: fix lints

--- packages/cli/src/commands/sandbox/sandbox_command.ts
+++ packages/cli/src/commands/sandbox/sandbox_command.ts
@@ -8,7 +8,6 @@ import { SandboxIdResolver } from './sandbox_id_resolver.js';
 import { LocalAppNameResolver } from '../../local_app_name_resolver.js';
 import { CwdPackageJsonLoader } from '../../cwd_package_json_loader.js';
 import { generateClientConfigToFile } from '@aws-amplify/client-config';
-import { ClientConfigGeneratorAdapter } from '../generate/config/client_config_generator_adapter.js';
 import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 
 export type SandboxCommandOptions = {
