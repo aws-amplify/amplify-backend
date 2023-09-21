3641d5296c034ea6aa1cae15a19d5bcafeec5c31
Spencer Dean Stolworthy <sdstolworthy@gmail.com>
2023-09-21 00:13:43 -0600

    fix: remove graphql models

--- src/graphql/mutations.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-// This is an auto generated file. Edits will be overwritten.
-
-export const createTodo = /* GraphQL */ `[object Object]`;
-export const deleteTodo = /* GraphQL */ `[object Object]`;
-export const updateTodo = /* GraphQL */ `[object Object]`;
--- src/graphql/queries.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-// This is an auto generated file. Edits will be overwritten.
-
-export const getTodo = /* GraphQL */ `[object Object]`;
-export const listTodos = /* GraphQL */ `[object Object]`;
