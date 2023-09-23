9269bc44395ab598cbaca68eaac8a3c8320f95ae
MJ☔ <zhamujun@amazon.com>
2023-09-22 17:08:28 -0700

    chore: fix config export lint error

--- packages/client-config/src/client-config-writer/client_config_writer.ts
+++ packages/client-config/src/client-config-writer/client_config_writer.ts
@@ -18,11 +18,13 @@ export class ClientConfigWriter {
     switch (fileExtension) {
       case '.ts':
       case '.js': {
-        const fileContent = `export default ${JSON.stringify(
+        const jsCode = JSON.stringify(clientConfig, null, 2);
+        const fileContent = `const amplifyConfig = ${JSON.stringify(
           clientConfig,
           null,
           2
-        )}${os.EOL}`;
+        )}${os.EOL}export default amplifyConfig;${os.EOL}
+        `;
         await fsp.writeFile(targetPath, fileContent);
         break;
       }
