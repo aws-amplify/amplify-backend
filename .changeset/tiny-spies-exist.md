4a51e43327fb1c8a381ff132acc1714fb009b8d4
Edward Foyle <foyleef@amazon.com>
2023-09-28 08:30:37 -0700

    Fix version print (#304)

---
+++ .changeset/tiny-spies-exist.md
@@ -0,0 +1,5 @@
+---
+'@aws-amplify/backend-cli': patch
+---
+
+Fix printing correct version on amplify --version
--- packages/cli/src/package_json_loader.test.ts
+++ packages/cli/src/package_json_loader.test.ts
@@ -1,29 +1,29 @@
 import { describe, it, mock } from 'node:test';
-import { CwdPackageJsonLoader } from './cwd_package_json_loader.js';
+import { PackageJsonFileLoader } from './package_json_loader.js';
 import assert from 'node:assert';
 
 void describe('CwdPackageJsonLoader', () => {
   void describe('loadCwdPackageJson', () => {
     void it('throws error if package.json does not exist', async () => {
-      const loader = new CwdPackageJsonLoader(
+      const loader = new PackageJsonFileLoader(
         {
           existsSync: mock.fn(() => false),
         } as never,
         { readFile: mock.fn(() => ({})) } as never
       );
-      await assert.rejects(() => loader.loadCwdPackageJson());
+      await assert.rejects(() => loader.loadPackageJson());
     });
     void it('throws error if contents are not valid json', async () => {
-      const loader = new CwdPackageJsonLoader(
+      const loader = new PackageJsonFileLoader(
         {
           existsSync: mock.fn(() => true),
         } as never,
         { readFile: mock.fn(() => Promise.resolve('not valid json')) } as never
       );
-      await assert.rejects(() => loader.loadCwdPackageJson());
+      await assert.rejects(() => loader.loadPackageJson());
     });
     void it('throws error if contents do not pass validation', async () => {
-      const loader = new CwdPackageJsonLoader(
+      const loader = new PackageJsonFileLoader(
         {
           existsSync: mock.fn(() => true),
         } as never,
@@ -33,11 +33,11 @@ void describe('CwdPackageJsonLoader', () => {
           ),
         } as never
       );
-      await assert.rejects(() => loader.loadCwdPackageJson());
+      await assert.rejects(() => loader.loadPackageJson());
     });
     void it('returns validated object', async () => {
-      const validValue = { name: 'test-name' };
-      const loader = new CwdPackageJsonLoader(
+      const validValue = { name: 'test-name', version: '0.289.29' };
+      const loader = new PackageJsonFileLoader(
         {
           existsSync: mock.fn(() => true),
         } as never,
@@ -45,7 +45,7 @@ void describe('CwdPackageJsonLoader', () => {
           readFile: mock.fn(() => Promise.resolve(JSON.stringify(validValue))),
         } as never
       );
-      const result = await loader.loadCwdPackageJson();
+      const result = await loader.loadPackageJson();
       assert.deepStrictEqual(result, validValue);
     });
   });
