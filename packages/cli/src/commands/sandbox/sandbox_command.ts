caaf1feeb90a8ee3a4569bcf031e1099cf0221d7
Spencer Stolworthy <sstol@amazon.com>
2023-09-28 22:28:04 -0700

    fix: add out directories to watch exclusions (#320)

--- packages/cli/src/commands/sandbox/sandbox_command.ts
+++ packages/cli/src/commands/sandbox/sandbox_command.ts
@@ -98,7 +98,11 @@ export class SandboxCommand
       args.outDir,
       args.format
     );
-    watchExclusions.push(clientConfigWritePath);
+    watchExclusions.push(
+      clientConfigWritePath,
+      args.uiOutDir,
+      args.modelsOutDir
+    );
     await sandbox.start({
       dir: args.dirToWatch,
       exclude: watchExclusions,
