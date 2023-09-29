b7f705d328a1f4d1ba8067f864cac1a67d088136
Dane Pilcher <dppilche@amazon.com>
2023-09-29 14:03:24 -0600

    test: mult swift file output

--- packages/model-generator/src/graphql_types_generator.test.ts
+++ packages/model-generator/src/graphql_types_generator.test.ts
@@ -1,5 +1,5 @@
 import assert from 'assert';
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import { AppSyncGraphqlTypesGenerator } from './graphql_types_generator.js';
 
 void describe('types generator', () => {
@@ -11,5 +11,55 @@ void describe('types generator', () => {
     await assert.rejects(() =>
       generator.generateTypes({ target: 'typescript' })
     );
+  });
+
+  void it('writes multiple files with the correct name', async () => {
+    const schema = `
+      type Blog {
+        id: ID!
+        name: String!
+        createdAt: AWSDateTime!
+        updatedAt: AWSDateTime!
+      }
+      type Query {
+        getBlog(id: ID!): Blog
+      }
+      input ModelBlogConditionInput {
+        and: [ModelBlogConditionInput]
+        or: [ModelBlogConditionInput]
+        not: ModelBlogConditionInput
+      }
+      input CreateBlogInput {
+        id: ID
+        name: String!
+      }
+      type Mutation {
+        createBlog(input: CreateBlogInput!, condition: ModelBlogConditionInput): Blog
+      }
+      `;
+
+    const mockResultBuilder = mock.fn(() => ({
+      writeToDirectory: () => Promise.resolve(),
+    }));
+    const generator = new AppSyncGraphqlTypesGenerator(
+      async () => schema,
+      mockResultBuilder
+    );
+    await generator.generateTypes({
+      target: 'swift',
+      multipleSwiftFiles: true,
+    });
+    const fileNames = Object.keys(
+      (
+        mockResultBuilder.mock.calls[0].arguments as unknown as [
+          Record<string, string>
+        ]
+      )[0]
+    );
+    assert.deepEqual(fileNames, [
+      'Types.graphql.swift',
+      'queries.graphql.swift',
+      'mutations.graphql.swift',
+    ]);
   });
 });
