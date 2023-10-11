8ae7d4970181ff0fbd3df9efd35ad10e4eb847b2
MJ☔ <o.6180339+github@gmail.com>
2023-10-11 11:38:56 -0700

    test: remove Amplify Prompts

--- packages/create-amplify/src/amplify_project_creator.test.ts
+++ packages/create-amplify/src/amplify_project_creator.test.ts
@@ -1,6 +1,5 @@
 import { describe, it, mock } from 'node:test';
 import assert from 'assert';
-import { AmplifyPrompter } from './amplify_prompts.js';
 import { AmplifyProjectCreator } from './amplify_project_creator.js';
 
 void describe('AmplifyProjectCreator', () => {
@@ -36,30 +35,5 @@ void describe('AmplifyProjectCreator', () => {
       1
     );
     assert.equal(tsConfigInitializerMock.ensureInitialized.mock.callCount(), 1);
-  });
-
-  void it('prompt questions', async (contextual) => {
-    contextual.mock.method(AmplifyPrompter, 'yesOrNo', () =>
-      Promise.resolve(true)
-    );
-
-    const logMock = mock.fn();
-
-    const packageManagerControllerMock = { installDependencies: mock.fn() };
-    const projectRootValidatorMock = { validate: mock.fn() };
-    const initialProjectFileGeneratorMock = {
-      generateInitialProjectFiles: mock.fn(),
-    };
-    const npmInitializedEnsurerMock = { ensureInitialized: mock.fn() };
-    const tsConfigInitializerMock = { ensureInitialized: mock.fn() };
-    const amplifyProjectCreator = new AmplifyProjectCreator(
-      packageManagerControllerMock as never,
-      projectRootValidatorMock as never,
-      initialProjectFileGeneratorMock as never,
-      npmInitializedEnsurerMock as never,
-      tsConfigInitializerMock as never,
-      { log: logMock } as never
-    );
-    assert.equal(await amplifyProjectCreator.create(), undefined);
   });
 });
