1d23ad787adc85ae7a798f6b950e62219f393d5f
Jordan Nelson <Jordan.Ryan.Nelson@gmail.com>
2023-10-16 08:34:00 -0400

    feat: passwordless auth lambdas  (#294)
    * chore: passwordless lambda boilerplate
    
    * fix: use correct file name
    
    * chore: use import.meta.url for ES module support
    
    * chore: add tests for passwordless lambdas
    
    * fix: bad merge conflict resolution
    
    * chore: remove deprecated functions
    
    * chore: refactor lambda functions
    
    * chore: update comments
    
    * chore: update logger, add tests
    
    * chore: update comments
    
    * chore: revert changes to package.json
    
    * chore: update checks to run on feature branch
    
    * chore: remove unused imports
    
    * chore: fix spelling
    
    * chore: update var to let
    
    * chore: unused vars
    
    * chore: remove unused vars from tests
    
    * chore: fix lints in types
    
    * chore: add types to test fns
    
    * chore: remove untested functions in mock
    
    * chore: restructure auth triggers
    
    * fix: file path on windows
    
    * chore: export PasswordlessAuthOptions
    
    * chore: export MagicLinkAuthOptions
    
    * chore: update api
    
    * chore: add changeset
    
    * chore: update path resolution
    
    * chore: move passwordless to new package
    
    * chore: undo changes to auth-construct
    
    * chore: update changeset
    
    * chore: cleanup tests
    
    * chore: update path resolution
    
    * chore: update api.md for backend auth
    
    * chore: update changeset
    
    * chore: remove plugin-types as peer dep
    
    * chore: update fromAddress, remove region
    
    * chore: inject logger
    
    * chore: replace StringMap with Record
    
    * chore: update lambda types
    
    * chore: update logger types
    
    * chore: remove `enabled` flag for magic link

---
+++ packages/passwordless-auth-construct/src/custom-auth/define_auth_challenge.ts
@@ -0,0 +1,76 @@
+import { DefineAuthChallengeTriggerEvent } from 'aws-lambda';
+import { logger } from '../logger.js';
+
+/**
+ * The Define Auth Challenge lambda handler.
+ * @param event - The Define Auth Challenge event provided by Cognito.
+ * @returns the response, including the challenge name.
+ *
+ * Reference: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-define-auth-challenge.html
+ */
+export const defineAuthChallenge = async (
+  event: DefineAuthChallengeTriggerEvent
+): Promise<DefineAuthChallengeTriggerEvent> => {
+  logger.debug(JSON.stringify(event, null, 2));
+
+  const previousSessions = event.request.session;
+  if (!previousSessions || !previousSessions.length) {
+    return customChallenge(event);
+  }
+
+  const onlyContainsCustomChallenges = previousSessions.every(
+    (attempt) => attempt.challengeName !== 'CUSTOM_CHALLENGE'
+  );
+
+  if (!onlyContainsCustomChallenges) {
+    return failAuthentication(event, 'Expected CUSTOM_CHALLENGE');
+  }
+
+  const { signInMethod } = event.request.clientMetadata ?? {};
+  logger.info(`Requested signInMethod: ${signInMethod}`);
+
+  if (signInMethod === 'MAGIC_LINK') {
+    // TODO: Implement magic link.
+    return failAuthentication(event, 'Magic Link is not yet implemented.');
+  } else if (signInMethod === 'OTP') {
+    // TODO: Implement OTP.
+    return failAuthentication(event, 'OTP is not yet implemented.');
+  }
+
+  return failAuthentication(
+    event,
+    `Unrecognized signInMethod: ${signInMethod}`
+  );
+};
+
+/**
+ * Updates the event to fail authentication and returns it.
+ * @param event - The lambda event.
+ * @param reason - The reason that authentication has failed.
+ * @returns the updated event.
+ */
+const failAuthentication = (
+  event: DefineAuthChallengeTriggerEvent,
+  reason: string
+) => {
+  logger.info('Failing authentication because:', reason);
+  event.response.issueTokens = false;
+  event.response.failAuthentication = true;
+  logger.debug(JSON.stringify(event, null, 2));
+  return event;
+};
+
+/**
+ * Updates the event with the name `CUSTOM_CHALLENGE` and returns it.
+ * @param event - The lambda event.
+ * @returns the updated event.
+ */
+const customChallenge = (event: DefineAuthChallengeTriggerEvent) => {
+  logger.info('No session yet, starting one ...');
+  event.response.issueTokens = false;
+  event.response.failAuthentication = false;
+  event.response.challengeName = 'CUSTOM_CHALLENGE';
+  logger.info('Next step: CUSTOM_CHALLENGE');
+  logger.debug(JSON.stringify(event, null, 2));
+  return event;
+};
