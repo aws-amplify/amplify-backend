700318b9d64866a8eaba36f95926be8f2e0e7dde
Spencer Dean Stolworthy <sdstolworthy@gmail.com>
2023-10-17 09:14:52 -0700

    formgen: do not run formgen automatically after sandbox

--- packages/cli/src/commands/sandbox/sandbox_command_factory.ts
+++ packages/cli/src/commands/sandbox/sandbox_command_factory.ts
@@ -1,21 +1,11 @@
 import { CommandModule } from 'yargs';
-import {
-  SandboxCommand,
-  SandboxCommandOptions,
-  SandboxEventHandlerCreator,
-} from './sandbox_command.js';
+import { SandboxCommand, SandboxCommandOptions } from './sandbox_command.js';
 import { SandboxSingletonFactory } from '@aws-amplify/sandbox';
 import { SandboxDeleteCommand } from './sandbox-delete/sandbox_delete_command.js';
 import { SandboxIdResolver } from './sandbox_id_resolver.js';
 import { CwdPackageJsonLoader } from '../../cwd_package_json_loader.js';
-import { ClientConfigGeneratorAdapter } from '../../client-config/client_config_generator_adapter.js';
-import { fromNodeProviderChain } from '@aws-sdk/credential-providers';
 import { LocalAppNameResolver } from '../../backend-identifier/local_app_name_resolver.js';
 import { createSandboxSecretCommand } from './sandbox-secret/sandbox_secret_command_factory.js';
-import { BackendOutputClientFactory } from '@aws-amplify/deployed-backend-client';
-import { graphqlOutputKey } from '@aws-amplify/backend-output-schemas';
-import { FormGenerationHandler } from '../../form-generation/form_generation_handler.js';
-import { SandboxBackendIdentifier } from '@aws-amplify/platform-core';
 
 /**
  * Creates wired sandbox command.
@@ -24,60 +14,13 @@ export const createSandboxCommand = (): CommandModule<
   object,
   SandboxCommandOptions
 > => {
-  const credentialProvider = fromNodeProviderChain();
   const sandboxIdResolver = new SandboxIdResolver(
     new LocalAppNameResolver(new CwdPackageJsonLoader())
   );
   const sandboxFactory = new SandboxSingletonFactory(sandboxIdResolver.resolve);
-  const clientConfigGeneratorAdapter = new ClientConfigGeneratorAdapter(
-    credentialProvider
-  );
-  const getBackendIdentifier = async (appName?: string) => {
-    const sandboxId = appName ?? (await sandboxIdResolver.resolve());
-    return new SandboxBackendIdentifier(sandboxId);
-  };
-  const formGeneratorHandler = new FormGenerationHandler({
-    credentialProvider,
-  });
-  const sandboxEventHandlerCreator: SandboxEventHandlerCreator = ({
-    appName,
-    clientConfigOutDir,
-    format,
-    modelsOutDir,
-    uiOutDir,
-    modelsFilter,
-  }) => {
-    return {
-      successfulDeployment: [
-        async () => {
-          const backendIdentifier = await getBackendIdentifier(appName);
-          await clientConfigGeneratorAdapter.generateClientConfigToFile(
-            backendIdentifier,
-            clientConfigOutDir,
-            format
-          );
-          const outputClient = BackendOutputClientFactory.getInstance({
-            credentials: credentialProvider,
-          });
-          const output = await outputClient.getOutput(backendIdentifier);
-          const apiUrl =
-            output[graphqlOutputKey]?.payload.amplifyApiModelSchemaS3Uri;
-          if (apiUrl) {
-            await formGeneratorHandler.generate({
-              backendIdentifier,
-              apiUrl,
-              modelsOutDir,
-              uiOutDir,
-              modelsFilter,
-            });
-          }
-        },
-      ],
-    };
-  };
-  return new SandboxCommand(
-    sandboxFactory,
-    [new SandboxDeleteCommand(sandboxFactory), createSandboxSecretCommand()],
-    sandboxEventHandlerCreator
-  );
+
+  return new SandboxCommand(sandboxFactory, [
+    new SandboxDeleteCommand(sandboxFactory),
+    createSandboxSecretCommand(),
+  ]);
 };
