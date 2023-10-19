df28a550e820238f8a3045c35acb4990532703b4
MJ☔ <o.6180339+github@gmail.com>
2023-10-19 14:27:35 -0700

    test: fix relative path

--- packages/create-amplify/src/get_project_root.test.ts
+++ packages/create-amplify/src/get_project_root.test.ts
@@ -54,7 +54,7 @@ void describe('getProjectRoot', () => {
 
   void it('creates the project root directory if the user provided relative path does not exist', async (ctx) => {
     process.env.npm_config_yes = 'false';
-    const userInput = 'test/root';
+    const userInput = path.resolve('test', 'root');
     const fsMkDirSyncMock = ctx.mock.method(fsp, 'mkdir', () => undefined);
     ctx.mock.method(fsp, 'stat', () => Promise.reject(new Error()));
     ctx.mock.method(AmplifyPrompter, 'input', () => Promise.resolve(userInput));
@@ -63,6 +63,6 @@ void describe('getProjectRoot', () => {
 
     assert.equal(fsMkDirSyncMock.mock.callCount(), 1);
     assert.equal(fsMkDirSyncMock.mock.calls[0].arguments[0], userInput);
-    assert.equal(projectRoot, path.resolve(process.cwd(), userInput));
+    assert.equal(projectRoot, path.resolve(userInput));
   });
 });
