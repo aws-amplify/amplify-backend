9a41fe5a7a162dfc0fcd683a03facdb13eca079e
Al Harris <alharris@amazon.com>
2023-10-19 11:31:42 -0700

    chore: refactor generate_api_code and add unit tests

--- packages/model-generator/src/generate_api_code.test.ts
+++ packages/model-generator/src/generate_api_code.test.ts
@@ -1,11 +1,253 @@
 import assert from 'assert';
-import { describe, it } from 'node:test';
-import { GenerateApiCodeProps, generateApiCode } from './generate_api_code.js';
+import { describe, it, mock } from 'node:test';
+import {
+  GenerateApiCodeFormat,
+  GenerateApiCodeModelTarget,
+  GenerateApiCodeProps,
+  GenerateApiCodeStatementTarget,
+  GenerateApiCodeTypeTarget,
+  generateApiCode,
+} from './generate_api_code.js';
+import {
+  GraphqlDocumentGenerator,
+  GraphqlModelsGenerator,
+  GraphqlTypesGenerator,
+} from './model_generator.js';
 
 void describe('generateAPICode', () => {
-  void describe('graphql-codegen', () => undefined);
-  void describe('modelgen', () => undefined);
-  void describe('introspection', () => undefined);
+  void describe('graphql-codegen', () => {
+    void it('is invoked with expected input, and returns generated output', async () => {
+      const expectedDocsResults = { docGen: 'results1' };
+      const unexpectedTypeResults = { typeGen: 'results2' };
+
+      // Setup mocked generators
+      const generateModels = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => expectedDocsResults,
+      }));
+      const generateTypes = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => unexpectedTypeResults,
+      }));
+      const overrideGraphqlDocumentGenerator = {
+        generateModels,
+      } as unknown as GraphqlDocumentGenerator;
+      const overrideGraphqlTypesGenerator = {
+        generateTypes,
+      } as unknown as GraphqlTypesGenerator;
+
+      const results = await generateApiCode({
+        stackName: 'testStack',
+        overrideGraphqlDocumentGenerator,
+        overrideGraphqlTypesGenerator,
+        format: GenerateApiCodeFormat.GRAPHQL_CODEGEN,
+        statementTarget: GenerateApiCodeStatementTarget.TYPESCRIPT,
+      });
+
+      // Validate generate calls
+      assert.equal(generateModels.mock.callCount(), 1);
+      assert.deepEqual((generateModels.mock.calls[0].arguments as any)[0], {
+        language: 'typescript',
+        maxDepth: undefined,
+        typenameIntrospection: undefined,
+      });
+      assert.equal(generateTypes.mock.callCount(), 0);
+
+      const receivedResults = await results.getResults();
+      assert.deepEqual(receivedResults, expectedDocsResults);
+    });
+
+    void it('passes through provided parameters', async () => {
+      const expectedDocsResults = { docGen: 'results1' };
+      const unexpectedTypeResults = { typeGen: 'results2' };
+
+      // Setup mocked generators
+      const generateModels = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => expectedDocsResults,
+      }));
+      const generateTypes = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => unexpectedTypeResults,
+      }));
+      const overrideGraphqlDocumentGenerator = {
+        generateModels,
+      } as unknown as GraphqlDocumentGenerator;
+      const overrideGraphqlTypesGenerator = {
+        generateTypes,
+      } as unknown as GraphqlTypesGenerator;
+
+      const results = await generateApiCode({
+        stackName: 'testStack',
+        overrideGraphqlDocumentGenerator,
+        overrideGraphqlTypesGenerator,
+        format: GenerateApiCodeFormat.GRAPHQL_CODEGEN,
+        statementTarget: GenerateApiCodeStatementTarget.TYPESCRIPT,
+        maxDepth: 3,
+        typeNameIntrospection: false,
+      });
+
+      // Validate generate calls
+      assert.equal(generateModels.mock.callCount(), 1);
+      assert.deepEqual((generateModels.mock.calls[0].arguments as any)[0], {
+        language: 'typescript',
+        maxDepth: 3,
+        typenameIntrospection: false,
+      });
+      assert.equal(generateTypes.mock.callCount(), 0);
+
+      const receivedResults = await results.getResults();
+      assert.deepEqual(receivedResults, expectedDocsResults);
+    });
+
+    void it('sets relativeTypesPath if both typescript statements and types are generated', async () => {
+      const expectedDocsResults = { docGen: 'results1' };
+      const expectedTypesResults = { typeGen: 'results2' };
+
+      // Setup mocked generators
+      const generateModels = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => expectedDocsResults,
+      }));
+      const generateTypes = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => expectedTypesResults,
+      }));
+      const overrideGraphqlDocumentGenerator = {
+        generateModels,
+      } as unknown as GraphqlDocumentGenerator;
+      const overrideGraphqlTypesGenerator = {
+        generateTypes,
+      } as unknown as GraphqlTypesGenerator;
+
+      const results = await generateApiCode({
+        stackName: 'testStack',
+        overrideGraphqlDocumentGenerator,
+        overrideGraphqlTypesGenerator,
+        format: GenerateApiCodeFormat.GRAPHQL_CODEGEN,
+        statementTarget: GenerateApiCodeStatementTarget.TYPESCRIPT,
+        typeTarget: GenerateApiCodeTypeTarget.TYPESCRIPT,
+      });
+
+      // Validate generate calls
+      assert.equal(generateModels.mock.callCount(), 1);
+      assert.deepEqual((generateModels.mock.calls[0].arguments as any)[0], {
+        language: 'typescript',
+        maxDepth: undefined,
+        relativeTypesPath: './API',
+        typenameIntrospection: undefined,
+      });
+      assert.equal(generateTypes.mock.callCount(), 1);
+      assert.deepEqual((generateTypes.mock.calls[0].arguments as any)[0], {
+        target: 'typescript',
+        multipleSwiftFiles: undefined,
+      });
+
+      const receivedResults = await results.getResults();
+      assert.deepEqual(receivedResults, {
+        ...expectedDocsResults,
+        ...expectedTypesResults,
+      });
+    });
+  });
+
+  void describe('modelgen', () => {
+    void it('is invoked with expected input, and returns generated output', async () => {
+      const expectedResults = { modelgen: 'results' };
+
+      // Setup mocked generator
+      const generateModels = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => expectedResults,
+      }));
+      const overrideGraphqlModelsGenerator = {
+        generateModels,
+      } as unknown as GraphqlModelsGenerator;
+
+      const results = await generateApiCode({
+        stackName: 'testStack',
+        overrideGraphqlModelsGenerator,
+        format: GenerateApiCodeFormat.MODELGEN,
+        modelTarget: GenerateApiCodeModelTarget.TYPESCRIPT,
+      });
+      assert.equal(generateModels.mock.callCount(), 1);
+      assert.deepEqual((generateModels.mock.calls[0].arguments as any)[0], {
+        target: 'typescript',
+        addTimestampFields: undefined,
+        emitAuthProvider: undefined,
+        generateIndexRules: undefined,
+        generateModelsForLazyLoadAndCustomSelectionSet: undefined,
+        handleListNullabilityTransparently: undefined,
+        respectPrimaryKeyAttributesOnConnectionField: undefined,
+        transformerVersion: undefined,
+        useExperimentalPipelinedTransformer: undefined,
+      });
+
+      const receivedResults = await results.getResults();
+      assert.deepEqual(receivedResults, expectedResults);
+    });
+
+    void it('is passing through any provided params', async () => {
+      // Setup mocked generator
+      const generateModels = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => ({}),
+      }));
+      const overrideGraphqlModelsGenerator = {
+        generateModels,
+      } as unknown as GraphqlModelsGenerator;
+
+      await generateApiCode({
+        stackName: 'testStack',
+        overrideGraphqlModelsGenerator,
+        format: GenerateApiCodeFormat.MODELGEN,
+        modelTarget: GenerateApiCodeModelTarget.DART,
+        respectPrimaryKeyAttributesOnConnectionField: true,
+        handleListNullabilityTransparently: true,
+        addTimestampFields: false,
+      });
+      assert.equal(generateModels.mock.callCount(), 1);
+      assert.deepEqual((generateModels.mock.calls[0].arguments as any)[0], {
+        target: 'dart',
+        addTimestampFields: false,
+        emitAuthProvider: undefined,
+        generateIndexRules: undefined,
+        generateModelsForLazyLoadAndCustomSelectionSet: undefined,
+        handleListNullabilityTransparently: true,
+        respectPrimaryKeyAttributesOnConnectionField: true,
+        transformerVersion: undefined,
+        useExperimentalPipelinedTransformer: undefined,
+      });
+    });
+  });
+
+  void describe('introspection', () => {
+    void it('is invoked with expected input, and returns generated output', async () => {
+      const expectedResults = { introspection: 'results' };
+
+      // Setup mocked generator
+      const generateModels = mock.fn(async () => ({
+        writeToDirectory: mock.fn(),
+        getResults: async () => expectedResults,
+      }));
+      const overrideGraphqlModelsGenerator = {
+        generateModels,
+      } as unknown as GraphqlModelsGenerator;
+
+      const results = await generateApiCode({
+        stackName: 'testStack',
+        overrideGraphqlModelsGenerator,
+        format: GenerateApiCodeFormat.INTROSPECTION,
+      });
+      assert.equal(generateModels.mock.callCount(), 1);
+      assert.deepEqual((generateModels.mock.calls[0].arguments as any)[0], {
+        target: 'introspection',
+      });
+
+      const receivedResults = await results.getResults();
+      assert.deepEqual(receivedResults, expectedResults);
+    });
+  });
 
   void it('throws error on unknown format', async () => {
     const props = {
--- packages/model-generator/src/generate_api_code.ts
+++ packages/model-generator/src/generate_api_code.ts
@@ -4,6 +4,9 @@ import { AwsCredentialIdentityProvider } from '@aws-sdk/types';
 import {
   DocumentGenerationParameters,
   GenerationResult,
+  GraphqlDocumentGenerator,
+  GraphqlModelsGenerator,
+  GraphqlTypesGenerator,
 } from './model_generator.js';
 import { createGraphqlModelsGenerator } from './create_graphql_models_generator.js';
 import { createGraphqlTypesGenerator } from './create_graphql_types_generator.js';
@@ -74,10 +77,35 @@ export type GenerateOptions =
   | GenerateModelsOptions
   | GenerateIntrospectionOptions;
 
+type GenerateApiCodeGeneratorOverrides = {
+  overrideGraphqlDocumentGenerator?: GraphqlDocumentGenerator;
+  overrideGraphqlTypesGenerator?: GraphqlTypesGenerator;
+  overrideGraphqlModelsGenerator?: GraphqlModelsGenerator;
+};
+
+type CredentialProvider = {
+  credentialProvider: AwsCredentialIdentityProvider;
+};
+
+type GenerateGraphqlCodegenApiCodeProps = GenerateGraphqlCodegenOptions &
+  BackendIdentifier &
+  GenerateApiCodeGeneratorOverrides &
+  CredentialProvider;
+
+type GenerateModelgenApiCodeProp = GenerateModelsOptions &
+  BackendIdentifier &
+  GenerateApiCodeGeneratorOverrides &
+  CredentialProvider;
+
+type GenerateIntrospectionApiCodeProps = GenerateIntrospectionOptions &
+  BackendIdentifier &
+  GenerateApiCodeGeneratorOverrides &
+  CredentialProvider;
+
 export type GenerateApiCodeProps = GenerateOptions &
-  BackendIdentifier & {
-    credentialProvider: AwsCredentialIdentityProvider;
-  };
+  BackendIdentifier &
+  GenerateApiCodeGeneratorOverrides &
+  Partial<CredentialProvider>;
 
 /**
  * Mock generateApiCode command.
@@ -85,91 +113,137 @@ export type GenerateApiCodeProps = GenerateOptions &
 export const generateApiCode = async (
   props: GenerateApiCodeProps
 ): Promise<GenerationResult> => {
-  const { credentialProvider = fromNodeProviderChain() } = props;
+  const propsWithDefaultCredentialProvider = {
+    credentialProvider: fromNodeProviderChain(),
+    ...props,
+  };
 
-  switch (props.format) {
+  switch (propsWithDefaultCredentialProvider.format) {
     case 'graphql-codegen': {
-      const generateModelsParams: DocumentGenerationParameters = {
-        language: props.statementTarget,
-        maxDepth: props.maxDepth,
-        typenameIntrospection: props.typeNameIntrospection,
-      };
-      if (
-        props.statementTarget === GenerateApiCodeStatementTarget.TYPESCRIPT &&
-        props.typeTarget === GenerateApiCodeTypeTarget.TYPESCRIPT
-      ) {
-        // Cast to unknown to string since the original input to this is typed as a required string, but expects an optional
-        // value, and we want to rely on that behavior.
-        const typeOutputFilepath = getOutputFileName(
-          null as unknown as string,
-          props.typeTarget
-        );
-        const fileName = path.parse(typeOutputFilepath).name;
-        // This is an node import path, so we don't want to use path.join, since that'll produce invalid paths on windows platforms
-        // Hard-coding since this method explicitly writes to the same directory if types are enabled.
-        generateModelsParams.relativeTypesPath = `./${fileName}`;
-      }
-      const documents = await createGraphqlDocumentGenerator({
-        backendIdentifier: props as BackendIdentifier,
-        credentialProvider,
-      }).generateModels(generateModelsParams);
-
-      if (props.typeTarget) {
-        const types = await createGraphqlTypesGenerator({
-          backendIdentifier: props as BackendIdentifier,
-          credentialProvider,
-        }).generateTypes({
-          target: props.typeTarget,
-          multipleSwiftFiles: props.multipleSwiftFiles,
-        });
-
-        return {
-          writeToDirectory: async (directoryPath: string) => {
-            await Promise.all([
-              documents.writeToDirectory(directoryPath),
-              types.writeToDirectory(directoryPath),
-            ]);
-          },
-          getResults: async () => ({
-            ...(await documents.getResults()),
-            ...(await types.getResults()),
-          }),
-        };
-      }
-
-      return documents;
+      return generateGraphqlCodegenApiCode(propsWithDefaultCredentialProvider);
     }
     case 'modelgen': {
-      return createGraphqlModelsGenerator({
-        backendIdentifier: props as BackendIdentifier,
-        credentialProvider,
-      }).generateModels({
-        target: props.modelTarget,
-        generateIndexRules: props.generateIndexRules,
-        emitAuthProvider: props.emitAuthProvider,
-        useExperimentalPipelinedTransformer:
-          props.useExperimentalPipelinedTransformer,
-        transformerVersion: props.transformerVersion,
-        respectPrimaryKeyAttributesOnConnectionField:
-          props.respectPrimaryKeyAttributesOnConnectionField,
-        generateModelsForLazyLoadAndCustomSelectionSet:
-          props.generateModelsForLazyLoadAndCustomSelectionSet,
-        addTimestampFields: props.addTimestampFields,
-        handleListNullabilityTransparently:
-          props.handleListNullabilityTransparently,
-      });
+      return generateModelgenApiCode(propsWithDefaultCredentialProvider);
     }
     case 'introspection': {
-      return createGraphqlModelsGenerator({
-        backendIdentifier: props as BackendIdentifier,
-        credentialProvider,
-      }).generateModels({
-        target: 'introspection',
-      });
+      return generateIntrospectionApiCode(propsWithDefaultCredentialProvider);
     }
     default:
       throw new Error(
         `${(props as GenerateApiCodeProps).format} is not a supported format.`
       );
   }
+};
+
+/**
+ * Execute document, and optionally type generation with relevant targets, wiring through types into statements if possible.
+ */
+const generateGraphqlCodegenApiCode = async (
+  props: GenerateGraphqlCodegenApiCodeProps
+): Promise<GenerationResult> => {
+  const documentGenerator =
+    props.overrideGraphqlDocumentGenerator ??
+    createGraphqlDocumentGenerator({
+      backendIdentifier: props as BackendIdentifier,
+      credentialProvider: props.credentialProvider,
+    });
+  const generateModelsParams: DocumentGenerationParameters = {
+    language: props.statementTarget,
+    maxDepth: props.maxDepth,
+    typenameIntrospection: props.typeNameIntrospection,
+  };
+  if (
+    props.statementTarget === GenerateApiCodeStatementTarget.TYPESCRIPT &&
+    props.typeTarget === GenerateApiCodeTypeTarget.TYPESCRIPT
+  ) {
+    // Cast to unknown to string since the original input to this is typed as a required string, but expects an optional
+    // value, and we want to rely on that behavior.
+    const typeOutputFilepath = getOutputFileName(
+      null as unknown as string,
+      props.typeTarget
+    );
+    const fileName = path.parse(typeOutputFilepath).name;
+    // This is an node import path, so we don't want to use path.join, since that'll produce invalid paths on windows platforms
+    // Hard-coding since this method explicitly writes to the same directory if types are enabled.
+    generateModelsParams.relativeTypesPath = `./${fileName}`;
+  }
+  const documents = await documentGenerator.generateModels(
+    generateModelsParams
+  );
+
+  if (props.typeTarget) {
+    const typesGenerator =
+      props.overrideGraphqlTypesGenerator ??
+      createGraphqlTypesGenerator({
+        backendIdentifier: props as BackendIdentifier,
+        credentialProvider: props.credentialProvider,
+      });
+    const types = await typesGenerator.generateTypes({
+      target: props.typeTarget,
+      multipleSwiftFiles: props.multipleSwiftFiles,
+    });
+
+    return {
+      writeToDirectory: async (directoryPath: string) => {
+        await Promise.all([
+          documents.writeToDirectory(directoryPath),
+          types.writeToDirectory(directoryPath),
+        ]);
+      },
+      getResults: async () => ({
+        ...(await documents.getResults()),
+        ...(await types.getResults()),
+      }),
+    };
+  }
+
+  return documents;
+};
+
+/**
+ * Execute model generation with model target.
+ */
+const generateModelgenApiCode = (
+  props: GenerateModelgenApiCodeProp
+): Promise<GenerationResult> => {
+  const generator =
+    props.overrideGraphqlModelsGenerator ??
+    createGraphqlModelsGenerator({
+      backendIdentifier: props as BackendIdentifier,
+      credentialProvider: props.credentialProvider,
+    });
+
+  return generator.generateModels({
+    target: props.modelTarget,
+    generateIndexRules: props.generateIndexRules,
+    emitAuthProvider: props.emitAuthProvider,
+    useExperimentalPipelinedTransformer:
+      props.useExperimentalPipelinedTransformer,
+    transformerVersion: props.transformerVersion,
+    respectPrimaryKeyAttributesOnConnectionField:
+      props.respectPrimaryKeyAttributesOnConnectionField,
+    generateModelsForLazyLoadAndCustomSelectionSet:
+      props.generateModelsForLazyLoadAndCustomSelectionSet,
+    addTimestampFields: props.addTimestampFields,
+    handleListNullabilityTransparently:
+      props.handleListNullabilityTransparently,
+  });
+};
+
+/**
+ * Execute model generation with introspection target.
+ */
+const generateIntrospectionApiCode = (
+  props: GenerateIntrospectionApiCodeProps
+): Promise<GenerationResult> => {
+  const generator =
+    props.overrideGraphqlModelsGenerator ??
+    createGraphqlModelsGenerator({
+      backendIdentifier: props as BackendIdentifier,
+      credentialProvider: props.credentialProvider,
+    });
+
+  return generator.generateModels({
+    target: 'introspection',
+  });
 };
