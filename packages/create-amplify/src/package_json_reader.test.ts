df9628ed07450b05260dcda1eb5d3986b39b83b0
Kamil Sobol <sobkamil@amazon.com>
2023-10-24 15:44:05 -0700

    more tests

--- packages/create-amplify/src/package_json_reader.test.ts
+++ packages/create-amplify/src/package_json_reader.test.ts
@@ -4,7 +4,7 @@ import assert from 'assert';
 
 void describe('Package JSON reader', () => {
   void it('Can read package json', async () => {
-    const readFileMock = mock.fn(() =>
+    const readFileMock = mock.fn<(arg0: string) => Promise<Buffer>>(() =>
       Promise.resolve(
         Buffer.from(
           `
@@ -25,10 +25,6 @@ void describe('Package JSON reader', () => {
     assert.strictEqual(packageJson.version, 'test_version');
     assert.strictEqual(packageJson.type, 'module');
     assert.strictEqual(1, readFileMock.mock.callCount());
-    assert.ok(
-      (readFileMock.mock.calls[0].arguments[0] as string).endsWith(
-        'package.json'
-      )
-    );
+    assert.ok(readFileMock.mock.calls[0].arguments[0].endsWith('package.json'));
   });
 });
