4f6a58f2108a3cbe0f0f46dbb72a5d9b1ca49528
Kamil Sobol <sobkamil@amazon.com>
2023-10-23 17:29:55 -0700

    fix that

--- scripts/check_dependencies.ts
+++ scripts/check_dependencies.ts
@@ -2,9 +2,9 @@ import { glob } from 'glob';
 import { DependenciesValidator } from './components/dependencies_validator.js';
 
 await new DependenciesValidator(await glob('packages/*'), {
-  '@aws-amplify/datastore': { denyAll: true },
   'aws-amplify': { denyAll: true },
-  'aws-amplify/core': { denyAll: true },
+  '@aws-amplify/datastore': { denyAll: true },
+  '@aws-amplify/core': { denyAll: true },
   '@aws-amplify/cli-core': {
     allowList: [
       '@aws-amplify/backend-cli',
