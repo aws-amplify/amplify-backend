4e4b439141ca545e7f0b770b35e2ea45a6c7c881
MJ☔ <o.6180339+github@gmail.com>
2023-10-24 11:41:01 -0700

    chore: move the default to FileWatchingSandbox

--- packages/sandbox/src/file_watching_sandbox.ts
+++ packages/sandbox/src/file_watching_sandbox.ts
@@ -80,7 +80,7 @@ export class FileWatchingSandbox extends EventEmitter implements Sandbox {
    */
   start = async (options: SandboxOptions) => {
     this.filesChangesTracker = await createFilesChangesTracker(
-      options.dir ?? process.cwd()
+      options.dir ?? './amplify'
     );
     const bootstrapped = await this.isBootstrapped();
     if (!bootstrapped) {
