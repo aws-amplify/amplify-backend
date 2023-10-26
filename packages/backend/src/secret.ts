dd2fe3ca486c9e502a4ecc000adc67b5c6d3d011
josefaidt <josef.aidt@gmail.com>
2023-10-25 17:12:28 -0700

    add secrets jsdoc

--- packages/backend/src/secret.ts
+++ packages/backend/src/secret.ts
@@ -4,7 +4,14 @@ import { BackendSecretFetcherProviderFactory } from './engine/backend-secret/bac
 import { BackendSecretFetcherFactory } from './engine/backend-secret/backend_secret_fetcher_factory.js';
 
 /**
- * Factory function for initializing a backend secret.
+ * Use a secret from AWS Systems Manager (SSM) Parameter Store
+ * @todo add docs link for using secrets
+ * @todo add docs link for creating secrets in the console
+ * @see {@link https://docs.aws.amazon.com/systems-manager/latest/userguide/systems-manager-parameter-store.html AWS documentation for SSM Parameter Store}
+ * @example <caption>Creating a sandbox secret with the CLI</caption>
+ * > amplify sandbox secret set MY_SECRET
+ * @example <caption>Using a secret</caption>
+ * secret('MY_SECRET')
  */
 export const secret = (name: string): BackendSecret => {
   const secretProviderFactory = new BackendSecretFetcherProviderFactory();
