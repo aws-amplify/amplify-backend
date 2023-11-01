e6a04a9760fb0d0bb0a45e61fdb8de8bbc145468
Kamil Sobol <sobkamil@amazon.com>
2023-11-01 11:57:35 -0700

    pr feedback.

--- scripts/components/package_lock_validator.ts
+++ scripts/components/package_lock_validator.ts
@@ -1,27 +1,39 @@
 import fsp from 'fs/promises';
 import { EOL } from 'os';
 
+type ValidationResult = {
+  status: 'pass' | 'fail';
+  jsonPath: string;
+  failureMessage?: string;
+};
+
 /**
  * Validates package lock file.
  */
 export class PackageLockValidator {
   /**
    * A dictionary of functions that validate certain keys.
-   * If value is valid function should return undefined,
-   * otherwise a string that describes violation.
    */
   private readonly validationRules: Record<
     string,
-    (jsonPath: string, value: unknown) => string | undefined
+    (jsonPath: string, value: unknown) => ValidationResult
   > = {
-    resolved: (jsonPath: string, value: unknown) => {
+    resolved: (jsonPath: string, value: unknown): ValidationResult => {
       if (typeof value != 'string') {
-        return `The ${jsonPath} property must be string, got ${typeof value}`;
+        return {
+          status: 'fail',
+          jsonPath,
+          failureMessage: `The ${jsonPath} property must be string, got ${typeof value}`,
+        };
       }
       if (value.includes('localhost') || value.includes('127.0.0.1')) {
-        return `The ${jsonPath} property value ${value} seems to point to localhost. Run 'npm run stop:npm-proxy && npm install' to recover`;
+        return {
+          status: 'fail',
+          jsonPath,
+          failureMessage: `The ${jsonPath} property value ${value} seems to point to localhost. Run 'npm run stop:npm-proxy && npm install' to recover`,
+        };
       }
-      return undefined;
+      return { status: 'pass', jsonPath };
     },
   };
 
@@ -32,35 +44,40 @@ export class PackageLockValidator {
 
   /**
    * Walks the tree and validates nodes.
-   * @returns array of violations
+   * @returns array of validation results.
    */
   private walkTree = (
     node: Record<string, unknown>,
     keyPrefix: string
-  ): Array<string> => {
-    const violations: Array<string> = [];
+  ): Array<ValidationResult> => {
+    const validationResults: Array<ValidationResult> = [];
     for (const [key, value] of Object.entries(node)) {
       const jsonPath = `${keyPrefix}.${key}`;
-      const violation = this.validationRules[key]?.(jsonPath, value);
-      if (violation) {
-        violations.push(violation);
+      const validationResult = this.validationRules[key]?.(jsonPath, value);
+      if (validationResult) {
+        validationResults.push(validationResult);
       }
       if (typeof value === 'object') {
         this.walkTree(value as Record<string, unknown>, jsonPath).forEach(
-          (violation) => violations.push(violation)
+          (validationResult) => validationResults.push(validationResult)
         );
       }
     }
-    return violations;
+    return validationResults;
   };
 
   validate = async (): Promise<void> => {
     const packageLockContent = JSON.parse(
       await fsp.readFile(this.packageLockPath, 'utf-8')
     );
-    const violations = this.walkTree(packageLockContent, '$root');
+    const validationResults = this.walkTree(packageLockContent, '$root');
+    const violations = validationResults.filter(
+      (validationResults) => validationResults.status === 'fail'
+    );
     if (violations.length > 0) {
-      throw new Error(violations.join(EOL));
+      throw new Error(
+        violations.map((violation) => violation.failureMessage).join(EOL)
+      );
     }
   };
 }
