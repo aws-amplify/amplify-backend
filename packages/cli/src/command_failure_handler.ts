4ad1db95cecde8ab2b801b39827933f9a2c62dc1
Hoang Nguyen <115044274+hoangnbn@users.noreply.github.com>
2023-11-03 12:54:01 -0700

    Gracefully handle Crtl+C error during CLI prompting (#569)
    * Gracefully handle Crtl+C error during CLI prompting
    
    * Add a changeset
    
    * Add unit tests
    
    * Remove cancellation message
    
    ---------
    
    Co-authored-by: Hoang Nguyen <hoangaz@amazon.com>

--- packages/cli/src/command_failure_handler.ts
+++ packages/cli/src/command_failure_handler.ts
@@ -1,5 +1,4 @@
 import { Argv } from 'yargs';
-
 import { COLOR, Printer } from '@aws-amplify/cli-core';
 
 /**
@@ -10,11 +9,18 @@ import { COLOR, Printer } from '@aws-amplify/cli-core';
  * @param yargs instance of yargs as made available in the builder
  */
 export const handleCommandFailure = (msg: string, err: Error, yargs: Argv) => {
-  Printer.printNewLine();
+  if (isUserForceClosePromptError(err)) {
+    return;
+  }
 
+  Printer.printNewLine();
   yargs.showHelp();
 
   Printer.printNewLine();
   Printer.print(msg || String(err), COLOR.RED);
   Printer.printNewLine();
+};
+
+const isUserForceClosePromptError = (err: Error): boolean => {
+  return err?.message.includes('User force closed the prompt');
 };
