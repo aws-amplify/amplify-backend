7d1419eda7730f745a145551d2b1cd6001bc6522
Praveen Gupta <pravgupt@amazon.com>
2023-11-07 23:32:17 +0100

    PR Updates

--- packages/platform-core/src/backend_entry_point_locator.ts
+++ packages/platform-core/src/backend_entry_point_locator.ts
@@ -1,26 +1,27 @@
 import fs from 'fs';
 import path from 'path';
 
-const relativePath = path.join('amplify', 'backend');
-
-// Give preference to JS if that exists over TS in case customers have their own compilation process.
-const supportedFileExtensions = ['.js', '.mjs', '.cjs', '.ts'];
-
 /**
  * Find the backend definition file in the customer app that represents a CDK app.
  */
 export class BackendLocator {
+  private relativePath = path.join('amplify', 'backend');
+
+  // Give preference to JS if that exists over TS in case customers have their own compilation process.
+  private supportedFileExtensions = ['.js', '.mjs', '.cjs', '.ts'];
   /**
    * Constructor for BackendLocator
    */
   constructor(private readonly rootDir: string = process.cwd()) {}
 
   locate = () => {
-    for (const fileExtension of supportedFileExtensions) {
+    for (const fileExtension of this.supportedFileExtensions) {
       if (
-        fs.existsSync(path.resolve(this.rootDir, relativePath + fileExtension))
+        fs.existsSync(
+          path.resolve(this.rootDir, this.relativePath + fileExtension)
+        )
       ) {
-        return relativePath + fileExtension;
+        return this.relativePath + fileExtension;
       }
     }
 
