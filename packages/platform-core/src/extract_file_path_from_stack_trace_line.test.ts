c6ec472146ee9a3489b02252bb08a8dcd39fd615
Praveen Gupta <pravgupt@amazon.com>
2023-11-07 22:29:42 +0100

    fix windows test

--- packages/platform-core/src/extract_file_path_from_stack_trace_line.test.ts
+++ packages/platform-core/src/extract_file_path_from_stack_trace_line.test.ts
@@ -2,6 +2,7 @@ import { describe, it } from 'node:test';
 import { FilePathExtractor } from './extract_file_path_from_stack_trace_line.js';
 import * as assert from 'assert';
 import { fileURLToPath, pathToFileURL } from 'url';
+import path from 'path';
 
 void describe('extractFilePathFromStackTraceLine regex', () => {
   // stack trace generated with TS files is of format `at SomeSymbol (C:\\Users\\alias\\some\\path\\src\\fileName.ts:11:11`
@@ -31,7 +32,10 @@ void describe('extractFilePathFromStackTraceLine regex', () => {
     void it('matches absolute windows paths', () => {
       const line = `    at ${someFileUrl.toString()}/fileName.ts:28:24`;
       const result = new FilePathExtractor(line).extract();
-      assert.equal(result, `${fileURLToPath(someFileUrl)}/fileName.ts`);
+      assert.equal(
+        result,
+        path.resolve(fileURLToPath(someFileUrl), 'fileName.ts')
+      );
     });
   });
 });
