db1d1cfb15d83d27d720301e75bf55a7df4326cc
josefaidt <josef.aidt@gmail.com>
2023-11-09 16:34:56 -0800

    rm comments

--- packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts
+++ packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts
@@ -4,17 +4,5 @@ import { defineAuth } from '@aws-amplify/backend'
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    // externalProviders: {
-    //   loginWithAmazon: {
-    //     clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
-    //     clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
-    //   },
-    //   callbackUrls: ['http://localhost:3000/welcome'],
-    //   logoutUrls: ['http://localhost:3000/come-back-soon'],
-    // },
   },
-  // multifactor: {
-  //   mode: 'OPTIONAL',
-  //   totp: true,
-  // }
 })
