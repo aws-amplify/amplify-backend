5aaf9ebadd2adc69cd0dc4f1be4124ee8646a4e1
josef <josef.aidt@gmail.com>
2023-11-10 10:39:30 -0800

    Update packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts

--- packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts
+++ packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend'
+import { defineAuth } from '@aws-amplify/backend';
 
 /** @see https://docs.amplify.aws/gen2/build-a-backend/auth */
 export const auth = defineAuth({
