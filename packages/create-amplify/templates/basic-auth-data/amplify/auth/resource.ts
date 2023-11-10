4470038af7c98dd2288cbfe5d795b9bafbbddad1
josefaidt <josef.aidt@gmail.com>
2023-11-09 16:32:38 -0800

    add comments, link to docs in defineAuth template

--- packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts
+++ packages/create-amplify/templates/basic-auth-data/amplify/auth/resource.ts
@@ -1,7 +1,20 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend'
 
+/** @see https://docs.amplify.aws/gen2/build-a-backend/auth */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    // externalProviders: {
+    //   loginWithAmazon: {
+    //     clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+    //     clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
+    //   },
+    //   callbackUrls: ['http://localhost:3000/welcome'],
+    //   logoutUrls: ['http://localhost:3000/come-back-soon'],
+    // },
   },
-});
+  // multifactor: {
+  //   mode: 'OPTIONAL',
+  //   totp: true,
+  // }
+})
