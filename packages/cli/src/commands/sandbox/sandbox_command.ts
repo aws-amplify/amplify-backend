32f5997f3b9d8468eb606042eb21abe11fe32010
MJ☔ <o.6180339+github@gmail.com>
2023-11-15 12:25:56 -0800

    feat: disable version for sandbox, generate, pipeline-deploy, and configure

--- packages/cli/src/commands/sandbox/sandbox_command.ts
+++ packages/cli/src/commands/sandbox/sandbox_command.ts
@@ -117,7 +117,7 @@ export class SandboxCommand
       yargs
         // Cast to erase options types used in internal sub command implementation. Otherwise, compiler fails here.
         .command(this.sandboxSubCommands)
-        .version()
+        .version(false)
         .option('dir-to-watch', {
           describe:
             'Directory to watch for file changes. All subdirectories and files will be included. defaults to the current directory.',
