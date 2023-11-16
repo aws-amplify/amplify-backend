3df4a38fe1f6a033d3c7db9ff3ceb8ca3e6f61b6
Kamil Sobol <sobkamil@amazon.com>
2023-11-16 12:13:29 -0800

    try that

--- packages/integration-tests/src/test-e2e/test_concurrency.ts
+++ packages/integration-tests/src/test-e2e/test_concurrency.ts
@@ -4,4 +4,4 @@ import os from 'os';
  * Defines concurrency level for e2e tests.
  * These tests are I/O bound so we can be aggressive even on machines without many cores.
  */
-export const testConcurrencyLevel = Math.min(4, os.availableParallelism());
+export const testConcurrencyLevel = Math.max(4, os.availableParallelism());
