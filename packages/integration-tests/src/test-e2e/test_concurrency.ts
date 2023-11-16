2fc810e16fe740ff3f857fb7bfd753d33d591027
Kamil Sobol <sobkamil@amazon.com>
2023-11-16 15:37:41 -0800

    lets see

--- packages/integration-tests/src/test-e2e/test_concurrency.ts
+++ packages/integration-tests/src/test-e2e/test_concurrency.ts
@@ -1,15 +1,7 @@
 import os from 'os';
 
-const getConcurrencyLevel = () => {
-  if (process.platform.includes('win32')) {
-    // TODO use default on Windows until we get better workers.
-    return undefined;
-  }
-  // These tests are I/O bound, so we can be aggressive even on machines without many cores.
-  return Math.max(4, os.availableParallelism());
-};
-
 /**
  * Defines concurrency level for e2e tests.
+ * These tests are I/O bound, so we can be aggressive even on machines without many cores.
  */
-export const testConcurrencyLevel = getConcurrencyLevel();
+export const testConcurrencyLevel = Math.max(4, os.availableParallelism());
