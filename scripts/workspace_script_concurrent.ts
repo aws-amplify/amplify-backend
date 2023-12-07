8e850d17dac6a31bbcd401533c85ea36b1b41ae0
Edward Foyle <foyleef@amazon.com>
2023-12-06 18:17:48 -0800

    store tsbuildinfo hashes per command

--- scripts/workspace_script_concurrent.ts
+++ scripts/workspace_script_concurrent.ts
@@ -11,15 +11,32 @@ import { createHash } from 'crypto';
 // this script also uses tsconfig.tsbuildinfo hashes to determine if a package has changes before running the script in that package
 // if a package does not have a tsconfig.tsbuildinfo file, or if it hashes to a different value than the last time this script ran, then the script is run in that package
 
+/* file that stores tsconfig.tsbuildinfo hahses for each package for each command
+    structure is
+    {
+      command-with-args: {
+        packages/path: tsbuildinfo hash
+      }
+    }
+ */
 const cacheFile = new URL(
   './workspace_script_concurrent_cache.json',
   import.meta.url
 );
+
+// get the existing cache, or initialize an empty cache
 const hashCache = existsSync(cacheFile)
   ? JSON.parse(await fs.readFile(cacheFile, 'utf-8'))
   : {};
 
+// extract the command args that should be run in each package
 const runArgs = process.argv.slice(2);
+
+// create a cache key from the command args
+const commandCacheKey = runArgs.join('-');
+
+// get the part of the cache relevant to this command
+const commandHashCache = hashCache[commandCacheKey] || {};
 
 const packagePaths = await glob('./packages/*');
 
@@ -29,21 +46,29 @@ const runInDir = (dir: string) =>
     stdio: 'inherit',
   });
 
+// iterate over all the packages in the project
 const runPromises = packagePaths.map(async (packagePath) => {
   const packageTsBuildInfoPath = path.join(packagePath, 'tsconfig.tsbuildinfo');
+  // if the package doesn't have a tsbuildinfo file, execute the command
   if (!existsSync(packageTsBuildInfoPath)) {
     return runInDir(packagePath);
   }
   const packageTsBuildInfoHash = createHash('sha512')
     .update(await fs.readFile(packageTsBuildInfoPath))
     .digest('hex');
-  if (hashCache[packagePath] !== packageTsBuildInfoHash) {
+  // if the tsbuildinfo file has a different hash than the last time the command ran, run it again
+  if (commandHashCache[packagePath] !== packageTsBuildInfoHash) {
     await runInDir(packagePath);
   }
-  hashCache[packagePath] = packageTsBuildInfoHash;
+  // store the new hash in the cache
+  commandHashCache[packagePath] = packageTsBuildInfoHash;
   return undefined;
 });
 
 await Promise.all(runPromises);
 
+// set the command cache in the main cache object
+hashCache[commandCacheKey] = commandHashCache;
+
+// persist the updated cache
 await fs.writeFile(cacheFile, JSON.stringify(hashCache, null, 2));
