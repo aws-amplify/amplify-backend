66c2cfb097afaeec466aa0de3ecbce4eceaa3872
Kamil Sobol <sobkamil@amazon.com>
2023-12-09 16:31:42 -0800

    comment

--- scripts/check_api_changes.ts
+++ scripts/check_api_changes.ts
@@ -4,6 +4,17 @@ import { existsSync } from 'fs';
 import { ApiChangesValidator } from './components/api-changes-validator/api_changes_validator.js';
 import { fileURLToPath } from 'url';
 
+/**
+ * This scripts checks whether current (aka latest) repository content
+ * introduces compile time breaking changes in exported APIs.
+ *
+ * In order to debug this check locally:
+ * 1. Checkout content of your PR
+ * 2. Checkout content of your PR's base branch
+ * 3. Create a directory where test projects are going to be generated
+ * 4. Run 'tsx scripts/check_api_changes.ts <baselineBranchPath> <workingDirectory>'
+ */
+
 // extract the command args that should be run in each package
 const runArgs = process.argv.slice(2);
 
@@ -11,7 +22,7 @@ let [baselineRepositoryPath, workingDirectory] = runArgs;
 
 if (!baselineRepositoryPath || !workingDirectory) {
   throw new Error(
-    'Usage: tsx scripts/check_api_changes.ts <baselineRepositoryPath> <workingDirectory>'
+    'Usage: tsx scripts/check_api_changes.ts <baselineBranchPath> <workingDirectory>'
   );
 }
 
