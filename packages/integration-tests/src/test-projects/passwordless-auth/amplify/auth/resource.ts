02941211fff1eb415d08b6ea5ab8dc069ba0bb30
Israel Arcos <israel.arcos2595@gmail.com>
2023-12-12 14:11:05 -0500

    chore: remove email required user attribute

--- packages/integration-tests/src/test-projects/passwordless-auth/amplify/auth/resource.ts
+++ packages/integration-tests/src/test-projects/passwordless-auth/amplify/auth/resource.ts
@@ -4,9 +4,6 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  userAttributes: {
-    email: { required: true },
-  },
   passwordlessAuth: {
     // originationNumber and fromAddress values are placeholders
     // until we find a solution to add them to work with CI.
