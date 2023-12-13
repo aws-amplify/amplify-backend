60d00d3356cc9e119a40e9f19f9606514c642f3d
Kamil Sobol <sobkamil@amazon.com>
2023-12-13 13:24:29 -0800

    build: cover class methods and properties in api check (#818)
    * build: cover class methods and properties in api check
    
    * integration test
    
    * fix that
    
    * indent with 2 spaces
    
    * indent with 2 spaces

--- scripts/components/api-changes-validator/api_usage_generator.test.ts
+++ scripts/components/api-changes-validator/api_usage_generator.test.ts
@@ -14,18 +14,31 @@ const testCases: Array<UsageGeneratorTestCase> = [
   {
     description: 'generates type usage',
     apiReportCode: `
-export type SomeType = {
+export type SomeType1 = {
     someProperty: string;
+};
+export type SomeType2<T1, T2> = {
+    someProperty1: T1;
+    someProperty1: T2;
 };
     `,
     expectedApiUsage: `
-import { SomeType } from 'samplePackageName';
+import { SomeType1 } from 'samplePackageName';
+import { SomeType2 } from 'samplePackageName';
 
-type SomeTypeBaseline = {
+type SomeType1Baseline = {
     someProperty: string;
 }
-const SomeTypeUsageFunction = (someTypeFunctionParameter: SomeTypeBaseline) => {
-    const someType: SomeType = someTypeFunctionParameter;
+const someType1UsageFunction = (someType1FunctionParameter: SomeType1Baseline) => {
+  const someType1: SomeType1 = someType1FunctionParameter;
+}
+
+type SomeType2Baseline<T1, T2> = {
+    someProperty1: T1;
+    someProperty1: T2;
+}
+const someType2UsageFunction = <T1, T2>(someType2FunctionParameter: SomeType2Baseline<T1, T2>) => {
+  const someType2: SomeType2<T1, T2> = someType2FunctionParameter;
 }
     `,
   },
@@ -76,36 +89,128 @@ import { someFunction3 } from 'samplePackageName';
 import { someFunction4 } from 'samplePackageName';
 
 const someFunction1UsageFunction = () => {
-    someFunction1();
-    someFunction1();
+  someFunction1();
+  someFunction1();
 }
 
 const someFunction2UsageFunction = (param1: string, param2?: number) => {
-    const returnValue: string = someFunction2(param1);
-    someFunction2(param1, param2);
+  const returnValue: string = someFunction2(param1);
+  someFunction2(param1, param2);
 }
 
 const someFunction3UsageFunction = (param1: string, param2: number = 1) => {
-    const returnValue: string = someFunction3(param1);
-    someFunction3(param1, param2);
+  const returnValue: string = someFunction3(param1);
+  someFunction3(param1, param2);
 }
 
 const someFunction4UsageFunction = <T1, T2, T3>(param1: T1, param2?: T2) => {
-    const returnValue: Promise<T3> = someFunction4(param1);
-    someFunction4(param1, param2);
+  const returnValue: Promise<T3> = someFunction4(param1);
+  someFunction4(param1, param2);
 }
     `,
   },
   {
     description: 'generates class usage',
     apiReportCode: `
-export class SomeClass {
-    constructor(someConstructorParameter: string);
-    someMethod: () => void;
+export class SomeClass1 {
+}
+export class SomeClass2 {
+  constructor(param1: string, param2?: string);
+}
+export class SomeClass3 {
+  someMethod: (param1: string, param2?: string) => string;
+}
+export class SomeClass4 {
+  someProperty: string;
+  someOptionalProperty?: string;
+}
+export class SomeClass5 {
+  static someStaticMethod: (param1: string, param2?: string) => string;
+}
+export class SomeClass6 {
+  static someStaticProperty: string;
+}
+export class SomeClass7<T1, T2, T3, T4, T5, T6> {
+  constructor(param1: T1, param2?: T2);
+  someMethod: (param1: T3, param2?: T4) => T5;
+  someProperty: T6;
+}
+export abstract class SomeAbstractClass {
+  constructor(param1: string, param2?: string);
+  someMethod: (param1: string, param2?: string) => string;
+  someProperty: string;
+  static someStaticMethod: (param1: string, param2?: string) => string;
+  static someStaticProperty: string;
 }
     `,
     expectedApiUsage: `
-import { SomeClass } from 'samplePackageName';
+import { SomeClass1 } from 'samplePackageName';
+import { SomeClass2 } from 'samplePackageName';
+import { SomeClass3 } from 'samplePackageName';
+import { SomeClass4 } from 'samplePackageName';
+import { SomeClass5 } from 'samplePackageName';
+import { SomeClass6 } from 'samplePackageName';
+import { SomeClass7 } from 'samplePackageName';
+import { SomeAbstractClass } from 'samplePackageName';
+
+const someClass3SomeMethodUsageOuterFunction = (someClass3SomeMethodUsageOuterFunctionParameter: SomeClass3) => {
+  const SomeClass3SomeMethodUsageInnerFunction = (param1: string, param2?: string) => {
+    const returnValue: string = someClass3SomeMethodUsageOuterFunctionParameter.someMethod(param1);
+    someClass3SomeMethodUsageOuterFunctionParameter.someMethod(param1, param2);
+  }
+}
+
+
+const someClass4SomePropertyUsageOuterFunction = (someClass4SomePropertyUsageOuterFunctionParameter: SomeClass4) => {
+  const propertyValue: string = someClass4SomePropertyUsageOuterFunctionParameter.someProperty;
+}
+const someClass4SomeOptionalPropertyUsageOuterFunction = (someClass4SomeOptionalPropertyUsageOuterFunctionParameter: SomeClass4) => {
+  const propertyValue: string | undefined = someClass4SomeOptionalPropertyUsageOuterFunctionParameter.someOptionalProperty;
+}
+
+
+const someClass5SomeStaticMethodUsageOuterFunction = (someClass5SomeStaticMethodUsageOuterFunctionParameter: SomeClass5) => {
+  const SomeClass5SomeStaticMethodUsageInnerFunction = (param1: string, param2?: string) => {
+    const returnValue: string = SomeClass5.someStaticMethod(param1);
+    SomeClass5.someStaticMethod(param1, param2);
+  }
+}
+
+
+const someClass6SomeStaticPropertyUsageOuterFunction = (someClass6SomeStaticPropertyUsageOuterFunctionParameter: SomeClass6) => {
+  const propertyValue: string = SomeClass6.someStaticProperty;
+}
+
+
+const someClass7SomeMethodUsageOuterFunction = <T1, T2, T3, T4, T5, T6>(someClass7SomeMethodUsageOuterFunctionParameter: SomeClass7<T1, T2, T3, T4, T5, T6>) => {
+  const SomeClass7SomeMethodUsageInnerFunction = (param1: T3, param2?: T4) => {
+    const returnValue: T5 = someClass7SomeMethodUsageOuterFunctionParameter.someMethod(param1);
+    someClass7SomeMethodUsageOuterFunctionParameter.someMethod(param1, param2);
+  }
+}
+const someClass7SomePropertyUsageOuterFunction = <T1, T2, T3, T4, T5, T6>(someClass7SomePropertyUsageOuterFunctionParameter: SomeClass7<T1, T2, T3, T4, T5, T6>) => {
+  const propertyValue: T6 = someClass7SomePropertyUsageOuterFunctionParameter.someProperty;
+}
+
+
+const someAbstractClassSomeMethodUsageOuterFunction = (someAbstractClassSomeMethodUsageOuterFunctionParameter: SomeAbstractClass) => {
+  const SomeAbstractClassSomeMethodUsageInnerFunction = (param1: string, param2?: string) => {
+    const returnValue: string = someAbstractClassSomeMethodUsageOuterFunctionParameter.someMethod(param1);
+    someAbstractClassSomeMethodUsageOuterFunctionParameter.someMethod(param1, param2);
+  }
+}
+const someAbstractClassSomePropertyUsageOuterFunction = (someAbstractClassSomePropertyUsageOuterFunctionParameter: SomeAbstractClass) => {
+  const propertyValue: string = someAbstractClassSomePropertyUsageOuterFunctionParameter.someProperty;
+}
+const someAbstractClassSomeStaticMethodUsageOuterFunction = (someAbstractClassSomeStaticMethodUsageOuterFunctionParameter: SomeAbstractClass) => {
+  const SomeAbstractClassSomeStaticMethodUsageInnerFunction = (param1: string, param2?: string) => {
+    const returnValue: string = SomeAbstractClass.someStaticMethod(param1);
+    SomeAbstractClass.someStaticMethod(param1, param2);
+  }
+}
+const someAbstractClassSomeStaticPropertyUsageOuterFunction = (someAbstractClassSomeStaticPropertyUsageOuterFunctionParameter: SomeAbstractClass) => {
+  const propertyValue: string = SomeAbstractClass.someStaticProperty;
+}
     `,
   },
 ];
--- scripts/components/api-changes-validator/test-resources/test-projects/without-breaks/project-without-breaks/API.md
+++ scripts/components/api-changes-validator/test-resources/test-projects/without-breaks/project-without-breaks/API.md
@@ -14,19 +14,49 @@ export type SampleTypeWithTypeParam<T, D extends SampleType> = {
   anotherProperty?: D;
 };
 
-export class SampleClass {}
+export class SampleClass1 {}
+export class SampleClass2 {
+  constructor(param1: string, param2?: string);
+}
+export class SampleClass3 {
+  someMethod: (param1: string, param2?: string) => string;
+}
+export class SampleClass4 {
+  someProperty: string;
+  someOptionalProperty?: string;
+}
+export class SampleClass5 {
+  static someStaticMethod: (param1: string, param2?: string) => string;
+}
+export class SampleClass6 {
+  static someStaticProperty: string;
+}
+export class SampleClass7<T1, T2, T3, T4, T5, T6> {
+  constructor(param1: T1, param2?: T2);
+  someMethod: (param1: T3, param2?: T4) => T5;
+  someProperty: T6;
+}
+export abstract class SomeAbstractClass {
+  constructor(param1: string, param2?: string);
+  someMethod: (param1: string, param2?: string) => string;
+  someProperty: string;
+  static someStaticMethod: (param1: string, param2?: string) => string;
+  static someStaticProperty: string;
+}
 
 export const someFunction1: () => void;
 export const someFunction2: (param1: string, param2?: number) => string;
 export const someFunction3: (param1: string, param2: number = 1) => string;
-export const someFunction4: <T1, T2, T3>(param1: T1, param2?: T2) => Promise<T3>;
+export const someFunction4: <T1, T2, T3>(
+  param1: T1,
+  param2?: T2
+) => Promise<T3>;
 
 export type SampleTypeUsingClass = {
-  sampleMethod: (param: SampleClass) => void;
+  sampleMethod: (param: SampleClass1) => void;
 };
 
-export type SampleTypeThatReferencesFunction<T extends typeof someFunction1> =
-  {
-    sampleProperty: T;
-  };
+export type SampleTypeThatReferencesFunction<T extends typeof someFunction1> = {
+  sampleProperty: T;
+};
 ```
