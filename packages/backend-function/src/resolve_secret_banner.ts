6fe713bf3a40f8747e320df6c68e1ea0d827eb50
Roshane Pascual <rotp@amazon.com>
2023-12-28 18:46:42 -0800

    test

--- packages/backend-function/src/resolve_secret_banner.ts
+++ packages/backend-function/src/resolve_secret_banner.ts
@@ -1,27 +1,27 @@
-import { SSM } from '@aws-sdk/client-ssm';
+// import { SSM } from '@aws-sdk/client-ssm';
 
-const ssmClient = new SSM();
+// const ssmClient = new SSM();
 
 /**
  * The body of this function will be used to resolve secrets for Lambda functions
  */
-export const resolveSecretBanner = async (client: SSM) => {
+export const resolveSecretBanner = async () => {
   const envArray = process.env.SECRET_PATH_ENV_VARS?.split(',');
-  if (envArray) {
-    const response = await client.getParameters({
-      Names: envArray.map((a) => process.env[a] ?? ''),
-      WithDecryption: true,
-    });
-    if (response.Parameters && response.Parameters?.length > 0) {
-      for (const parameter of response.Parameters) {
-        for (const envName of envArray) {
-          if (parameter.Name === process.env[envName]) {
-            process.env[envName.replace('_PATH', '')] = parameter.Value;
-          }
-        }
-      }
-    }
-  }
+  // if (envArray) {
+  //   const response = await client.getParameters({
+  //     Names: envArray.map((a) => process.env[a] ?? ''),
+  //     WithDecryption: true,
+  //   });
+  //   if (response.Parameters && response.Parameters?.length > 0) {
+  //     for (const parameter of response.Parameters) {
+  //       for (const envName of envArray) {
+  //         if (parameter.Name === process.env[envName]) {
+  //           process.env[envName.replace('_PATH', '')] = parameter.Value;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 };
 
-// await resolveSecretBanner();
+await resolveSecretBanner();
