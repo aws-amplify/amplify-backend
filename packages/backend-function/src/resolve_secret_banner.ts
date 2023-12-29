899bcfd48e9e4f24aa6c017d00b525f1f5e6a046
Roshane Pascual <rotp@amazon.com>
2023-12-28 16:48:43 -0800

    try that

--- packages/backend-function/src/resolve_secret_banner.ts
+++ packages/backend-function/src/resolve_secret_banner.ts
@@ -2,24 +2,26 @@ import { SSM } from '@aws-sdk/client-ssm';
 
 const ssmClient = new SSM();
 
-// eslint-disable-next-line jsdoc/require-jsdoc
-export const resolveSecretBanner = async (client: SSM = ssmClient) => {
-  // const envArray = process.env.SECRET_PATH_ENV_VARS?.split(',');
-  // if (envArray) {
-  //   const response = await client.getParameters({
-  //     Names: envArray.map((a) => process.env[a] ?? ''),
-  //     WithDecryption: true,
-  //   });
-  //   if (response.Parameters && response.Parameters?.length > 0) {
-  //     for (const parameter of response.Parameters) {
-  //       for (const envName of envArray) {
-  //         if (parameter.Name === process.env[envName]) {
-  //           process.env[envName.replace('_PATH', '')] = parameter.Value;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-};
+/**
+ * The body of this function will be used to resolve secrets for Lambda functions
+ */
+// export const resolveSecretBanner = async (client: SSM = ssmClient) => {
+//   const envArray = process.env.SECRET_PATH_ENV_VARS?.split(',');
+//   if (envArray) {
+//     const response = await client.getParameters({
+//       Names: envArray.map((a) => process.env[a] ?? ''),
+//       WithDecryption: true,
+//     });
+//     if (response.Parameters && response.Parameters?.length > 0) {
+//       for (const parameter of response.Parameters) {
+//         for (const envName of envArray) {
+//           if (parameter.Name === process.env[envName]) {
+//             process.env[envName.replace('_PATH', '')] = parameter.Value;
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
 
-await resolveSecretBanner();
+// await resolveSecretBanner();
