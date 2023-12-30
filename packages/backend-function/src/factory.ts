1507e9163963fb63ba1c3b3a84dfb78d785bba45
Roshane Pascual <rotp@amazon.com>
2023-12-29 16:36:11 -0800

    lint fix

--- packages/backend-function/src/factory.ts
+++ packages/backend-function/src/factory.ts
@@ -15,7 +15,6 @@ import { Duration, Stack } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import fs from 'fs';
-import os from 'os';
 import { fileURLToPath } from 'url';
 
 /**
