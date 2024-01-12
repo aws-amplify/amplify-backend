5ff8a9a921077cd85132c1b877f72d8708148602
Kamil Sobol <sobkamil@amazon.com>
2024-01-12 09:17:56 -0800

    comment

--- packages/eslint-rules/src/rules/no_empty_catch.ts
+++ packages/eslint-rules/src/rules/no_empty_catch.ts
@@ -1,5 +1,12 @@
 import { ESLintUtils } from '@typescript-eslint/utils';
 
+/**
+ * This rule flags empty catch blocks. Even if they contain comments.
+ *
+ * This rule differs from built in https://github.com/eslint/eslint/blob/main/lib/rules/no-empty.js
+ * in such a way that it uses typescript-eslint and typescript AST
+ * which does not include comments as statements in catch clause body block.
+ */
 export const rule = ESLintUtils.RuleCreator.withoutDocs({
   create(context) {
     return {
