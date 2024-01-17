e35b3e38fe0b1c1d5d8934b40ec8da9fab66909a
MJ☔ <o.6180339+github@gmail.com>
2024-01-17 11:57:37 -0800

    refactor packageManagerSetup to use switch

--- packages/integration-tests/src/e2e-flow/setup_package_manager.ts
+++ packages/integration-tests/src/e2e-flow/setup_package_manager.ts
@@ -23,60 +23,77 @@ export const setupPackageManager = async (
     ? 'npx'
     : (packageManager as PackageManagerExecutable);
   const execaOptions = {
-    cwd: packageManager === 'yarn-modern' ? dir : os.homedir(),
+    cwd: os.homedir(),
     stdio: 'inherit' as const,
   };
 
-  if (packageManager === 'npm') {
-    // nuke the npx cache to ensure we are installing packages from the npm proxy
-    const { stdout } = await execa('npm', ['config', 'get', 'cache']);
-    const npxCacheLocation = path.join(stdout.toString().trim(), '_npx');
+  switch (packageManager) {
+    case 'npm':
+      {
+        // nuke the npx cache to ensure we are installing packages from the npm proxy
+        const { stdout } = await execa('npm', ['config', 'get', 'cache']);
+        const npxCacheLocation = path.join(stdout.toString().trim(), '_npx');
+        if (existsSync(npxCacheLocation)) {
+          await fsp.rm(npxCacheLocation, { recursive: true });
+        }
+      }
+      break;
 
-    if (existsSync(npxCacheLocation)) {
-      await fsp.rm(npxCacheLocation, { recursive: true });
-    }
-  } else if (packageManager.startsWith('yarn')) {
-    if (packageManager === 'yarn-modern') {
-      await execa('corepack', ['enable'], execaOptions);
-      await execa('yarn', ['init', '-2'], execaOptions);
+    case 'pnpm':
+      {
+        await execa('npm', ['install', '-g', packageManager], {
+          stdio: 'inherit',
+        });
+        await execa(packageManager, ['--version']);
+        await execa(packageManager, [
+          'config',
+          'set',
+          'registry',
+          'http://localhost:4873',
+        ]);
+        await execa(packageManager, ['config', 'get', 'registry']);
+      }
+      break;
 
-      await execa(
-        'yarn',
-        ['config', 'set', 'npmRegistryServer', 'http://localhost:4873'],
-        execaOptions
-      );
-      await execa(
-        'yarn',
-        ['config', 'set', 'unsafeHttpWhitelist', 'localhost'],
-        execaOptions
-      );
-      await execa(
-        'yarn',
-        ['config', 'set', 'nodeLinker', 'node-modules'],
-        execaOptions
-      );
-    } else {
-      await execa('npm', ['install', '-g', 'yarn'], { stdio: 'inherit' });
-      await execa(
-        'yarn',
-        ['config', 'set', 'registry', 'http://localhost:4873'],
-        execaOptions
-      );
-      await execa('yarn', ['config', 'get', 'registry'], execaOptions);
-    }
-    await execa('yarn', ['cache', 'clean'], execaOptions);
-  } else if (packageManager === 'pnpm') {
-    await execa('npm', ['install', '-g', packageManager], {
-      stdio: 'inherit',
-    });
-    await execa(packageManager, ['--version']);
-    await execa(packageManager, [
-      'config',
-      'set',
-      'registry',
-      'http://localhost:4873',
-    ]);
-    await execa(packageManager, ['config', 'get', 'registry']);
+    case 'yarn-classic':
+      {
+        await execa('npm', ['install', '-g', 'yarn'], { stdio: 'inherit' });
+        await execa(
+          'yarn',
+          ['config', 'set', 'registry', 'http://localhost:4873'],
+          execaOptions
+        );
+        await execa('yarn', ['config', 'get', 'registry'], execaOptions);
+        await execa('yarn', ['cache', 'clean'], execaOptions);
+      }
+      break;
+
+    case 'yarn-modern':
+      {
+        execaOptions.cwd = dir;
+        await execa('npm', ['install', '-g', 'yarn'], { stdio: 'inherit' });
+        await execa('yarn', ['init', '-2'], execaOptions);
+        await execa(
+          'yarn',
+          ['config', 'set', 'npmRegistryServer', 'http://localhost:4873'],
+          execaOptions
+        );
+        await execa(
+          'yarn',
+          ['config', 'set', 'unsafeHttpWhitelist', 'localhost'],
+          execaOptions
+        );
+        await execa(
+          'yarn',
+          ['config', 'set', 'nodeLinker', 'node-modules'],
+          execaOptions
+        );
+        await execa('yarn', ['cache', 'clean'], execaOptions);
+      }
+      break;
+
+    default:
+      break;
   }
 
   return { packageManagerExecutable, packageManager };
