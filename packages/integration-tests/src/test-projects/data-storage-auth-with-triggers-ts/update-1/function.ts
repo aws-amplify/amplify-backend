5d316ce7b938a1dcca4413bbdf132d1fb69b2456
Roshane Pascual <rotp@amazon.com>
2024-01-16 17:44:50 -0800

    add amplifySharedSecretNameKey

--- packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/update-1/function.ts
+++ packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/update-1/function.ts
@@ -2,8 +2,4 @@ import { defineFunction, secret } from '@aws-amplify/backend';
 
 export const myFunc = defineFunction({
   entry: './func-src/handler.ts',
-  environment: {
-    TEST_SECRET: secret('amazonSecret'),
-    TEST_SHARED_SECRET: secret(process.env.AMPLIFY_SHARED_SECRET_NAME as string),
-  },
 });
