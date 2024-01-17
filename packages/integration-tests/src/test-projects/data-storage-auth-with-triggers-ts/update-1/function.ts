1f8167a2a381cd9244a4bc4c3fdb86b2bf3195c9
Roshane Pascual <rotp@amazon.com>
2024-01-16 17:54:42 -0800

    fix lint

--- packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/update-1/function.ts
+++ packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/update-1/function.ts
@@ -1,4 +1,4 @@
-import { defineFunction, secret } from '@aws-amplify/backend';
+import { defineFunction } from '@aws-amplify/backend';
 
 export const myFunc = defineFunction({
   entry: './func-src/handler.ts',
