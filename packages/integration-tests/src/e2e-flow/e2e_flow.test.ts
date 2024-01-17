f4c415c4337fc269d2c63358d999302b5c7c7046
MJ☔ <o.6180339+github@gmail.com>
2024-01-17 10:44:19 -0800

    remove the pnpm patch

--- packages/integration-tests/src/e2e-flow/e2e_flow.test.ts
+++ packages/integration-tests/src/e2e-flow/e2e_flow.test.ts
@@ -230,12 +230,6 @@ void describe('installs expected packages and scaffolds expected files', async (
       }
     }
 
-    if (packageManager === 'pnpm') {
-      await execa('pnpm', ['add', '@aws-sdk/credential-providers'], {
-        cwd: tempDir,
-      });
-    }
-
     await execa(
       packageManagerExecutable,
       [
