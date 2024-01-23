01846d56bb3bacd0a66bc535566ad2b91a6b1735
Charles Shin <shin.charles11@gmail.com>
2024-01-23 10:47:08 -0800

    optional chaining
    Co-authored-by: Edward Foyle <foyleef@amazon.com>

--- packages/client-config/src/client-config-writer/client_config_writer.ts
+++ packages/client-config/src/client-config-writer/client_config_writer.ts
@@ -34,8 +34,6 @@ export class ClientConfigWriter {
     const targetPath = await this.pathResolver(outDir, format);
     const fileContent = this.formatter.format(clientConfig, format);
     await this.fsp.writeFile(targetPath, fileContent);
-    if (logFilePath) {
-      logFilePath(targetPath);
-    }
+    logFilePath?.(targetPath)
   };
 }
