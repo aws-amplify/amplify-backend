3ba6071c62caee70de7b721edcfd064bd7ed0aab
MJ☔ <o.6180339+github@gmail.com>
2024-01-25 15:51:03 -0800

    clean up package-manager-controller export

--- packages/cli-core/src/index.ts
+++ packages/cli-core/src/index.ts
@@ -1,4 +1,4 @@
 export * from './prompter/amplify_prompts.js';
 export { COLOR } from './colors.js';
 export * from './printer/printer.js';
-export * from './package-manager-controller/index.js';
+export * from './package-manager-controller/package_manager_controller_factory.js';
--- packages/cli-core/src/package-manager-controller/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './package_manager_controller.js';
-export * from './package_manager_controller_factory.js';
-export * from './npm_package_manager_controller.js';
-export * from './pnpm_package_manager_controller.js';
-export * from './yarn_classic_package_manager_controller.js';
-export * from './yarn_modern_package_manager_controller.js';
