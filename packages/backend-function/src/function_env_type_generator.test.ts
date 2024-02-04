03efd66f4ae1cfab5b03991f66c0782944820e9b
Roshane Pascual <rotp@amazon.com>
2024-02-03 21:54:06 -0800

    add test to import typedef file

--- packages/backend-function/src/function_env_type_generator.test.ts
+++ packages/backend-function/src/function_env_type_generator.test.ts
@@ -1,22 +1,16 @@
-import { beforeEach, describe, it, mock } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import fs from 'fs';
 import { FunctionEnvironmentTypeGenerator } from './function_env_type_generator.js';
 import assert from 'assert';
+import path from 'path';
+import { pathToFileURL } from 'url';
 
-void describe('functionEnvironmentTypeGenerator', () => {
-  const fdCloseMock = mock.fn();
-  const fsOpenSyncMock = mock.method(fs, 'openSync');
-  const fsMkdirSyncMock = mock.method(fs, 'mkdirSync', () => null);
-  const fsWriteFileSyncMock = mock.method(fs, 'writeFileSync', () => null);
-
-  beforeEach(() => {
-    fdCloseMock.mock.resetCalls();
-    fsOpenSyncMock.mock.resetCalls();
-    fsMkdirSyncMock.mock.resetCalls();
-    fsWriteFileSyncMock.mock.resetCalls();
-  });
-
+void describe('FunctionEnvironmentTypeGenerator', () => {
   void it('generates a type definition file', () => {
+    const fdCloseMock = mock.fn();
+    const fsOpenSyncMock = mock.method(fs, 'openSync');
+    const fsMkdirSyncMock = mock.method(fs, 'mkdirSync', () => null);
+    const fsWriteFileSyncMock = mock.method(fs, 'writeFileSync', () => null);
     fsOpenSyncMock.mock.mockImplementation(() => {
       return {
         close: fdCloseMock,
@@ -42,5 +36,25 @@ void describe('functionEnvironmentTypeGenerator', () => {
         ?.toString()
         .includes(sampleStaticEnv)
     );
+
+    mock.restoreAll();
+  });
+
+  void it('generated type definition file has valid syntax', async () => {
+    const targetDirectory = await fs.promises.mkdtemp('func_env_type_gen_test');
+    const functionEnvironmentTypeGenerator =
+      new FunctionEnvironmentTypeGenerator(
+        'testFunction',
+        `${targetDirectory}/handler.ts`
+      );
+    const filePath = path.join(targetDirectory, '/amplify/testFunction_env.ts');
+
+    functionEnvironmentTypeGenerator.generateTypeDefFile();
+
+    assert.doesNotThrow(
+      async () => await import(pathToFileURL(filePath).toString())
+    );
+
+    await fs.promises.rm(targetDirectory, { recursive: true, force: true });
   });
 });
