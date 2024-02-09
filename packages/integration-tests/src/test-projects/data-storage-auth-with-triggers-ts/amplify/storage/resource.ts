719b45c9ac52e5fc6ef148810ee6dd5a1da5ea21
Edward Foyle <foyleef@amazon.com>
2024-02-09 08:54:08 -0800

    fix access

--- packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/amplify/storage/resource.ts
+++ packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/amplify/storage/resource.ts
@@ -2,8 +2,8 @@ import { defineStorage } from '@aws-amplify/backend';
 import { myFunc } from '../function.js';
 
 export const storage = defineStorage({
-  name: 'testName',
+  name: 'public',
   access: (allow) => ({
-    public: [allow.resource(myFunc).to('read', 'write')],
+    '/public/*': [allow.resource(myFunc).to('read', 'write')],
   }),
 });
