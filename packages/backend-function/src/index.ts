045e9c33c0dfea39fc6f3a811e504343b16bc618
MJ☔ <o.6180339+github@gmail.com>
2024-02-27 11:45:52 -0800

    move re-export to backend-function

--- packages/backend-function/src/index.ts
+++ packages/backend-function/src/index.ts
@@ -1,1 +1,13 @@
 export * from './factory.js';
+
+// re-export trigger types as "<category><trigger>Handler"
+export type {
+  CustomMessageTriggerHandler as AuthCustomMessageHandler,
+  PreAuthenticationTriggerHandler as AuthPreAuthenticationHandler,
+  PreSignUpTriggerHandler as AuthPreSignUpHandler,
+  PostAuthenticationTriggerHandler as AuthPostAuthenticationHandler,
+  PostConfirmationTriggerHandler as AuthPostConfirmationHandler,
+  UserMigrationTriggerHandler as AuthUserMigrationHandler,
+  S3Handler as StorageHandler,
+  AppSyncResolverHandler as DataResolverHandler,
+} from 'aws-lambda';
--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,15 +16,3 @@ export { defineStorage } from '@aws-amplify/backend-storage';
 
 // function
 export { defineFunction } from '@aws-amplify/backend-function';
-
-// re-export trigger types as "<category><trigger>Handler"
-export type {
-  CustomMessageTriggerHandler as AuthCustomMessageHandler,
-  PreAuthenticationTriggerHandler as AuthPreAuthenticationHandler,
-  PreSignUpTriggerHandler as AuthPreSignUpHandler,
-  PostAuthenticationTriggerHandler as AuthPostAuthenticationHandler,
-  PostConfirmationTriggerHandler as AuthPostConfirmationHandler,
-  UserMigrationTriggerHandler as AuthUserMigrationHandler,
-  S3Handler as StorageHandler,
-  AppSyncResolverHandler as DataResolverHandler,
-} from 'aws-lambda';
