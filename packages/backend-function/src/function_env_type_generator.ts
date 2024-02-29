cec91d5bc1fb94574050e6e2bfde8d87008baf58
Roshane Pascual <rtpascual@users.noreply.github.com>
2024-02-28 17:02:09 -0800

    add dynamic environment variables to function type definition files (#1076)
    * add dynamic environment variables to function type definition files
    
    * add comments
    
    * rename method and move file generation to
    
    * group env vars into logical chunks
    
    * nit fixes
    
    * fix to get all defined env variables
    
    * move tsconfig options to test project dir
    
    * update update_tsconfig_refs script
    
    * rename DefinedEnvVars, nit fixes, and expanded on comments

--- packages/backend-function/src/function_env_type_generator.ts
+++ packages/backend-function/src/function_env_type_generator.ts
@@ -1,25 +1,33 @@
 import fs from 'fs';
 import { staticEnvironmentVariables } from './static_env_types.js';
 import path from 'path';
-import os from 'os';
+import { EOL } from 'os';
 
 /**
- * Generates a type definition file for environment variables
+ * Generates a typed process.env shim for environment variables
  */
 export class FunctionEnvironmentTypeGenerator {
   private typeDefFilePath: string;
 
   /**
-   * Initialize type definition file name and location
+   * Initialize typed process.env shim file name and location
    */
-  constructor(functionName: string) {
-    this.typeDefFilePath = `${process.cwd()}/.amplify/function-env/${functionName}.ts`;
+  constructor(
+    private readonly functionName: string,
+    private readonly amplifyBackendEnvVars: string[] = []
+  ) {
+    this.typeDefFilePath = `${process.cwd()}/.amplify/function-env/${
+      this.functionName
+    }.ts`;
   }
 
   /**
-   * Generate a type definition file
+   * Generate a typed process.env shim
    */
-  generateTypeDefFile() {
+  generateTypedProcessEnvShim() {
+    const lambdaEnvVarTypeName = 'LambdaProvidedEnvVars';
+    const amplifyBackendEnvVarTypeName = 'AmplifyBackendEnvVars';
+
     const declarations = [];
     const typeDefFileDirname = path.dirname(this.typeDefFilePath);
 
@@ -27,18 +35,34 @@ export class FunctionEnvironmentTypeGenerator {
       fs.mkdirSync(typeDefFileDirname, { recursive: true });
     }
 
+    // Add Lambda runtime environment variables to the typed shim
+    declarations.push(`type ${lambdaEnvVarTypeName} = {`);
     for (const key in staticEnvironmentVariables) {
       const comment = `/** ${staticEnvironmentVariables[key]} */`;
       const declaration = `${key}: string;`;
 
-      declarations.push(comment + os.EOL + declaration);
+      declarations.push(comment + EOL + declaration + EOL);
     }
+    declarations.push(`};${EOL}`);
+
+    /**
+     * Add Amplify backend environment variables to the typed shim which can be either of the following:
+     * 1. Defined by the customer passing env vars to the environment parameter for defineFunction
+     * 2. Defined by resource access mechanisms
+     */
+    declarations.push(`type ${amplifyBackendEnvVarTypeName} = {`);
+    this.amplifyBackendEnvVars.forEach((envName) => {
+      const declaration = `${envName}: string;`;
+
+      declarations.push(declaration);
+    });
+    declarations.push(`};${EOL}`);
 
     const content =
-      `/** Lambda runtime environment variables, see https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime */${os.EOL}` +
-      `export const env = process.env as {${os.EOL}` +
-      declarations.join(os.EOL + os.EOL) +
-      `${os.EOL}};`;
+      `/** Lambda runtime environment variables, see https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html#configuration-envvars-runtime */${EOL}` +
+      `export const env = process.env as ${lambdaEnvVarTypeName} & ${amplifyBackendEnvVarTypeName};${EOL}${EOL}${declarations.join(
+        EOL
+      )}`;
 
     fs.writeFileSync(this.typeDefFilePath, content);
   }
