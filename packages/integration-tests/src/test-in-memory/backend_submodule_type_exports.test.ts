dc948dde7cba0e82e95a5c3e703ef54620708268
Edward Foyle <foyleef@amazon.com>
2024-03-11 10:17:16 -0700

    add build-time check for submodule exports

---
+++ packages/integration-tests/src/test-in-memory/backend_submodule_type_exports.test.ts
@@ -0,0 +1,23 @@
+/**
+ * If this "test" builds, then it means that these types are available in the types/platform submodule export from @aws-amplify/backend
+ */
+// prettier-ignore-start
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  AuthCfnResources,
+  AuthResources,
+  AuthRoleName,
+  BackendOutputEntry,
+  BackendOutputStorageStrategy,
+  BackendSecretResolver,
+  ConstructContainer,
+  ConstructContainerEntryGenerator,
+  ConstructFactory,
+  ConstructFactoryGetInstanceProps,
+  FunctionResources,
+  GenerateContainerEntryProps,
+  ImportPathVerifier,
+  ResourceProvider,
+  SsmEnvironmentEntriesGenerator,
+  SsmEnvironmentEntry,
+} from '@aws-amplify/backend/types/platform';
