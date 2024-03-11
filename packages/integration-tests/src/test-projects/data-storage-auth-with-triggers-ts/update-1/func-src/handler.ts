bce16f276a93ad6bdcd28011a0da0ae8fd07fc3a
Edward Foyle <foyleef@amazon.com>
2024-03-11 13:42:11 -0700

    fix ts ignore

--- packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/update-1/func-src/handler.ts
+++ packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/update-1/func-src/handler.ts
@@ -1,4 +1,6 @@
-// @ts-expect-error Ignoring TS here because this code will be hotswapped in for the original handler code. The destination location contains the response_generator dependency
+// we have to use ts-ignore instead of ts-expect-error because when the tsc check as part of the deployment runs, there will no longer be an error
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore Ignoring TS here because this code will be hotswapped in for the original handler code. The destination location contains the response_generator dependency
 import { getResponse } from './response_generator.js';
 
 /**
