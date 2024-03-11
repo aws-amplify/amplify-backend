2bf18ad7c2a729577f42abdac0e83058f54adc27
Edward Foyle <foyleef@amazon.com>
2024-03-11 10:00:38 -0700

    scope down exports

--- packages/backend/src/types/platform.ts
+++ packages/backend/src/types/platform.ts
@@ -1,1 +1,21 @@
-export * from '@aws-amplify/plugin-types';
+/**
+ * Subset of types exported from @aws-amplify/plugin-types that are useful when building additional abstractions around the `define*` functions.
+ */
+export {
+  AuthCfnResources,
+  AuthResources,
+  FunctionResources,
+  AuthRoleName,
+  ConstructFactory,
+  ResourceProvider,
+  ConstructFactoryGetInstanceProps,
+  ConstructContainer,
+  ConstructContainerEntryGenerator,
+  GenerateContainerEntryProps,
+  BackendSecretResolver,
+  SsmEnvironmentEntriesGenerator,
+  BackendOutputStorageStrategy,
+  BackendOutputEntry,
+  ImportPathVerifier,
+  SsmEnvironmentEntry,
+} from '@aws-amplify/plugin-types';
