20c2e91cbd2a7d5fab8c15fd7dedb3a9729920e0
Edward Foyle <foyleef@amazon.com>
2024-03-12 13:34:09 -0700

    update unit tests

--- packages/cli-core/src/package-manager-controller/npm_package_manager_controller.test.ts
+++ packages/cli-core/src/package-manager-controller/npm_package_manager_controller.test.ts
@@ -143,7 +143,8 @@ void describe('NpmPackageManagerController', () => {
         existsSyncMock
       );
       await npmPackageManagerController.initializeTsConfig('./amplify');
-      assert.equal(executeWithDebugLoggerMock.mock.callCount(), 1);
+      assert.equal(executeWithDebugLoggerMock.mock.callCount(), 0);
+      assert.equal(fspMock.writeFile.mock.callCount(), 1);
     });
   });
 });
