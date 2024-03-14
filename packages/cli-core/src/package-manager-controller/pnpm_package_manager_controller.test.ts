e0ae60cd39c4e5d2eeb0a77681b289ae2a6bdd9c
Edward Foyle <foyleef@amazon.com>
2024-03-14 16:42:54 -0700

    initialize tsconfig from template rather than IPC call (#1140)

--- packages/cli-core/src/package-manager-controller/pnpm_package_manager_controller.test.ts
+++ packages/cli-core/src/package-manager-controller/pnpm_package_manager_controller.test.ts
@@ -143,7 +143,8 @@ void describe('PnpmPackageManagerController', () => {
         existsSyncMock
       );
       await pnpmPackageManagerController.initializeTsConfig('./amplify');
-      assert.equal(executeWithDebugLoggerMock.mock.callCount(), 1);
+      assert.equal(executeWithDebugLoggerMock.mock.callCount(), 0);
+      assert.equal(fspMock.writeFile.mock.callCount(), 1);
     });
   });
 });
