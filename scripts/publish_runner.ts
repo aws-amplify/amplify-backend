0e7edcb86b88bf67ff7d38041176389829297940
Kamil Sobol <sobkamil@amazon.com>
2024-03-15 12:36:33 -0700

    iteration

--- scripts/publish_runner.ts
+++ scripts/publish_runner.ts
@@ -1,23 +1,39 @@
 import { Options, execa } from 'execa';
 
-const publishDefaults = {
+export type PublishOptions = {
   /**
    * Publish defaults to creating git tags for the packages being published.
    * Set false to disable this behavior
    */
-  includeGitTags: true,
+  includeGitTags: boolean;
   /**
    * Defaults to publishing to the public npm registry
    * Set true to publish to the local registry
    */
+  useLocalRegistry: boolean;
+  /**
+   * Defaults to publishing a usual release.
+   * Set true to publish a snapshot.
+   * See https://github.com/changesets/changesets/blob/main/docs/snapshot-releases.md
+   */
+  snapshotRelease?: boolean;
+  /**
+   * A release tag. Required for snapshot releases.
+   */
+  tag?: string;
+};
+
+const publishDefaults: PublishOptions = {
+  includeGitTags: true,
   useLocalRegistry: false,
+  snapshotRelease: false,
 };
 /**
  * Wrapper around `changeset publish` that exposes a few config options
  * To keep behavior consistent, this wrapper should be the ONLY path by which we execute `changeset publish`
  */
-export const runPublish = async (props?: Partial<typeof publishDefaults>) => {
-  const { includeGitTags, useLocalRegistry } = {
+export const runPublish = async (props?: PublishOptions) => {
+  const { includeGitTags, useLocalRegistry, snapshotRelease, tag } = {
     ...publishDefaults,
     ...props,
   };
