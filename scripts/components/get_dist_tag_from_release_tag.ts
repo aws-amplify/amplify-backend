84275f14b43027690a02dd49597f2bf53dc97a0f
Edward Foyle <foyleef@amazon.com>
2024-04-04 18:46:41 -0700

    remove unused code

--- scripts/components/get_dist_tag_from_release_tag.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Given a string like '@aws-amplify/auth-construct-alpha@0.6.0-beta.8', this regex will grab 'beta' from the string.
- * For a non-pre release tag like @aws-amplify/backend-cli@0.10.0, this regex will not match and we default to the 'latest' tag.
- */
-export const getDistTagFromReleaseTag = (releaseTag: string) => {
-  const distTagMatch = releaseTag.match(/\.\d+-(?<distTag>[\w\-.]+)\.\d+$/);
-  return distTagMatch?.groups?.distTag ?? 'latest';
-};
