cab0bf3dcb8f598116f83301d3f8efeb391d9dd3
Edward Foyle <foyleef@amazon.com>
2024-04-16 13:41:06 -0700

    add docs link to getting started template

--- packages/create-amplify/templates/basic-auth-data/amplify/backend.ts
+++ packages/create-amplify/templates/basic-auth-data/amplify/backend.ts
@@ -2,6 +2,9 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
+/**
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions and more.
+ */
 defineBackend({
   auth,
   data,
