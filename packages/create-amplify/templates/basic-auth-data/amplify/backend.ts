11959a7a15324ead1389e9a0cea9832b407e2158
Amplifiyer <51211245+Amplifiyer@users.noreply.github.com>
2024-04-30 17:57:46 +0200

    chore: remove unused assignment in create_amplify template (#1410)

--- packages/create-amplify/templates/basic-auth-data/amplify/backend.ts
+++ packages/create-amplify/templates/basic-auth-data/amplify/backend.ts
@@ -5,7 +5,7 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
 });
