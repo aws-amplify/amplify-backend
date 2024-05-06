a723bf4f1570a9e424e39d8460615153c3afb0bc
Charles Shin <shin.charles11@gmail.com>
2024-05-06 14:44:54 -0700

    feat: node compatibility test (#1416)
    * feat: node compatibility test in CI
    
    * use numbers instead of nvm format
    
    * update cache key with node version
    
    * add inputs to restore actions
    
    * removing v2 suffix
    
    * try removing install cache

--- packages/integration-tests/src/test-project-setup/access_testing_project.ts
+++ packages/integration-tests/src/test-project-setup/access_testing_project.ts
@@ -43,12 +43,18 @@ import crypto from 'node:crypto';
 import { gql } from 'graphql-tag';
 import { IamCredentials } from '../types.js';
 import { AmplifyAuthCredentialsFactory } from '../amplify_auth_credentials_factory.js';
+import { SemVer } from 'semver';
 
 // TODO: this is a work around
 // it seems like as of amplify v6 , some of the code only runs in the browser ...
 // see https://github.com/aws-amplify/amplify-js/issues/12751
-// @ts-expect-error altering typing for global to make compiler happy is not worth the effort assuming this is temporary workaround
-globalThis.crypto = crypto;
+if (process.versions.node) {
+  // node >= 20 now exposes crypto by default. This workaround is not needed: https://github.com/nodejs/node/pull/42083
+  if (new SemVer(process.versions.node).major < 20) {
+    // @ts-expect-error altering typing for global to make compiler happy is not worth the effort assuming this is temporary workaround
+    globalThis.crypto = crypto;
+  }
+}
 
 /**
  * Creates access testing projects with typescript idioms.
