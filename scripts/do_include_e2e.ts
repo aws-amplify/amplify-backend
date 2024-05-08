3b22981f1cc8d238bc29ebb28a84bb09e58edc63
Edward Foyle <foyleef@amazon.com>
2024-05-08 10:19:48 -0700

    refactor run e2e check and only run on version packages commits to hotfix

---
+++ scripts/do_include_e2e.ts
@@ -0,0 +1,37 @@
+import { context as ghContext } from '@actions/github';
+import { isVersionPackagesCommit } from './components/is_version_packages_commit.js';
+import { GithubClient } from './components/github_client.js';
+
+const { GITHUB_EVENT_NAME: eventName, GITHUB_REF_NAME: refName } = process.env;
+
+const gitHubClient = new GithubClient();
+
+const prHasRunE2ELabel = async () => {
+  if (!ghContext.payload.pull_request) {
+    // event is not a pull request
+    return false;
+  }
+  const prInfo = await gitHubClient.fetchPullRequest(
+    ghContext.payload.pull_request.number
+  );
+  const hasRunE2ELabel = prInfo.labels.some(
+    (label) => label.name === 'run-e2e'
+  );
+  return hasRunE2ELabel;
+};
+
+// e2e tests run if...
+const doIncludeE2e =
+  await // this workflow is running on a push to a branch besides hotfix
+  ((eventName === 'push' && refName !== 'hotfix') ||
+    // this workflow is running on a version packages commit to hotfix
+    (eventName === 'push' &&
+      refName === 'hotfix' &&
+      isVersionPackagesCommit()) ||
+    // this workflow was triggered manually
+    eventName === 'workflow_dispatch' ||
+    // this workflow is running on a PR with the 'run-e2e' label
+    (await prHasRunE2ELabel()));
+
+// print a true/false of whether e2e tests should run
+console.log(doIncludeE2e);
--- scripts/is_version_packages_commit.ts
+++ scripts/is_version_packages_commit.ts
@@ -1,24 +1,7 @@
-import { readFile } from 'fs/promises';
-import { PushEvent } from '@octokit/webhooks-types';
+import { isVersionPackagesCommit } from './components/is_version_packages_commit.js';
 
 /*
-Reads the GitHub webhook event payload from the specified file path
-Returns a true/false of whether the push event contains one and only one version bump commit
-The result is returned in a form that is intended to be piped to GITHUB_OUTPUT
-See https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+Prints a true/false of whether the push event is a version packages commit
 */
 
-const githubPushEventPayloadPath = process.argv[2];
-
-const githubPushEventPayload = JSON.parse(
-  await readFile(githubPushEventPayloadPath, 'utf-8')
-) as PushEvent;
-
-const isVersionPackagesCommit =
-  githubPushEventPayload.commits.length === 1 &&
-  githubPushEventPayload.commits[0].author.name.includes(
-    'github-actions[bot]'
-  ) &&
-  githubPushEventPayload.commits[0].message.includes('Version Packages');
-
-console.log(`is_version_packages_commit=${isVersionPackagesCommit}`);
+console.log(await isVersionPackagesCommit());
