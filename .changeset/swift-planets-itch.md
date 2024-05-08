6eeffb9175b1d06579ec10ca1b222230279b6c39
magisystem0408 <masato.matsudo@gmail.com>
2024-05-09 05:26:57 +0900

    fix: refactoring text Amplify (Gen 2) to Amplify Gen 2

---
+++ .changeset/swift-planets-itch.md
@@ -0,0 +1,6 @@
+---
+'@aws-amplify/integration-tests': patch
+'create-amplify': patch
+---
+
+fix: refactoring text Amplify (Gen 2) to Amplify Gen 2
--- packages/create-amplify/src/amplify_project_creator.test.ts
+++ packages/create-amplify/src/amplify_project_creator.test.ts
@@ -92,7 +92,7 @@ void describe('AmplifyProjectCreator', () => {
     assert.equal(
       logSpy.mock.calls[14].arguments[0],
       grey(
-        `Amplify (Gen 2) collects anonymous telemetry data about general usage of the CLI. Participation is optional, and you may opt-out by using ${cyan(
+        `Amplify Gen 2 collects anonymous telemetry data about general usage of the CLI. Participation is optional, and you may opt-out by using ${cyan(
           'npx ampx configure telemetry disable'
         )}. To learn more about telemetry, visit ${underline(
           blue('https://docs.amplify.aws/gen2/reference/telemetry')
@@ -137,7 +137,7 @@ void describe('AmplifyProjectCreator', () => {
     assert.equal(
       logSpy.mock.calls[14].arguments[0],
       grey(
-        `Amplify (Gen 2) collects anonymous telemetry data about general usage of the CLI. Participation is optional, and you may opt-out by using ${cyan(
+        `Amplify Gen 2 collects anonymous telemetry data about general usage of the CLI. Participation is optional, and you may opt-out by using ${cyan(
           'npx ampx configure telemetry disable'
         )}. To learn more about telemetry, visit ${underline(
           blue('https://docs.amplify.aws/gen2/reference/telemetry')
