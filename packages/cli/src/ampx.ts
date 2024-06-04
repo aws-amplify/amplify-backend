d3e93e7e938cb839c524fb5ed2f041e3e03cc50f
fangyuwu7 <wufangy372@gamil.com>
2024-06-04 16:25:00 -0700

    add a comment

--- packages/cli/src/ampx.ts
+++ packages/cli/src/ampx.ts
@@ -37,12 +37,10 @@ const parser = createMainParser(libraryVersion, usageDataEmitter);
 
 try {
   await parser.parseAsync(hideBin(process.argv));
+  //Yargs invoke the command failure handler before rethrowing the error.This prevents it from propagating to unhandled exception handler and being printed again.
 } catch (e) {
   if (e instanceof Error) {
-    printer.log(
-      format.error('Failed to parse command line arguments'),
-      LogLevel.DEBUG
-    );
+    printer.log(format.error('Failed to execute command'), LogLevel.DEBUG);
     printer.log(format.error(e), LogLevel.DEBUG);
     if (e.stack) {
       printer.log(e.stack, LogLevel.DEBUG);
