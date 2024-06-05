21f06f5be46007ee9ac886d6d1537ae2eb4e0586
Kamil Sobol <sobkamil@amazon.com>
2024-06-05 14:37:09 -0700

    add comment here

--- packages/cli/src/commands/sandbox/sandbox-seed/sandbox_seed_command.ts
+++ packages/cli/src/commands/sandbox/sandbox-seed/sandbox_seed_command.ts
@@ -29,6 +29,13 @@ export class SandboxSeedCommand implements CommandModule<object> {
    * @inheritDoc
    */
   handler = async (): Promise<void> => {
+    // TODO alternative considered:
+    // Other option was to have seed.ts export a function but it causes the following problems:
+    // 1. In order to load it dynamically it must be compiled to js
+    // 2. It may import backend.ts for dynamic typing, that import makes TSC compile whole graph
+    // 3. Pointing tsc to seed.ts makes it hard/impossible to properly honor tsconfig.json (same problems we had when discussing what to do with tsconfig.json in amplify folder.
+    //
+    // Therefore the POC pivots here to make the script self contained and executable with tsx.
     const seedPath = path.join('amplify', 'seed.ts');
     await execa('tsx', [seedPath], { cwd: process.cwd(), stdio: 'inherit' });
     return;
