88364810ad0236a62d47b3da8397186c3a62a349
Kamil Sobol <sobkamil@amazon.com>
2024-06-05 15:27:59 -0700

    read sandbox client config

--- packages/backend-seed/src/index.ts
+++ packages/backend-seed/src/index.ts
@@ -1,3 +1,8 @@
+import { generateClientConfig } from '@aws-amplify/client-config';
+import { SandboxBackendIdResolver } from './sandbox_id_resolver.js';
+import { LocalNamespaceResolver } from './local_namespace_resolver.js';
+import { PackageJsonReader } from '@aws-amplify/platform-core';
+
 export type SeedFunction = () => Promise<void>;
 
 const seedFunctions: Array<SeedFunction> = [];
@@ -7,6 +12,11 @@ export const defineSeed = (seedFunction: SeedFunction) => {
 };
 
 process.once('beforeExit', async () => {
+  const backendId = await new SandboxBackendIdResolver(
+    new LocalNamespaceResolver(new PackageJsonReader())
+  ).resolve();
+  const clientConfig = await generateClientConfig(backendId, '1');
+  console.log(clientConfig);
   for (const seedFunction of seedFunctions) {
     await seedFunction();
   }
