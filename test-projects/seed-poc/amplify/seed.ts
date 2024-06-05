a9ce68dd24ddf1b453ec250ae9a8c2f90cc38056
Kamil Sobol <sobkamil@amazon.com>
2024-06-05 14:45:42 -0700

    add comment here

--- test-projects/seed-poc/amplify/seed.ts
+++ test-projects/seed-poc/amplify/seed.ts
@@ -1,3 +1,5 @@
+// TODO: importing as type doesn't trigger synthesis, otherwise it does and it's failing.
+// Synthesis is not needed for seed, seed depends on post-deployment artifacts (client config).
 import type { backend } from './backend';
 
 let foo: typeof backend;
