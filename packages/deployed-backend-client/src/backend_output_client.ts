85130e3eb69bd0445d2a8e6311d7274b40408f00
MJ☔ <o.6180339+github@gmail.com>
2024-07-16 17:37:18 -0700

    refactor: backend_output_client

--- packages/deployed-backend-client/src/backend_output_client.ts
+++ packages/deployed-backend-client/src/backend_output_client.ts
@@ -5,6 +5,7 @@ import { AmplifyUserError } from '@aws-amplify/platform-core';
 import { BackendOutputFetcherFactory } from './backend_output_fetcher_factory.js';
 import { DeployedBackendIdentifier } from './index.js';
 import { BackendOutputClient } from './backend_output_client_factory.js';
+import { BackendOutput } from '@aws-amplify/plugin-types';
 
 /**
  * Simplifies the retrieval of all backend output values
@@ -25,49 +26,59 @@ export class DefaultBackendOutputClient implements BackendOutputClient {
     const output = await outputFetcher.fetchBackendOutput();
 
     if (output['AWS::Amplify::Storage']) {
-      const payload = output['AWS::Amplify::Storage'].payload;
-      const buckets: {
-        bucketName: string;
-        storageRegion: string;
-      }[] = [];
-      const bucketNames = Object.keys(payload).filter((key) =>
-        key.startsWith('bucketName')
-      );
-      const hasDefaultBucket = bucketNames.includes('bucketName');
-      if (!hasDefaultBucket && bucketNames.length > 1) {
-        throw new AmplifyUserError('NoDefaultBucketError', {
-          message: 'No default bucket set in the Amplify project.',
-          resolution:
-            'Add `isDefault: true` to one of the buckets `defineStorage` in the Amplify project.',
-        });
-      }
-      bucketNames.forEach((key) => {
-        if (!key.startsWith('bucketName')) {
-          return;
-        }
-        const postfix = key.replace('bucketName', '');
-        const bucketName = payload[`bucketName${postfix}`];
-        const storageRegion = payload[`storageRegion${postfix}`];
-        buckets.push({ bucketName, storageRegion });
-        if (postfix) {
-          delete payload[`bucketName${postfix}`];
-          delete payload[`storageRegion${postfix}`];
-          if (!hasDefaultBucket && bucketNames.length === 1) {
-            payload.bucketName = bucketName;
-            payload.storageRegion = storageRegion;
-          }
-        }
-      });
-
-      return unifiedBackendOutputSchema.parse({
-        ...output,
-        'AWS::Amplify::Storage': {
-          ...output['AWS::Amplify::Storage'],
-          payload: { ...payload, buckets },
-        },
-      });
+      return this.parseStorageOutput(output);
     }
 
     return unifiedBackendOutputSchema.parse(output);
   };
+
+  /**
+   * Parses the storage output from the backend output.
+   * @param output The backend output.
+   * @returns The parsed storage output.
+   */
+  private parseStorageOutput(output: BackendOutput) {
+    const payload = output['AWS::Amplify::Storage'].payload;
+    const buckets: {
+      bucketName: string;
+      storageRegion: string;
+    }[] = [];
+    const bucketNames = Object.keys(payload).filter((key) =>
+      key.startsWith('bucketName')
+    );
+    const hasDefaultBucket = bucketNames.includes('bucketName');
+    if (!hasDefaultBucket && bucketNames.length > 1) {
+      throw new AmplifyUserError('NoDefaultBucketError', {
+        message: 'No default bucket set in the Amplify project.',
+        resolution:
+          'Add `isDefault: true` to one of the buckets `defineStorage` in the Amplify project.',
+      });
+    }
+
+    bucketNames.forEach((key) => {
+      if (!key.startsWith('bucketName')) {
+        return;
+      }
+      const postfix = key.replace('bucketName', '');
+      const bucketName = payload[`bucketName${postfix}`];
+      const storageRegion = payload[`storageRegion${postfix}`];
+      buckets.push({ bucketName, storageRegion });
+      if (postfix) {
+        delete payload[`bucketName${postfix}`];
+        delete payload[`storageRegion${postfix}`];
+        if (!hasDefaultBucket && bucketNames.length === 1) {
+          payload.bucketName = bucketName;
+          payload.storageRegion = storageRegion;
+        }
+      }
+    });
+
+    return unifiedBackendOutputSchema.parse({
+      ...output,
+      'AWS::Amplify::Storage': {
+        ...output['AWS::Amplify::Storage'],
+        payload: { ...payload, buckets },
+      },
+    });
+  }
 }
