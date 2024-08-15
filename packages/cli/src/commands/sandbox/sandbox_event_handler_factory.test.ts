d31132fe9af1d6a7f74123f6b33c05e2dc5e3246
MJ☔ <o.6180339+github@gmail.com>
2024-08-15 09:48:58 -0700

    fix: more unit test

--- packages/cli/src/commands/sandbox/sandbox_event_handler_factory.test.ts
+++ packages/cli/src/commands/sandbox/sandbox_event_handler_factory.test.ts
@@ -73,7 +73,7 @@ void describe('sandbox_event_handler_factory', () => {
         namespace: 'test',
         name: 'name',
       },
-      '1',
+      '1.1',
       'test-out',
       'json',
     ]);
@@ -185,7 +185,7 @@ void describe('sandbox_event_handler_factory', () => {
         namespace: 'test',
         name: 'name',
       },
-      '1',
+      '1.1',
       'test-out',
       'json',
     ]);
