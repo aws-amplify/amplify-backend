0135b3c6cf67403f17920b31ac522219032b3957
Kamil Sobol <sobkamil@amazon.com>
2024-09-03 15:34:06 -0700

    handle usage of subnamespaced types in api check (#1952)

--- scripts/components/api-changes-validator/usage_statemets_renderer.ts
+++ scripts/components/api-changes-validator/usage_statemets_renderer.ts
@@ -69,7 +69,7 @@ export class UsageStatementsRenderer {
 
       // characters that can be found before or after symbol
       // this is to prevent partial matches in case one symbol's characters are subset of longer one
-      const symbolTerminators = '[\\s\\,\\(\\)<>]';
+      const symbolTerminators = '[\\s\\,\\(\\)<>;]';
       const regex = new RegExp(
         `(${symbolTerminators})(${symbolName})(${symbolTerminators})`,
         'g'
