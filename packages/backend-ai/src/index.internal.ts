eb17b03b620f4094658b27f48a067c524bc71bf8
Kamil Sobol <sobkamil@amazon.com>
2024-09-03 11:22:21 -0700

    grrr

--- packages/backend-ai/src/index.internal.ts
+++ packages/backend-ai/src/index.internal.ts
@@ -2,7 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import * as __export__conversation from './conversation/index.js';
 // eslint-disable-next-line @typescript-eslint/naming-convention
-import * as __export__conversation_runtime from './conversation/runtime/index.js';
+import * as __export__conversation__runtime from './conversation/runtime/index.js';
 
 /*
  Api-extractor does not ([yet](https://github.com/microsoft/rushstack/issues/1596)) support multiple package entry points
@@ -10,4 +10,4 @@ import * as __export__conversation_runtime from './conversation/runtime/index.js
  This allows api-extractor to pick up the submodule exports in its analysis
  */
 
-export { __export__conversation, __export__conversation_runtime };
+export { __export__conversation, __export__conversation__runtime };
