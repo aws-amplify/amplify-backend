4ccd7bd7053942261c1aaf8b82571fcdf105bf32
Leena <81589006+ShadowCat567@users.noreply.github.com>
2024-09-06 18:22:34 -0400

    Added new lint rules for AmplifyUserError and AmplifyFault to restrict where they should be used (#1960)
    * chore: added new lint rules, but no specification for what they should act on
    
    * chore: new rules disabled globally
    
    * chore: added controls to turn on new rules in auth-construct and cli packages
    
    * chore: changeset update
    
    * chore: changeset added for cli and auth-construct
    
    * chore: added tests for new lint rules
    
    * chore: working on updating how rule is applied
    
    * chore: moved selective rule activations
    
    * chore: added comment about updating off to error in prefer-amplify-errors
    
    * chore: change set update
    
    * removed regex, ignore now happens with overrides
    
    * changeset is now empty
    
    * removed checkNode - unnecessary
    
    ---------
    
    Co-authored-by: Vieltojarvi <lvielto@amazon.com>

--- packages/eslint-rules/src/index.ts
+++ packages/eslint-rules/src/index.ts
@@ -1,9 +1,13 @@
 import { noEmptyCatchRule } from './rules/no_empty_catch.js';
 import { amplifyErrorNameRule } from './rules/amplify_error_name.js';
+import { preferAmplifyErrorsRule } from './rules/prefer_amplify_errors.js';
+import { noAmplifyErrors } from './rules/no_amplify_errors.js';
 
 export const rules: Record<string, unknown> = {
   'amplify-error-name': amplifyErrorNameRule,
   'no-empty-catch': noEmptyCatchRule,
+  'prefer-amplify-errors': preferAmplifyErrorsRule,
+  'no-amplify-errors': noAmplifyErrors,
 };
 
 export const configs = {
@@ -12,6 +16,23 @@ export const configs = {
     rules: {
       'amplify-backend-rules/amplify-error-name': 'error',
       'amplify-backend-rules/no-empty-catch': 'error',
+      'amplify-backend-rules/prefer-amplify-errors': 'off',
+      'amplify-backend-rules/no-amplify-errors': 'off',
     },
+    overrides: [
+      {
+        files: ['packages/cli/src/**'],
+        excludedFiles: ['**/*.test.ts'],
+        rules: {
+          'amplify-backend-rules/prefer-amplify-errors': 'off', //will be changed to 'error' in the future
+        },
+      },
+      {
+        files: ['packages/auth-construct/src/**'],
+        rules: {
+          'amplify-backend-rules/no-amplify-errors': 'error',
+        },
+      },
+    ],
   },
 };
