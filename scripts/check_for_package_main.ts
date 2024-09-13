540f2b44e46ce8c1c8591f6b989440f17fb961fa
Vieltojarvi <lvielto@amazon.com>
2024-09-13 14:14:12 -0700

    updated error message for script to check for main field

--- scripts/check_for_package_main.ts
+++ scripts/check_for_package_main.ts
@@ -3,14 +3,20 @@ import { readPackageJson } from './components/package-json/package_json.js';
 
 const packagePaths = await glob('./packages/*');
 
+const errors: string[] = [];
 for (const packagePath of packagePaths) {
   const { main, name, isPrivate } = await readPackageJson(packagePath);
   if (isPrivate || name === '@aws-amplify/integration-tests') {
     continue;
   }
   if (!main) {
-    throw new Error(
+    errors.push(
       `Expected package ${name} to have field main, but main is missing.`
     );
   }
+}
+if (errors.length > 0) {
+  const errorMessagePrefix =
+    'Packages were found to be missing main fields.\nAdd main field to package.json of the following packages:';
+  throw new Error(`${errorMessagePrefix}\n ${errors.join('\n ')}`);
 }
