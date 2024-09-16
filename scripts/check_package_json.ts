e648e8e8f5fb6e7056fb41ae6f2c0cc0954b1ea3
Leena <81589006+ShadowCat567@users.noreply.github.com>
2024-09-16 12:11:44 -0400

    Updates to packages unresolvable by no-extraneous-dependencies (#1997)
    * added main field to packages that lack it
    
    * added changeset
    
    * added custom script for verifying packages have main, added main to 9 packages
    
    * updated error message for script to check for main field
    
    * added changeset
    
    * fixed private problem, changed file name
    
    * added ignore for packages with violations, included in lint check
    
    * Update scripts/check_package_json.ts
    
    Co-authored-by: Kamil Sobol <sobol.k.r@gmail.com>
    
    ---------
    
    Co-authored-by: Vieltojarvi <lvielto@amazon.com>
    Co-authored-by: Kamil Sobol <sobol.k.r@gmail.com>

---
+++ scripts/check_package_json.ts
@@ -0,0 +1,31 @@
+import { glob } from 'glob';
+import { readPackageJson } from './components/package-json/package_json.js';
+
+const packagePaths = await glob('./packages/*');
+
+// Excluding packages that violate 'import/no-extraneous-dependencies' rule until they're fixed.
+const tempExclude: string[] = [
+  '@aws-amplify/plugin-types',
+  '@aws-amplify/cli-core',
+];
+const errors: string[] = [];
+for (const packagePath of packagePaths) {
+  const {
+    main,
+    name,
+    private: privatePackage,
+  } = await readPackageJson(packagePath);
+  if (privatePackage || tempExclude.includes(name)) {
+    continue;
+  }
+  if (!main) {
+    errors.push(
+      `Expected package ${name} to have field main, but main is missing.`
+    );
+  }
+}
+if (errors.length > 0) {
+  const errorMessagePrefix =
+    'Packages were found to be missing main fields.\nAdd main field to package.json of the following packages:';
+  throw new Error(`${errorMessagePrefix}\n ${errors.join('\n ')}`);
+}
