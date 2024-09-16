8dd7286bf1a870e69766963e872fad4add68bc84
Leena <81589006+ShadowCat567@users.noreply.github.com>
2024-09-16 14:30:11 -0400

    Fix for cli-core so it is no longer unresolvable (#2003)
    * fixed cli-core and plugin-types violations
    
    * added changeset
    
    * plugin-types cannot have a main field because it cannot export functional code
    
    * cli-core removed from excluded packages
    
    * comment for plugin-types exclusion from check for main
    
    ---------
    
    Co-authored-by: Vieltojarvi <lvielto@amazon.com>

--- packages/cli-core/src/package-manager-controller/yarn_modern_package_manager_controller.ts
+++ packages/cli-core/src/package-manager-controller/yarn_modern_package_manager_controller.ts
@@ -2,7 +2,8 @@ import { existsSync as _existsSync } from 'fs';
 import _fsp from 'fs/promises';
 import { execa as _execa } from 'execa';
 import * as _path from 'path';
-import { LogLevel, Printer, format } from '@aws-amplify/cli-core';
+import { LogLevel, Printer } from '../printer/printer.js';
+import { format } from '../format/format.js';
 import { executeWithDebugLogger as _executeWithDebugLogger } from './execute_with_debugger_logger.js';
 import { PackageManagerControllerBase } from './package_manager_controller_base.js';
 
