70c6c580bf31cdd8be53c62333052a2fffe4d30f
Vieltojarvi <lvielto@amazon.com>
2024-09-18 12:29:50 -0700

    trying to make api happy

--- packages/backend-auth/src/index.ts
+++ packages/backend-auth/src/index.ts
@@ -1,2 +1,7 @@
-export { BackendAuth, AmplifyAuthProps, defineAuth } from './factory.js';
+export {
+  BackendAuth,
+  AmplifyAuthProps,
+  AuthStackFactory,
+  defineAuth,
+} from './factory.js';
 export * from './types.js';
