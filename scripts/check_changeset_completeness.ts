58f2e01716312bc2d8e32a0d2f949219dc037baf
Vieltojarvi <lvielto@amazon.com>
2024-09-24 09:10:36 -0700

    moved missing changesets check to function

--- scripts/check_changeset_completeness.ts
+++ scripts/check_changeset_completeness.ts
@@ -11,7 +11,16 @@ enum VersionTypeEnum {
   'MAJOR' = 3,
 }
 
-const getModifiedPackages = (changedFiles: string[]): Set<string> => {
+const checkForMissingChangesets = async (
+  releasePlan: ReleasePlan,
+  gitClient: GitClient,
+  baseRef: string
+) => {
+  const packagesWithChangeset = new Set(
+    releasePlan.releases.map((release) => release.name)
+  );
+
+  const changedFiles = await gitClient.getChangedFiles(baseRef);
   const modifiedPackageDirs = new Set<string>();
 
   changedFiles
@@ -24,7 +33,26 @@ const getModifiedPackages = (changedFiles: string[]): Set<string> => {
         changedPackageFile.split('/').slice(0, 2).join('/')
       );
     });
-  return modifiedPackageDirs;
+
+  const packagesMissingChangesets = [];
+  for (const modifiedPackageDir of modifiedPackageDirs) {
+    const { name: modifiedPackageName, private: isPrivate } =
+      await readPackageJson(modifiedPackageDir);
+    if (isPrivate) {
+      continue;
+    }
+    if (!packagesWithChangeset.has(modifiedPackageName)) {
+      packagesMissingChangesets.push(modifiedPackageName);
+    }
+  }
+
+  if (packagesMissingChangesets.length > 0) {
+    throw new Error(
+      `The following packages have changes but are not included in any changeset:${EOL}${EOL}${packagesMissingChangesets.join(
+        EOL
+      )}${EOL}${EOL}Add a changeset using 'npx changeset add'.`
+    );
+  }
 };
 
 const convertVersionType = (version: VersionType): VersionTypeEnum => {
@@ -105,32 +133,5 @@ if (baseRef === undefined) {
 
 const releasePlan = await getReleasePlan(process.cwd());
 
-const packagesWithChangeset = new Set(
-  releasePlan.releases.map((release) => release.name)
-);
-
-const changedFiles = await gitClient.getChangedFiles(baseRef);
-
-const modifiedPackageDirs = getModifiedPackages(changedFiles);
-
-const packagesMissingChangesets = [];
-for (const modifiedPackageDir of modifiedPackageDirs) {
-  const { name: modifiedPackageName, private: isPrivate } =
-    await readPackageJson(modifiedPackageDir);
-  if (isPrivate) {
-    continue;
-  }
-  if (!packagesWithChangeset.has(modifiedPackageName)) {
-    packagesMissingChangesets.push(modifiedPackageName);
-  }
-}
-
-if (packagesMissingChangesets.length > 0) {
-  throw new Error(
-    `The following packages have changes but are not included in any changeset:${EOL}${EOL}${packagesMissingChangesets.join(
-      EOL
-    )}${EOL}${EOL}Add a changeset using 'npx changeset add'.`
-  );
-}
-
+await checkForMissingChangesets(releasePlan, gitClient, baseRef);
 checkBackendDependenciesVersion(releasePlan);
