dbbb9b8610a65632c296b2a93f9116cdb923ce98
vigy02 <vigy@amazon.com>
2024-10-02 15:06:11 -0700

    feat: adding custom lambda function trigger for email

--- packages/auth-construct/src/test-assets/default-lambda/handler.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Hello world lambda used for testing defaults to `defineFunction`
- */
-export const handler = async () => {
-  return 'hello world lambda';
-};
--- packages/auth-construct/src/test-assets/default-lambda/resource.ts
+++
@@ -1,7 +0,0 @@
-import { defineFunction } from '../../factory.js';
-
-/**
- * Because the defineFunction() defaults depend on a specific file convention,
- * we are defining a test asset here where the directory structure can be controlled
- */
-export const defaultLambda = defineFunction();
