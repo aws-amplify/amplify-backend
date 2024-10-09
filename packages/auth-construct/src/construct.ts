872f88b27c0979d01626f75e1172a6017ed06cef
vigy02 <vigy@amazon.com>
2024-10-09 12:51:19 -0700

    feat: added a functionality to translate auth-props for custom fucntion email

--- packages/auth-construct/src/construct.ts
+++ packages/auth-construct/src/construct.ts
@@ -154,6 +154,13 @@ export class AmplifyAuth
     const userPoolProps = {
       ...this.computedUserPoolProps,
     };
+
+    this.userPool = new cognito.UserPool(
+      this,
+      `${this.name}UserPool`,
+      userPoolProps
+    );
+
     let customSenderKmsKey: Key | undefined;
     if (
       props.senders?.email &&
@@ -172,12 +179,6 @@ export class AmplifyAuth
       });
       userPoolProps.customSenderKmsKey = customSenderKmsKey;
     }
-
-    this.userPool = new cognito.UserPool(
-      this,
-      `${this.name}UserPool`,
-      userPoolProps
-    );
 
     // UserPool - External Providers (Oauth, SAML, OIDC) and User Pool Domain
     this.providerSetupResult = this.setupExternalProviders(
