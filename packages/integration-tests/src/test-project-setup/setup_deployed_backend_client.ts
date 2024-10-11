fe847e61ea366235072fe3fa7ff79c3eeda3313a
Kamil Sobol <sobkamil@amazon.com>
2024-10-11 12:38:10 -0700

    try this

--- packages/integration-tests/src/test-project-setup/setup_deployed_backend_client.ts
+++ packages/integration-tests/src/test-project-setup/setup_deployed_backend_client.ts
@@ -1,4 +1,11 @@
 import { execa } from 'execa';
+import fsp from 'fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+
+const packageLockPath = fileURLToPath(
+  new URL('../../../../package-lock.json', import.meta.url)
+);
 
 /**
  * Configures package.json for testing the specified project directory with the version of deployed-backend-client on npm
@@ -6,7 +13,16 @@ import { execa } from 'execa';
 export const setupDeployedBackendClient = async (
   projectRootDirPath: string
 ) => {
-  await execa('npm', ['install', '@aws-amplify/deployed-backend-client'], {
-    cwd: projectRootDirPath,
-  });
+  // copy lock file to assure that CDK dependencies don't diverge.
+  await fsp.copyFile(
+    packageLockPath,
+    path.join(projectRootDirPath, 'package-lock.json')
+  );
+  await execa(
+    'npm',
+    ['install', '@aws-amplify/deployed-backend-client@latest'],
+    {
+      cwd: projectRootDirPath,
+    }
+  );
 };
