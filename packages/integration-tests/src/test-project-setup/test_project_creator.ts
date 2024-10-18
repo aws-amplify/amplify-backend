023df171dd63fab840f88ea33baab81a87182c16
Kamil Sobol <sobkamil@amazon.com>
2024-10-18 14:30:44 -0700

    that's not needed

--- packages/integration-tests/src/test-project-setup/test_project_creator.ts
+++ packages/integration-tests/src/test-project-setup/test_project_creator.ts
@@ -4,11 +4,3 @@ export type TestProjectCreator = {
   readonly name: string;
   createProject: (e2eProjectDir: string) => Promise<TestProjectBase>;
 };
-
-/**
- * Generates a list of test projects.
- */
-export const getTestProjectCreators = (): TestProjectCreator[] => {
-  const testProjectCreators: TestProjectCreator[] = [];
-  return testProjectCreators;
-};
