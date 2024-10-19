8a718b22f3b72b659b8abe3e8ea4ff9a0cb8ccb5
Kamil Sobol <sobkamil@amazon.com>
2024-10-18 22:19:37 -0700

    try this

---
+++ scripts/components/sparse_test_matrix_generator.ts
@@ -0,0 +1,92 @@
+// See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/running-variations-of-jobs-in-a-workflow
+import { glob } from 'glob';
+
+type JobMatrix = {
+  include?: Array<Record<string, string>>;
+} & Record<string, string>;
+
+export type SparseTestMatrixGeneratorProps = {
+  testDirectory: string;
+  testGlobPattern: string;
+  maxTestsPerJob: number;
+  dimensions: Record<string, Array<string>>;
+};
+
+/**
+ * Generates a sparse test matrix.
+ *
+ * Sparse matrix is created is such a way that:
+ * 1. Every test is included
+ * 2. Every dimension's value is included
+ * 3. Algorithm avoids cartesian product of dimensions, just minimal subset that uses all values.
+ */
+export class SparseTestMatrixGenerator {
+  /**
+   * Creates sparse test matrix generator.
+   */
+  constructor(private readonly props: SparseTestMatrixGeneratorProps) {
+    if (Object.keys(props.dimensions).length === 0) {
+      throw new Error('At least one dimension is required');
+    }
+  }
+
+  generate = async (): Promise<JobMatrix> => {
+    const tests = await glob(this.props.testGlobPattern, {
+      cwd: this.props.testDirectory,
+    });
+
+    const matrix: JobMatrix = {};
+    matrix.include = [];
+
+    for (const testBatch of this.chunkArray(tests, this.props.maxTestsPerJob)) {
+      const dimensionsIndexes: Record<string, number> = {};
+      const dimensionCoverageComplete: Record<string, boolean> = {};
+
+      Object.keys(this.props.dimensions).forEach((key) => {
+        dimensionsIndexes[key] = 0;
+        dimensionCoverageComplete[key] = false;
+      });
+
+      let allDimensionsComplete = false;
+
+      do {
+        const matrixEntry: Record<string, string> = {};
+        matrixEntry.tests = testBatch.join(' ');
+        Object.keys(this.props.dimensions).forEach((key) => {
+          matrixEntry[key] = this.props.dimensions[key][dimensionsIndexes[key]];
+        });
+        matrixEntry.testPaths = testBatch
+          .map((test) => `${this.props.testDirectory}/${test}`)
+          .join(' ');
+        matrix.include?.push(matrixEntry);
+
+        Object.keys(this.props.dimensions).forEach((key) => {
+          dimensionsIndexes[key]++;
+          if (dimensionsIndexes[key] === this.props.dimensions[key].length) {
+            // mark dimension as complete and start the cycle from start until all dimensions are used.
+            dimensionCoverageComplete[key] = true;
+            dimensionsIndexes[key] = 0;
+          }
+        });
+
+        // check if all dimensions are processed.
+        allDimensionsComplete = Object.keys(this.props.dimensions).reduce(
+          (acc, key) => {
+            return acc && dimensionCoverageComplete[key];
+          },
+          true
+        );
+      } while (!allDimensionsComplete);
+    }
+
+    return matrix;
+  };
+
+  private chunkArray = <T>(array: Array<T>, chunkSize: number) => {
+    const result: Array<Array<T>> = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+  };
+}
--- scripts/generate_sparse_test_matrix.ts
+++ scripts/generate_sparse_test_matrix.ts
@@ -1,9 +1,4 @@
-import { glob } from 'glob';
-
-// See https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/running-variations-of-jobs-in-a-workflow
-type JobMatrix = {
-  include?: Array<Record<string, string>>;
-} & Record<string, string>;
+import { SparseTestMatrixGenerator } from './components/sparse_test_matrix_generator.js';
 
 // This script generates a sparse test matrix.
 // Every test must run on each type of OS and each version of node.
@@ -15,54 +10,18 @@ if (process.argv.length < 4) {
   );
 }
 
-const directory = process.argv[2];
+const testDirectory = process.argv[2];
 const testGlobPattern = process.argv[3];
 const maxTestsPerJob = process.argv[4] ? parseInt(process.argv[4]) : 2;
 
-const tests = await glob(testGlobPattern, {
-  cwd: directory,
-});
-
-const matrix: JobMatrix = {};
-matrix.include = [];
-
-// eslint-disable-next-line spellcheck/spell-checker
-const osKinds = ['ubuntu-latest', 'macos-14-xlarge', 'windows-latest'];
-const nodeVersions = ['18', '20'];
-
-const chunks = <T>(array: Array<T>, chunkSize: number) => {
-  const result: Array<Array<T>> = [];
-  for (let i = 0; i < array.length; i += chunkSize) {
-    result.push(array.slice(i, i + chunkSize));
-  }
-  return result;
-};
-
-for (const testBatch of chunks(tests, maxTestsPerJob)) {
-  let allOsCovered = false;
-  let allNodeVersionsCovered = false;
-  let osIndex = 0;
-  let nodeVersionIndex = 0;
-
-  do {
-    matrix.include?.push({
-      tests: testBatch.join(' '),
-      'node-version': nodeVersions[nodeVersionIndex],
-      os: osKinds[osIndex],
-      testPaths: testBatch.map((test) => `${directory}/${test}`).join(' '),
-    });
-
-    osIndex++;
-    nodeVersionIndex++;
-    if (osIndex === osKinds.length) {
-      osIndex = 0;
-      allOsCovered = true;
-    }
-    if (nodeVersionIndex === nodeVersions.length) {
-      nodeVersionIndex = 0;
-      allNodeVersionsCovered = true;
-    }
-  } while (!allOsCovered || !allNodeVersionsCovered);
-}
+const matrix = await new SparseTestMatrixGenerator({
+  testDirectory,
+  testGlobPattern,
+  maxTestsPerJob,
+  dimensions: {
+    'node-version': ['18', '20'],
+    os: ['ubuntu-latest', 'macos-14-xlarge', 'windows-latest'],
+  },
+}).generate();
 
 console.log(JSON.stringify(matrix));
