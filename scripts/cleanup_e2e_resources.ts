e72cfb14e4932229896822d803743c710faa9753
Kamil Sobol <sobkamil@amazon.com>
2024-10-23 06:45:49 -0700

    condition

--- scripts/cleanup_e2e_resources.ts
+++ scripts/cleanup_e2e_resources.ts
@@ -584,9 +584,10 @@ const listAllStaleTestLogGroups = async (): Promise<Array<LogGroup>> => {
     listLogGroupsResponse.logGroups
       ?.filter(
         (logGroup) =>
-          // some log groups have form /aws/lambda/<prefix><rest>
-          // some have <prefix><rest>
-          logGroup.logGroupName?.includes(TEST_AMPLIFY_RESOURCE_PREFIX) &&
+          (logGroup.logGroupName?.startsWith(TEST_AMPLIFY_RESOURCE_PREFIX) ||
+            logGroup.logGroupName?.startsWith(
+              `/aws/lambda/${TEST_AMPLIFY_RESOURCE_PREFIX}`
+            )) &&
           isLogGroupStale(logGroup)
       )
       .forEach((item) => {
