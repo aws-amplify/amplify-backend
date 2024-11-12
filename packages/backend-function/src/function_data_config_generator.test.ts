ac424a57c4b34c5c40b02d547bd3a8957a88b4ce
Aaron S <stocaaro@stocad.com>
2024-11-12 13:46:44 -0600

    Is it the file location?

--- packages/backend-function/src/function_data_config_generator.test.ts
+++ packages/backend-function/src/function_data_config_generator.test.ts
@@ -1,8 +1,10 @@
 import { describe, it, mock } from 'node:test';
 import fs from 'fs';
+import fsp from 'fs/promises';
 import { FunctionDataConfigGenerator } from './function_data_config_generator.js';
 import assert from 'assert';
 import { pathToFileURL } from 'url';
+import { FunctionEnvironmentTypeGenerator } from './function_env_type_generator.js';
 
 void describe('FunctionDataConfigGenerator', () => {
   void it('generates a type definition file', () => {
@@ -33,15 +35,29 @@ void describe('FunctionDataConfigGenerator', () => {
     mock.restoreAll();
   });
 
-  void it('generated data configuration file has valid syntax', async () => {
-    const functionDataConfigGenerator = new FunctionDataConfigGenerator(
-      'testFunction'
-    );
-    const filePath = `${process.cwd()}/.amplify/generated/data-config/testFunction.ts`;
+  // void it('generated data configuration file has valid syntax', async () => {
+  //   const functionDataConfigGenerator = new FunctionDataConfigGenerator(
+  //     'testFunction'
+  //   );
+  //   const filePath = `${process.cwd()}/.amplify/generated/data-config/testFunction.ts`;
 
-    functionDataConfigGenerator.generateDataConfigShim();
+  //   functionDataConfigGenerator.generateDataConfigShim();
 
-    // import to validate syntax of data config file
+  //   // import to validate syntax of data config file
+  //   await import(pathToFileURL(filePath).toString());
+  // });
+
+  void it('generated type definition file has valid syntax', async () => {
+    const targetDirectory = await fsp.mkdtemp('func_env_type_gen_test');
+    const functionEnvironmentTypeGenerator =
+      new FunctionEnvironmentTypeGenerator('testFunction');
+    const filePath = `${process.cwd()}/.amplify/generated/env/testFunction.ts`;
+
+    functionEnvironmentTypeGenerator.generateTypedProcessEnvShim(['TEST_ENV']);
+
+    // import to validate syntax of type definition file
     await import(pathToFileURL(filePath).toString());
+
+    await fsp.rm(targetDirectory, { recursive: true, force: true });
   });
 });
