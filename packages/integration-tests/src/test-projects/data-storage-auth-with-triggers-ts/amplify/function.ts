3bd782d2040c40f13bd0c664e5c72a0808ef16f2
Kamil Sobol <sobkamil@amazon.com>
2024-11-26 14:11:35 -0800

    try this

--- packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/amplify/function.ts
+++ packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/amplify/function.ts
@@ -28,9 +28,9 @@ export const node16Func = defineFunction({
 
 export const onDelete = defineFunction({
   name: 'onDelete',
-  entry: './handler_storage_triggers.ts',
+  entry: './func-src/handler_storage_triggers.ts',
 });
 export const onUpload = defineFunction({
   name: 'onUpload',
-  entry: './handler_storage_triggers.ts',
+  entry: './func-src/handler_storage_triggers.ts',
 });
