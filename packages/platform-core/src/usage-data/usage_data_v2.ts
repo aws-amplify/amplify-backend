65b3566ec5cbfe6bc6606cfd4f056c09053ef33b
vigy02 <vigy@amazon.com>
2024-12-05 15:22:35 -0800

    Added types for new version of usage data

---
+++ packages/platform-core/src/usage-data/usage_data_v2.ts
@@ -0,0 +1,70 @@
+// Core Identifiers
+export type CoreIdentifiersDetails = {
+  sessionUuid: string;
+  timestamp: string;
+  localProjectId: string;
+  accountId?: string;
+  payloadVersion: string;
+  awsRegion: string;
+};
+
+// Command Details
+export type CommandDetails = {
+  success: boolean;
+  command: {
+    path: string[];
+    parameters: string[];
+  };
+  executionTime: number;
+};
+
+// Environment Details
+export type EnvironmentDetails = {
+  osRelease: string;
+  shell: string;
+  npmUserAgent: string;
+  ci: boolean;
+  memory: {
+    total: number;
+    free: number;
+  };
+};
+
+// Latency (Optional)
+export type Latency = {
+  init?: number;
+  synthesis?: number;
+  deployment?: number;
+  hotSwap?: number;
+};
+
+// Error Details (Optional)
+export type ErrorDetails = {
+  primary: {
+    name: string;
+    message: string;
+  };
+  cause?: {
+    name: string;
+    message: string;
+    methodName: string;
+    file: string;
+    lineNumber: number;
+    columnNumber: string;
+  };
+};
+
+// Project Details
+export type ProjectDetails = {
+  dependencies: Array<{ name: string; version: string }>;
+};
+
+// Main Telemetry Data Structure
+export type AmplifyCliTelemetryData = {
+  identifiers: CoreIdentifiersDetails;
+  command: CommandDetails;
+  environment: EnvironmentDetails;
+  project: ProjectDetails;
+  latency?: Latency;
+  error?: ErrorDetails;
+};
