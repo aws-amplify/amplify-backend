9ba1ad060b2581f821e73994ff03fbf7535c5c6e
Blake Dunson <bdnsn@amazon.com>
2024-12-16 22:05:47 +0000

    fix select

--- scripts/select_e2e_test_account.ts
+++ scripts/select_e2e_test_account.ts
@@ -1,11 +1,7 @@
 if (!process.env.E2E_TEST_ACCOUNTS) {
-  throw new Error(
-    'E2E_TEST_ACCOUNTS environment variable must be defined and contain array of strings with account numbers'
-  );
+  console.log("061039800489")
+} else {
+  const accounts = JSON.parse(process.env.E2E_TEST_ACCOUNTS);
+  const selectedAccount = accounts[Math.floor(Math.random() * accounts.length)];
+  console.log(selectedAccount);
 }
-
-const accounts = JSON.parse(process.env.E2E_TEST_ACCOUNTS);
-
-//const selectedAccount = accounts[Math.floor(Math.random() * accounts.length)];
-const selectedAccount = "061039800489";
-console.log(selectedAccount);
