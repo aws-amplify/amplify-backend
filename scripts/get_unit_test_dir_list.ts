486b8b1802a0482e743ae2938b04bf82f0a60c12
Kamil Sobol <sobkamil@amazon.com>
2024-12-17 07:54:15 -0800

    this

--- scripts/get_unit_test_dir_list.ts
+++ scripts/get_unit_test_dir_list.ts
@@ -6,4 +6,5 @@ result = result.filter((result) => !result.includes('integration-tests'));
 result.push(
   path.join('packages', 'integration-tests', 'lib', 'test-in-memory')
 );
+result = result.map((path) => `${path}/**/*.test.js`);
 console.log(result.join(' '));
