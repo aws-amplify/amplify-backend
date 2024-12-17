9b90d779e02b989253edea712d09675ca69848e5
Vieltojarvi <lvielto@amazon.com>
2024-12-17 13:40:09 -0800

    most things but backendId

--- packages/backend-seed/src/index.ts
+++ packages/backend-seed/src/index.ts
@@ -1,7 +1,15 @@
-/*import { Secret } from '@aws-amplify/backend-secret';
+//import { BackendIdentifier } from '@aws-amplify/plugin-types';
+import { getSecretClientWithAmplifyErrorHandling } from '@aws-amplify/backend-secret';
 
-//ultimatly want the API to look something like this: getSecret(secretID)
-export const getSecret = (secretName: string): Promise<Secret> => {
-    //somehow need to get the secret from the secret manager
+//ultimately want the API to look something like this: getSecret(secretID)
+//eslint-disable-next-line jsdoc/require-description
+/**
+ *
+ */
+export const getSecret = async (secretName: string): Promise<string> => {
+  //need to get access to the BackendIdentifier
+  const backendId = new BackendIdResolver();
+  const secretClient = getSecretClientWithAmplifyErrorHandling();
+  const secret = await secretClient.getSecret(backendId, { name: secretName });
+  return secret.value;
 };
-*/
