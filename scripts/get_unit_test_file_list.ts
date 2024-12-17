476231dcb0ff2374dcee49d05718feece5fbde76
Kamil Sobol <sobkamil@amazon.com>
2024-12-17 14:46:26 -0800

    this

--- scripts/get_unit_test_file_list.ts
+++ scripts/get_unit_test_file_list.ts
@@ -1,12 +1,12 @@
 import { glob } from 'glob';
 
 let result = await glob('packages/**/*.test.js', {
-  absolute: true,
+  posix: true,
 });
 result = result.filter((result) => !result.includes('integration-tests'));
 result.push(
   ...(await glob('packages/integration-tests/lib/test-in-memory/**/*.test.js', {
-    absolute: true,
+    posix: true,
   }))
 );
 
