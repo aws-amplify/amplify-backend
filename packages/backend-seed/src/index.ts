5f94ddab173899544233817955577674ab3467ea
Vieltojarvi <lvielto@amazon.com>
2024-12-17 09:31:00 -0800

    set up for seed secrets

---
+++ packages/backend-seed/src/index.ts
@@ -0,0 +1,7 @@
+/*import { Secret } from '@aws-amplify/backend-secret';
+
+//ultimatly want the API to look something like this: getSecret(secretID)
+export const getSecret = (secretName: string): Promise<Secret> => {
+    //somehow need to get the secret from the secret manager
+};
+*/
