c38fb30f5f7a1c7b54dbf3d99be6c79e0803c967
Vieltojarvi <lvielto@amazon.com>
2024-12-19 10:06:33 -0800

    adjustments

--- packages/backend-seed/src/get_secret_seed.ts
+++ packages/backend-seed/src/get_secret_seed.ts
@@ -6,12 +6,12 @@ import { getSecretClientWithAmplifyErrorHandling } from '@aws-amplify/backend-se
  */
 export const GetSeedSecret = async (secretName: string): Promise<string> => {
   const serializedBackendId = process.env.SANDBOX_IDENTIFIER;
-  if (serializedBackendId === undefined) {
+  if (!serializedBackendId) {
     throw new Error(
       'SANDBOX_IDENTIFIER is undefined. Have you run ampx sandbox seed yet?'
     );
   }
-  const backendId = JSON.parse(process.env.SANDBOX_IDENTIFIER!);
+  const backendId = JSON.parse(serializedBackendId);
   const secretClient = getSecretClientWithAmplifyErrorHandling();
   const secret = await secretClient.getSecret(backendId, { name: secretName });
   return secret.value;
