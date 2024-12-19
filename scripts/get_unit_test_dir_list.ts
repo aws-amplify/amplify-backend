36ca26a96a71b6480c50019559a41927a3d447ec
Kamil Sobol <sobkamil@amazon.com>
2024-12-19 08:03:13 -0800

    try this

--- scripts/get_unit_test_dir_list.ts
+++ scripts/get_unit_test_dir_list.ts
@@ -1,9 +1,18 @@
 import { glob } from 'glob';
 import * as path from 'path';
+import semver from 'semver';
 
 let result = await glob('packages/*');
 result = result.filter((result) => !result.includes('integration-tests'));
 result.push(
   path.join('packages', 'integration-tests', 'lib', 'test-in-memory')
 );
+
+const nodeVersion = semver.parse(process.versions.node);
+if (nodeVersion && nodeVersion.major >= 21) {
+  // Starting from version 21. Node test runner's cli changed how search pattern works.
+  // See https://github.com/nodejs/node/issues/50219.
+  result = result.map((p) => `${p}/**/*.test.?(c|m)js`);
+}
+
 console.log(result.join(' '));
