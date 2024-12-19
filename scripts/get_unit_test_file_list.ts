74f707cd3cbd5c9aa7edfc112a93a85a6cd87c9a
Kamil Sobol <sobkamil@amazon.com>
2024-12-19 07:24:57 -0800

    Revert "this"
    This reverts commit c68ed7c67f0a672a3c638614f4e3b1d67ffce0fa.

--- scripts/get_unit_test_file_list.ts
+++
@@ -1,9 +0,0 @@
-import { glob } from 'glob';
-
-let result = await glob('packages/**/*.test.js');
-result = result.filter((result) => !result.includes('integration-tests'));
-result.push(
-  ...(await glob('packages/integration-tests/lib/test-in-memory/**/*.test.js'))
-);
-
-console.log(result.join(' '));
