25974511431025e4a39dbfb0ab896854be6d1120
Roshane Pascual <rotp@amazon.com>
2024-12-20 15:14:12 -0800

    PR feedback

--- scripts/dependabot_handle_version_update.ts
+++ scripts/dependabot_handle_version_update.ts
@@ -1,16 +1,18 @@
+import { context as ghContext } from '@actions/github';
 import fsp from 'fs/promises';
 import { EOL } from 'os';
 import { GitClient } from './components/git_client.js';
 import { readPackageJson } from './components/package-json/package_json.js';
+import { GithubClient } from './components/github_client.js';
 
 const createChangesetFile = async (
-  versionUpdates: Map<string, string>,
+  versionUpdates: string[],
   packageNames: string[]
 ) => {
   let message = '';
 
-  for (const [dep, version] of versionUpdates) {
-    message += `Update dependency ${dep} to ${version}.${EOL}`;
+  for (const update of versionUpdates) {
+    message += `${update}${EOL}`;
   }
 
   const frontmatterContent = packageNames
@@ -20,30 +22,31 @@ const createChangesetFile = async (
   await fsp.writeFile(fileName, body);
 };
 
-const getVersionUpdates = async (files: string[]) => {
-  const updates = new Map<string, string>();
-  for (const file of files) {
-    const changes = await gitClient.getFileChanges(file);
-    for (const change of changes.split(EOL)) {
-      if (!change.startsWith('+ ')) {
-        continue;
-      }
-      // This will capture lines in git diff like:
-      // +  "<dependency>": "^<version>"
-      const match = change.match(/"(.*)": "(.*)"/);
+const getVersionUpdates = async () => {
+  const updates: string[] = [];
+  const prBody = ghContext.payload.pull_request?.body;
 
-      if (!match) {
-        continue;
-      }
-      // Add dependency name (match[1]) and new version (match[2]) to Map
-      updates.set(match[1].replace(/"/g, ''), match[2].replace(/"/g, ''));
-    }
+  // Match lines in PR body that are one of the following:
+  // Updates `<dependency>` from <old-version> to <new-version>
+  // Bumps [<dependency>](<dependency-link>) from <old-version> to <new-version>.
+  const matches = prBody?.match(
+    /(Updates|Bumps) (.*) from [0-9.]+ to [0-9.]+/g
+  );
+
+  for (const match of matches ?? []) {
+    updates.push(match);
   }
 
   return updates;
 };
 
+if (!ghContext.payload.pull_request) {
+  // event is not a pull request, return early
+  process.exit();
+}
+
 const gitClient = new GitClient();
+const ghClient = new GithubClient();
 
 const branch = await gitClient.getCurrentBranch();
 if (!branch.startsWith('dependabot/')) {
@@ -56,38 +59,40 @@ if (baseRef === undefined) {
   throw new Error('No base ref specified for generate changeset check');
 }
 
-const headRef = process.argv[3].trim();
+const headRef = process.argv[3];
 if (headRef === undefined) {
   throw new Error('No head ref specified for generate changeset check');
 }
 
 const changedFiles = await gitClient.getChangedFiles(baseRef);
+if (changedFiles.find((file) => file.startsWith('.changeset'))) {
+  // if changeset file already exists, return early
+  process.exit();
+}
+
 const modifiedPackageDirs = new Set<string>();
 
-// Get modified package.json files that we care about for changesets
 const packageJsonFiles = changedFiles.filter(
   (changedFile) =>
-    changedFile.startsWith('packages/') &&
-    !['packages/integration-tests', 'packages/eslint-rules'].some(
-      (packageName) => changedFile.startsWith(packageName)
-    ) &&
-    changedFile.endsWith('package.json')
+    changedFile.startsWith('packages/') && changedFile.endsWith('package.json')
 );
-
 packageJsonFiles.forEach((changedPackageFile) => {
   modifiedPackageDirs.add(changedPackageFile.split('/').slice(0, 2).join('/'));
 });
 
 const packageNames = [];
 for (const modifiedPackageDir of modifiedPackageDirs) {
-  const { name: modifiedPackageName } = await readPackageJson(
-    modifiedPackageDir
-  );
-  packageNames.push(modifiedPackageName);
+  const packageJson = await readPackageJson(modifiedPackageDir);
+  if (!packageJson.private) {
+    packageNames.push(packageJson.name);
+  }
 }
 
 const fileName = `.changeset/dependabot-${headRef}.md`;
-const versionUpdates = await getVersionUpdates(packageJsonFiles);
+const versionUpdates = await getVersionUpdates();
 await createChangesetFile(versionUpdates, packageNames);
 await gitClient.commitAllChanges('add changeset');
+await ghClient.labelPullRequest(ghContext.payload.pull_request.number, [
+  'run-e2e',
+]);
 await gitClient.push({ force: true });
