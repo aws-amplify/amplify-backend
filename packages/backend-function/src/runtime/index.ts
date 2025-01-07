bfc87a4503790655d970773593c84a71e5a5efcb
Aaron S <stocaaro@stocad.com>
2025-01-07 11:34:33 -0600

    fix(backend-function): Remove env type guards and use plain specific types

--- packages/backend-function/src/runtime/index.ts
+++ packages/backend-function/src/runtime/index.ts
@@ -2,6 +2,9 @@ export {
   getAmplifyDataClientConfig,
   DataClientConfig,
   DataClientEnv,
+  DataClientError,
+  DataClientReturn,
+  InvalidConfig,
   LibraryOptions,
   ResourceConfig,
 } from './get_amplify_clients_configuration.js';
