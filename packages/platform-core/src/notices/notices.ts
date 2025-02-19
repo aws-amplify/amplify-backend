eb8b9d4354a665d5bdd60396a7e0562e2c3b0cae
Kamil Sobol <5849952+sobolk@users.noreply.github.com>
2025-02-19 09:22:30 -0800

    validator

--- packages/platform-core/src/notices/notices.ts
+++ packages/platform-core/src/notices/notices.ts
@@ -1,9 +1,22 @@
 import z from 'zod';
 
+const noticePredicateSchema = z.discriminatedUnion('type', [
+  z.object({
+    type: z.literal('packageVersion'),
+    packageName: z.string(),
+    versionRange: z.string(),
+  }),
+  z.object({
+    type: z.literal('backendComponent'),
+    backendComponent: z.enum(['data', 'auth', 'function', 'storage']),
+  }),
+]);
+
 export const noticeSchema = z.object({
   title: z.string(),
   details: z.string(),
   link: z.string(),
+  predicates: z.array(noticePredicateSchema),
 });
 
 export type Notice = z.infer<typeof noticeSchema>;
--- packages/platform-core/src/notices/notices_manifest_validator.ts
+++ packages/platform-core/src/notices/notices_manifest_validator.ts
@@ -1,5 +1,6 @@
 import { Notice, NoticesManifest } from './notices';
 import assert from 'node:assert';
+import semver from 'semver';
 
 export type NoticesManifestValidatorProps = {
   checkLinksWithGitHubApi?: boolean;
@@ -41,6 +42,27 @@ export class NoticesManifestValidator {
         `https://api.github.com/repos/aws-amplify/amplify-backend/issues/${issueNumber}`
       );
       assert.ok(response.ok);
+    }
+
+    for (const predicate of notice.predicates) {
+      switch (predicate.type) {
+        case 'backendComponent':
+          assert.ok(
+            predicate.backendComponent,
+            'Backend component predicate must have backend component'
+          );
+          break;
+        case 'packageVersion':
+          assert.ok(
+            predicate.packageName,
+            'Package version predicate must have package name'
+          );
+          assert.ok(
+            semver.validRange(predicate.versionRange),
+            'Package version predicate must have a valid version range'
+          );
+          break;
+      }
     }
   };
 }
