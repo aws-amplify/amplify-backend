07c7e432153628ffb834bfbc05e678343e2ce966
Roshane Pascual <rtpascual@users.noreply.github.com>
2025-02-26 13:33:09 -0800

    pin cdk versions for create amplify (#2535)
    * pin cdk versions for create amplify
    
    * change to json and refactor update script to be run as git hook
    
    * add check for create amplify deps
    
    * fix test
    
    * import json instead of using fsp
    
    * run for prod packages

---
+++ scripts/components/create_amplify_dep_updater.ts
@@ -0,0 +1,76 @@
+import fsp from 'fs/promises';
+import path from 'path';
+import { Dependency } from './get_dependencies_from_package_lock.js';
+
+/**
+ * Modifies target dependencies used for create amplify
+ */
+export const createAmplifyDepUpdater = async (
+  dependencies: Dependency[],
+  createAmplifyDepsToFilter: string[] = ['aws-cdk', 'aws-cdk-lib']
+) => {
+  const targetDependencies: Dependency[] = dependencies.filter((dependency) =>
+    createAmplifyDepsToFilter.includes(dependency.name)
+  );
+
+  if (targetDependencies.length === 0) {
+    return;
+  }
+
+  const defaultPackagesPath = path.join(
+    process.cwd(),
+    'packages/create-amplify/src/default_packages.json'
+  );
+
+  const dependenciesToUpdate = new Map(
+    targetDependencies.map((dep) => [dep.name, dep.version])
+  );
+  const defaultPackagesContent = JSON.parse(
+    await fsp.readFile(defaultPackagesPath, 'utf-8')
+  );
+  const defaultDevPackages: string[] =
+    defaultPackagesContent.defaultDevPackages;
+  const defaultProdPackages: string[] =
+    defaultPackagesContent.defaultProdPackages;
+  let update = false; // keeps track if any create amplify dep was updated
+
+  // create new array of dev packages with possible updates
+  const newDevPackages = defaultDevPackages.map((depString) => {
+    if (!depString.includes('@')) {
+      return depString;
+    }
+
+    const [depName] = depString.split('@');
+    const expectedDepString = `${depName}@${dependenciesToUpdate.get(depName)}`;
+    const hasDepName = dependenciesToUpdate.has(depName);
+    update = update || (hasDepName && depString !== expectedDepString);
+    return dependenciesToUpdate.has(depName) ? expectedDepString : depString;
+  });
+
+  // create new array of prod packages with possible updates
+  const newProdPackages = defaultProdPackages.map((depString) => {
+    if (!depString.includes('@')) {
+      return depString;
+    }
+
+    const [depName] = depString.split('@');
+    const expectedDepString = `${depName}@${dependenciesToUpdate.get(depName)}`;
+    const hasDepName = dependenciesToUpdate.has(depName);
+    update = update || (hasDepName && depString !== expectedDepString);
+    return dependenciesToUpdate.has(depName) ? expectedDepString : depString;
+  });
+
+  if (update) {
+    await fsp.writeFile(
+      defaultPackagesPath,
+      JSON.stringify(
+        {
+          defaultDevPackages: newDevPackages,
+          defaultProdPackages: newProdPackages,
+        },
+        null,
+        2
+      )
+    );
+  }
+};
