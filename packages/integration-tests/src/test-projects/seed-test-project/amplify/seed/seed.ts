b1dcca96ca575d2d1449e3c3c15cb76537302b09
Vieltojarvi <lvielto@amazon.com>
2025-03-05 10:48:10 -0800

    crpyto polyfill in e2e test seed script

--- packages/integration-tests/src/test-projects/seed-test-project/amplify/seed/seed.ts
+++ packages/integration-tests/src/test-projects/seed-test-project/amplify/seed/seed.ts
@@ -5,6 +5,19 @@ import { Amplify } from 'aws-amplify';
 // @ts-ignore typescript does not like this import
 import outputs from '../../amplify_outputs.json';
 import { generateClient } from 'aws-amplify/api';
+import { SemVer } from 'semver';
+import crypto from 'node:crypto';
+
+// TODO: this is a work around - in theory this should be fixed
+// it seems like as of amplify v6 , some of the code only runs in the browser ...
+// see https://github.com/aws-amplify/amplify-js/issues/12751
+if (process.versions.node) {
+  // node >= 20 now exposes crypto by default. This workaround is not needed: https://github.com/nodejs/node/pull/42083
+  if (new SemVer(process.versions.node).major < 20) {
+    // @ts-expect-error altering typing for global to make compiler happy is not worth the effort assuming this is temporary workaround
+    globalThis.crypto = crypto;
+  }
+}
 
 Amplify.configure(outputs);
 
