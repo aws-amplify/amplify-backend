3de5bca7d9e8d5a8b713f1400727154fce160f20
Roshane Pascual <rotp@amazon.com>
2025-03-11 15:37:07 -0700

    polyfill Blob for e2e

--- packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/amplify/func-src/handler_node16.ts
+++ packages/integration-tests/src/test-projects/data-storage-auth-with-triggers-ts/amplify/func-src/handler_node16.ts
@@ -1,9 +1,10 @@
 import { getResponse } from './response_generator.js';
 
-// node 16 does not include fetch, so we need to polyfill it
+// node 16 does not include fetch/Blob, so we need to polyfill it
 // once lambda sunsets node 16 we can remove this dev dependency
-import fetch from 'node-fetch';
+import fetch, { Blob } from 'node-fetch';
 global.fetch = fetch as never;
+global.Blob = Blob as never;
 
 /**
  * Dummy lambda handler to test building a function with a local import
