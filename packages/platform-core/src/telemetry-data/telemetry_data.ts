1c7fda26710cafecbfce1f16cfaf032dd3063af3
Roshane Pascual <rotp@amazon.com>
2025-03-17 14:08:23 -0700

    add telemetry schema

---
+++ packages/platform-core/src/telemetry-data/telemetry_data.ts
@@ -0,0 +1,79 @@
+import z from 'zod';
+
+const identifiersSchema = z.object({
+  payloadVersion: z.string(),
+  sessionUuid: z.string(),
+  eventId: z.string(),
+  timestamp: z.string(),
+  localProjectId: z.string(),
+  accountId: z.string().optional(),
+  awsRegion: z.string().optional(),
+});
+
+const eventSchema = z.object({
+  state: z.enum(['ABORTED', 'FAILED', 'SUCCEEDED']),
+  command: z.object({
+    path: z.array(z.string()),
+    parameters: z.array(z.string()),
+  }),
+});
+
+const environmentSchema = z.object({
+  os: z.object({
+    platform: z.string(),
+    release: z.string(),
+  }),
+  shell: z.string(),
+  npmUserAgent: z.string(),
+  ci: z.boolean(),
+  memory: z.object({
+    total: z.number(),
+    free: z.number(),
+  }),
+});
+
+const projectSchema = z.object({
+  dependencies: z
+    .array(
+      z.object({
+        name: z.string(),
+        version: z.string(),
+      }),
+    )
+    .optional(),
+});
+
+const latencySchema = z.object({
+  total: z.number(),
+  init: z.number().optional(),
+  synthesis: z.number().optional(),
+  deployment: z.number().optional(),
+  hotSwap: z.number().optional(),
+});
+
+export type ErrorDetails = {
+  name: string;
+  message: string;
+  stack: string;
+  cause?: ErrorDetails;
+};
+
+const errorSchema: z.ZodType<ErrorDetails> = z.lazy(() =>
+  z.object({
+    name: z.string(),
+    message: z.string(),
+    stack: z.string(),
+    cause: z.optional(errorSchema), // Recursive reference
+  }),
+);
+
+export const telemetryPayloadSchema = z.object({
+  identifiers: identifiersSchema,
+  event: eventSchema,
+  environment: environmentSchema,
+  project: projectSchema,
+  latency: latencySchema,
+  error: z.optional(errorSchema),
+});
+
+export type TelemetryPayload = z.infer<typeof telemetryPayloadSchema>;
