2857cf3efc9dd6fe3ce91ec738c900cb527e5e97
Amplifiyer <51211245+Amplifiyer@users.noreply.github.com>
2025-03-27 16:37:29 -0400

    refactor type checks to use TS compiler APIs instead of tsc cli (#2632)
    * stash
    
    * stash
    
    * refactor type checks to use TS compiler APIs instead of tsc cli
    
    * fix windows test
    
    * fix test
    
    * increase timeout for check_api_changes
    
    * remove unnecessary test

--- packages/backend-deployer/src/cdk_deployer.ts
+++ packages/backend-deployer/src/cdk_deployer.ts
@@ -1,13 +1,10 @@
-import stream from 'stream';
-import readline from 'readline';
 import process from 'node:process';
+
 import {
   BackendDeployer,
   DeployProps,
-  DeployResult,
-  DestroyResult,
 } from './cdk_deployer_singleton_factory.js';
-import { CDKDeploymentError, CdkErrorMapper } from './cdk_error_mapper.js';
+import { CdkErrorMapper } from './cdk_error_mapper.js';
 import {
   AmplifyIOHost,
   BackendIdentifier,
@@ -15,11 +12,11 @@ import {
 } from '@aws-amplify/plugin-types';
 import {
   AmplifyError,
-  AmplifyUserError,
+  AmplifyFault,
   BackendLocator,
   CDKContextKey,
 } from '@aws-amplify/platform-core';
-import path, { dirname } from 'path';
+import path from 'path';
 import {
   HotswapMode,
   RequireApproval,
@@ -30,6 +27,7 @@ import { tsImport } from 'tsx/esm/api';
 import { CloudAssembly } from 'aws-cdk-lib/cx-api';
 import { pathToFileURL } from 'url';
 import { AssetStaging } from 'aws-cdk-lib/core';
+import { Worker } from 'node:worker_threads';
 
 /**
  * Invokes CDK command via execa
@@ -110,33 +108,33 @@ export class CDKDeployer implements BackendDeployer {
       data: undefined,
     });
 
-    try {
-      await this.invokeTsc(deployProps);
-    } catch (typeError) {
-      if (
-        synthError &&
-        AmplifyError.isAmplifyError(typeError) &&
-        typeError.cause?.message.match(
-          /Cannot find module '\$amplify\/env\/.*' or its corresponding type declarations/,
-        )
-      ) {
-        // synth has failed and we don't have auto generated function environment definition files. This
-        // resulted in the exception caught here, which is not very useful for the customers.
-        // We instead throw the synth error for customers to fix what caused the synth to fail.
-        throw this.cdkErrorMapper.getAmplifyError(synthError, backendId.type);
+    if (deployProps?.validateAppSources) {
+      try {
+        await this.compileProject(path.dirname(this.backendLocator.locate()));
+      } catch (typeError) {
+        if (
+          synthError &&
+          AmplifyError.isAmplifyError(typeError) &&
+          typeError.name === 'FunctionEnvVarFileNotGeneratedError'
+        ) {
+          // synth has failed and we don't have auto generated function environment definition files. This
+          // resulted in the exception caught here, which is not very useful for the customers.
+          // We instead throw the synth error for customers to fix what caused the synth to fail.
+          throw this.cdkErrorMapper.getAmplifyError(synthError, backendId.type);
+        }
+        throw typeError;
+      } finally {
+        const typeCheckTimeSeconds =
+          Math.floor((Date.now() - typeCheckStartTime) / 10) / 100;
+        await this.ioHost.notify({
+          message: `Type checks completed in ${typeCheckTimeSeconds} seconds`,
+          code: 'TS_FINISHED',
+          action: 'amplify',
+          time: new Date(),
+          level: 'result',
+          data: undefined,
+        });
       }
-      throw typeError;
-    } finally {
-      const typeCheckTimeSeconds =
-        Math.floor((Date.now() - typeCheckStartTime) / 10) / 100;
-      await this.ioHost.notify({
-        message: `Type checks completed in ${typeCheckTimeSeconds} seconds`,
-        code: 'TS_FINISHED',
-        action: 'amplify',
-        time: new Date(),
-        level: 'result',
-        data: undefined,
-      });
     }
 
     // If typescript compilation was successful but synth had failed, we throw synth error
@@ -190,73 +188,26 @@ export class CDKDeployer implements BackendDeployer {
     };
   };
 
-  /**
-   * Wrapper for the child process executor. Helps in unit testing as node:test framework
-   * doesn't have capabilities to mock exported functions like `execa` as of right now.
-   */
-  executeCommand = async (
-    commandArgs: string[],
-    options: { redirectStdoutToStderr: boolean } = {
-      redirectStdoutToStderr: false,
-    },
-  ) => {
-    // We let the stdout and stdin inherit and streamed to parent process but pipe
-    // the stderr and use it to throw on failure. This is to prevent actual
-    // actionable errors being hidden among the stdout. Moreover execa errors are
-    // useless when calling CLIs unless you made execa calling error.
-    let aggregatedStderr = '';
-    const aggregatorStderrStream = new stream.Writable();
-    aggregatorStderrStream._write = function (chunk, encoding, done) {
-      aggregatedStderr += chunk;
-      done();
-    };
-    const childProcess = this.packageManagerController.runWithPackageManager(
-      commandArgs,
-      process.cwd(),
-      {
-        stdin: 'inherit',
-        stdout: 'pipe',
-        stderr: 'pipe',
-        // Piping the output by default strips off the color. This is a workaround to
-        // preserve the color being piped to parent process.
-        extendEnv: true,
-        env: { FORCE_COLOR: '1' },
-      },
-    );
-
-    childProcess.stderr?.pipe(aggregatorStderrStream);
-
-    if (options?.redirectStdoutToStderr) {
-      childProcess.stdout?.pipe(aggregatorStderrStream);
-    } else {
-      childProcess.stdout?.pipe(process.stdout);
-    }
-
-    const cdkOutput = { deploymentTimes: {} };
-    if (childProcess.stdout) {
-      await this.populateCDKOutputFromStdout(cdkOutput, childProcess.stdout);
-    }
-
-    try {
-      await childProcess;
-      return cdkOutput;
-      // eslint-disable-next-line amplify-backend-rules/propagate-error-cause
-    } catch (error) {
-      // swallow execa error if the cdk cli ran and produced some stderr.
-      // Most of the time this error is noise(basically child exited with exit code...)
-      // bubbling this up to customers add confusion (Customers don't need to know we are running IPC calls
-      // and their exit codes printed while sandbox continue to run). Hence we explicitly don't pass error in the cause
-      // rather throw the entire stderr for clients to figure out what to do with it.
-      // However if the cdk process didn't run or produced no output, then we have nothing to go on with. So we throw
-      // this error to aid in some debugging.
-      if (aggregatedStderr.trim()) {
-        // If the string is more than 65KB, truncate and keep the last portion.
-        // eslint-disable-next-line amplify-backend-rules/prefer-amplify-errors
-        throw new Error(this.truncateString(aggregatedStderr, 65000));
-      } else {
-        throw error;
-      }
-    }
+  compileProject = (projectDirectory: string): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(new URL('ts_compiler.js', import.meta.url), {
+        workerData: { projectDirectory },
+      });
+      worker.on('message', () => {
+        // do nothing
+      });
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          reject(
+            new AmplifyFault('TSCompilerWorkerFault', {
+              message: `Worker stopped with exit code ${code}`,
+            }),
+          );
+        }
+        resolve();
+      });
+    });
   };
 
   /**
@@ -295,79 +246,5 @@ export class CDKDeployer implements BackendDeployer {
       },
       { context: contextParams, outdir: this.absoluteCloudAssemblyLocation },
     );
-  };
-
-  private truncateString = (str: string, size: number) => {
-    const encoder = new TextEncoder();
-    const decoder = new TextDecoder();
-    const encoded = encoder.encode(str);
-    return encoded.byteLength > size
-      ? '...truncated...' + decoder.decode(encoded.slice(-size))
-      : str;
-  };
-
-  private invokeTsc = async (deployProps?: DeployProps) => {
-    if (!deployProps?.validateAppSources) {
-      return;
-    }
-    try {
-      await this.executeCommand(
-        [
-          'tsc',
-          '--showConfig',
-          '--project',
-          dirname(this.backendLocator.locate()),
-        ],
-        { redirectStdoutToStderr: true }, // TSC prints errors to stdout by default
-      );
-    } catch {
-      // If we cannot load ts config, turn off type checking
-      return;
-    }
-    try {
-      await this.executeCommand(
-        [
-          'tsc',
-          '--noEmit',
-          '--skipLibCheck',
-          // pointing the project arg to the amplify backend directory will use the tsconfig present in that directory
-          '--project',
-          dirname(this.backendLocator.locate()),
-        ],
-        { redirectStdoutToStderr: true }, // TSC prints errors to stdout by default
-      );
-    } catch (err) {
-      throw new AmplifyUserError<CDKDeploymentError>(
-        'SyntaxError',
-        {
-          message: 'TypeScript validation check failed.',
-          resolution:
-            'Fix the syntax and type errors in your backend definition.',
-        },
-        err instanceof Error ? err : undefined,
-      );
-    }
-  };
-
-  private populateCDKOutputFromStdout = async (
-    output: DeployResult | DestroyResult,
-    stdout: stream.Readable,
-  ) => {
-    const regexTotalTime = /✨ {2}Total time: (\d*\.*\d*)s.*/;
-    const regexSynthTime = /✨ {2}Synthesis time: (\d*\.*\d*)s/;
-    const reader = readline.createInterface(stdout);
-    for await (const line of reader) {
-      if (line.includes('✨')) {
-        // Good chance that it contains timing information
-        const totalTime = line.match(regexTotalTime);
-        if (totalTime && totalTime.length > 1 && !isNaN(+totalTime[1])) {
-          output.deploymentTimes.totalTime = +totalTime[1];
-        }
-        const synthTime = line.match(regexSynthTime);
-        if (synthTime && synthTime.length > 1 && !isNaN(+synthTime[1])) {
-          output.deploymentTimes.synthesisTime = +synthTime[1];
-        }
-      }
-    }
   };
 }
