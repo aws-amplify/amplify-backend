f79ef1c29a5a28bc95682f23637dda2b04d817d6
sobolk <5849952+sobolk@users.noreply.github.com>
2025-03-31 10:16:50 -0700

    PR feedback

--- .changeset/hungry-games-cover.md
+++ .changeset/hungry-games-cover.md
@@ -1,6 +1,10 @@
 ---
 '@aws-amplify/backend-function': minor
 '@aws-amplify/backend': minor
+'@aws-amplify/auth-construct': patch
+'@aws-amplify/ai-constructs': patch
+'@aws-amplify/backend-auth': patch
+'@aws-amplify/backend-data': patch
 ---
 
-Use Node 20 as default runtime
+Use Node 20 as default runtime in functions
--- packages/backend/src/engine/backend-secret/backend_secret_fetcher_provider_factory.ts
+++ packages/backend/src/engine/backend-secret/backend_secret_fetcher_provider_factory.ts
@@ -36,7 +36,7 @@ export class BackendSecretFetcherProviderFactory {
     }
 
     const secretLambda = new NodejsFunction(scope, `${providerId}Lambda`, {
-      runtime: LambdaRuntime.NODEJS_18_X,
+      runtime: LambdaRuntime.NODEJS_20_X,
       timeout: Duration.seconds(10),
       entry: backendSecretLambdaFilePath,
       handler: 'handler',
