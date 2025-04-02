a1ed2d6fe2a6236d834f63662f300bb9112cde52
sobolk <5849952+sobolk@users.noreply.github.com>
2025-04-02 09:43:32 -0700

    boop

--- packages/integration-tests/src/test-e2e/hosting.test.ts
+++ packages/integration-tests/src/test-e2e/hosting.test.ts
@@ -7,9 +7,12 @@ import {
   CreateDeploymentCommand,
   DeleteBranchCommand,
   JobStatus,
+  JobSummary,
   GetBranchCommand,
   GetJobCommand,
+  LimitExceededException,
   ListJobsCommand,
+  ListJobsCommandOutput,
   ListBranchesCommand,
   NotFoundException,
   StartDeploymentCommand,
@@ -19,6 +22,7 @@ import {
   UpdateBranchCommand,
 } from '@aws-sdk/client-amplify';
 import { e2eToolingClientConfig } from '../e2e_tooling_client_config.js';
+import { runWithRetry } from '../retry.js';
 
 const amplifyClient = new AmplifyClient({
   ...e2eToolingClientConfig,
@@ -38,98 +42,119 @@ void describe('hosting', () => {
     await ensureBranchIsConnected(appId, branchName);
   });
   void it('can deploy backend', async () => {
-    //await startDeployment(appId, branchName, commitSha);
+    const deploymentJob = await startOrGetDeploymentJob(
+      appId,
+      branchName,
+      commitSha,
+    );
+    if (deploymentJob.status !== JobStatus.SUCCEED) {
+      assert.ok(deploymentJob.jobId);
+      await waitForSuccessfulJobCompletion(
+        appId,
+        branchName,
+        deploymentJob.jobId,
+      );
+    }
+  });
+});
 
-    // await amplifyClient.send(
-    //   new CreateBranchCommand({
-    //     branchName: branchName,
-    //     appId: appId,
-    //   }),
-    // );
+const waitForSuccessfulJobCompletion = async (
+  appId: string,
+  branchName: string,
+  jobId: string,
+) => {
+  const timeoutMs = 10 * 60 * 1000; // 10 minutes;
+  const pollingInterval = 10 * 1000; // 10 seconds;
+  const startTime = Date.now();
+  while (Date.now() - startTime <= timeoutMs) {
+    const getJobResult = await amplifyClient.send(
+      new GetJobCommand({
+        appId,
+        branchName,
+        jobId,
+      }),
+    );
+    const jobSummary = getJobResult.job?.summary;
+    assert.ok(jobSummary);
+    console.log(`Job ${jobId} is in status ${jobSummary.status}`);
+    // Fail if job is in terminal non-successful state.
+    assert.ok(jobSummary.status !== 'FAILED');
+    assert.ok(jobSummary.status !== 'CANCELLED');
+    if (jobSummary.status === 'SUCCEED') {
+      return;
+    }
+    await new Promise((resolve) => setTimeout(resolve, pollingInterval));
+  }
+};
 
-    // await amplifyClient.send(
-    //   new UpdateAppCommand({
-    //     appId: appId,
-    //     buildSpec: 'foo',
-    //     enableBranchAutoBuild: false,
-    //     repository: 'https://github.com/aws-amplify/amplify-backend',
-    //     accessToken: process.env.AMPLIFY_BACKEND_TESTS_GITHUB_TOKEN,
-    //   }),
-    // );
+const startOrGetDeploymentJob = async (
+  appId: string,
+  branchName: string,
+  commitSha: string,
+): Promise<JobSummary> => {
+  return runWithRetry(
+    async () => {
+      const existingJobs = await listJobs(appId, branchName);
+      // Find existing job that's either successful or is in progress first that belongs to the commit.
+      const existingJob = existingJobs.find((item) => {
+        return (
+          item.commitId === commitSha &&
+          (item.status === JobStatus.SUCCEED ||
+            item.status === JobStatus.RUNNING ||
+            item.status === JobStatus.PENDING)
+        );
+      });
+      if (existingJob) {
+        console.log(
+          `Found existing job ${existingJob.jobId} for commit ${commitSha} in status ${existingJob.status}`,
+        );
+        return existingJob;
+      }
+      const startJobResult = await amplifyClient.send(
+        new StartJobCommand({
+          appId,
+          branchName,
+          commitId: commitSha,
+          jobType: 'RELEASE',
+        }),
+      );
+      assert.ok(startJobResult.jobSummary);
+      const newJob = startJobResult.jobSummary;
+      console.log(`Started a new job ${newJob.jobId} for commit ${commitSha}`);
+      return newJob;
+    },
+    (error) => {
+      // This happens if there's any deployment in progress for a branch.
+      // In that case retry couple of times with a delay.
+      // If this becomes un-effective we should create more apps and shard the traffic.
+      return error instanceof LimitExceededException;
+    },
+    5, // maxAttempts
+    60 * 1000, // delayMs
+  );
+};
 
-    const jobsList = await amplifyClient.send(
+const listJobs = async (
+  appId: string,
+  branchName: string,
+): Promise<Array<JobSummary>> => {
+  const jobs: Array<JobSummary> = [];
+  let listJobsResult: ListJobsCommandOutput | undefined;
+  do {
+    listJobsResult = await amplifyClient.send(
       new ListJobsCommand({
         appId,
         branchName,
         maxResults: 50,
+        nextToken: listJobsResult?.nextToken,
       }),
     );
-
-    //console.log(JSON.stringify(jobsList, null, 2));
-
-    for (const jobSummary of jobsList.jobSummaries ?? []) {
-      const job = await amplifyClient.send(
-        new GetJobCommand({
-          appId,
-          branchName,
-          jobId: jobSummary.jobId,
-        }),
-      );
-      //console.log(JSON.stringify(job, null, 2));
+    if (listJobsResult.jobSummaries) {
+      jobs.push(...listJobsResult.jobSummaries);
     }
+  } while (listJobsResult.nextToken);
 
-    // for (const jobSummary of jobsList.jobSummaries ?? []) {
-    //   if (jobSummary.status === JobStatus.CREATED)
-    //     await amplifyClient.send(
-    //       new StopJobCommand({
-    //         appId,
-    //         branchName,
-    //         jobId: jobSummary.jobId,
-    //       }),
-    //     );
-    // }
-
-    // await amplifyClient.send(
-    //   new StartJobCommand({
-    //     appId: appId,
-    //     branchName,
-    //     jobType: 'MANUAL',
-    //     commitId: '1dccf9e09e3aec18041b0071528feab67275ea07',
-    //   }),
-    // );
-
-    // const deployment = await amplifyClient.send(
-    //   new CreateDeploymentCommand({
-    //     appId: appId,
-    //     branchName,
-    //   }),
-    // );
-
-    // await amplifyClient.send(
-    //   new StartDeploymentCommand({
-    //     appId: appId,
-    //     branchName,
-    //     // jobId: deployment.jobId,
-    //     sourceUrl: `s3://${bucketName}/content/`,
-    //     sourceUrlType: 'BUCKET_PREFIX',
-    //   }),
-    // );
-  });
-});
-
-const startDeployment = async (
-  appId: string,
-  branchName: string,
-  commitSha: string,
-) => {
-  await amplifyClient.send(
-    new StartJobCommand({
-      appId,
-      branchName,
-      commitId: commitSha,
-      jobType: 'RELEASE',
-    }),
-  );
+  return jobs;
 };
 
 /**
@@ -147,8 +172,10 @@ const ensureBranchIsConnected = async (appId: string, branchName: string) => {
       }),
     );
     assert.ok(getBranchResult.branch);
+    console.log(`Branch ${branchName} is already connected`);
     const branch = getBranchResult.branch;
     if (getBranchResult.branch.enableAutoBuild) {
+      console.log(`Updating branch ${branchName} configuration`);
       await amplifyClient.send(
         new UpdateBranchCommand({
           appId,
@@ -161,6 +188,7 @@ const ensureBranchIsConnected = async (appId: string, branchName: string) => {
     if (!(e instanceof NotFoundException)) {
       throw e;
     }
+    console.log(`Connecting branch ${branchName}`);
     await amplifyClient.send(
       new CreateBranchCommand({
         appId,
