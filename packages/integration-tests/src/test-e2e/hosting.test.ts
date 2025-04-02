2f478be44564b52f37dd1a5e73a2f8d6018bd3d5
sobolk <5849952+sobolk@users.noreply.github.com>
2025-04-02 08:28:40 -0700

    boop

--- packages/integration-tests/src/test-e2e/hosting.test.ts
+++ packages/integration-tests/src/test-e2e/hosting.test.ts
@@ -5,14 +5,18 @@ import {
   CreateAppCommand,
   CreateBranchCommand,
   CreateDeploymentCommand,
+  DeleteBranchCommand,
   JobStatus,
   GetBranchCommand,
   GetJobCommand,
   ListJobsCommand,
+  ListBranchesCommand,
+  NotFoundException,
   StartDeploymentCommand,
   StartJobCommand,
   StopJobCommand,
   UpdateAppCommand,
+  UpdateBranchCommand,
 } from '@aws-sdk/client-amplify';
 import { e2eToolingClientConfig } from '../e2e_tooling_client_config.js';
 
@@ -22,18 +26,19 @@ const amplifyClient = new AmplifyClient({
 });
 
 void describe('hosting', () => {
+  let appId: string;
+  let branchName: string;
+  let commitSha: string;
+
+  before(async () => {
+    appId = 'd1z9ikmnr11ttr';
+    branchName = 'hosting-test';
+    commitSha = '55ba72b95241fcdf9f6e13e5c6614cfee57aaf4b';
+    await pruneStaleBranches(appId);
+    await ensureBranchIsConnected(appId, branchName);
+  });
   void it('can deploy backend', async () => {
-    const appId = 'd1z9ikmnr11ttr';
-    const branchName = 'hosting-test';
-
-    const branch = await amplifyClient.send(
-      new GetBranchCommand({
-        branchName: branchName,
-        appId: appId,
-      }),
-    );
-
-    console.log(JSON.stringify(branch, null, 2));
+    //await startDeployment(appId, branchName, commitSha);
 
     // await amplifyClient.send(
     //   new CreateBranchCommand({
@@ -111,3 +116,90 @@ void describe('hosting', () => {
     // );
   });
 });
+
+const startDeployment = async (
+  appId: string,
+  branchName: string,
+  commitSha: string,
+) => {
+  await amplifyClient.send(
+    new StartJobCommand({
+      appId,
+      branchName,
+      commitId: commitSha,
+      jobType: 'RELEASE',
+    }),
+  );
+};
+
+/**
+ * Ensures that branch is connected with desired settings.
+ *
+ * Desired settings:
+ * 1. Auto builds are turned off (we kick jobs explicitly from test).
+ */
+const ensureBranchIsConnected = async (appId: string, branchName: string) => {
+  try {
+    const getBranchResult = await amplifyClient.send(
+      new GetBranchCommand({
+        appId,
+        branchName,
+      }),
+    );
+    assert.ok(getBranchResult.branch);
+    const branch = getBranchResult.branch;
+    if (getBranchResult.branch.enableAutoBuild) {
+      await amplifyClient.send(
+        new UpdateBranchCommand({
+          appId,
+          ...branch,
+          enableAutoBuild: false,
+        }),
+      );
+    }
+  } catch (e) {
+    if (!(e instanceof NotFoundException)) {
+      throw e;
+    }
+    await amplifyClient.send(
+      new CreateBranchCommand({
+        appId,
+        branchName,
+
+        enableAutoBuild: false,
+      }),
+    );
+  }
+};
+
+/**
+ * Prune branches that haven't seen any activity for a period of time.
+ * Single app can have up to 50 branches. We're using a predefined app for hosting tests, as setup requires manual steps.
+ * Therefore, we need to collect garbage to avoid hitting the limit.
+ *
+ * Note: if we ever hit the limit due to test traffic volume, then we should consider sharding it across
+ * couple of predefined apps.
+ */
+const pruneStaleBranches = async (appId: string) => {
+  const staleDurationInMilliseconds = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+  const listBranchesResult = await amplifyClient.send(
+    new ListBranchesCommand({
+      appId,
+      maxResults: 50,
+    }),
+  );
+
+  for (const branch of listBranchesResult.branches ?? []) {
+    if (
+      branch.updateTime &&
+      Date.now() - branch.updateTime.getTime() > staleDurationInMilliseconds
+    ) {
+      await amplifyClient.send(
+        new DeleteBranchCommand({
+          appId,
+          branchName: branch.branchName,
+        }),
+      );
+    }
+  }
+};
