25059346f67050e540b85e9737095368f72051a6
Roshane Pascual <rotp@amazon.com>
2025-05-12 13:07:28 -0700

    send health checks telemetry to beta endpoint

--- packages/platform-core/src/telemetry/get_telemetry_url.test.ts
+++ packages/platform-core/src/telemetry/get_telemetry_url.test.ts
@@ -1,12 +1,24 @@
-import { afterEach, describe, test } from 'node:test';
+import { after, afterEach, before, describe, test } from 'node:test';
 import assert from 'node:assert';
 import url from 'node:url';
 
 void describe('getUrl', () => {
-  afterEach(() => {
+  const originalAmplifyTelemetryEndpoint =
+    process.env.AMPLIFY_BACKEND_TELEMETRY_TRACKING_ENDPOINT;
+  before(() => {
+    // unset AMPLIFY_BACKEND_TELEMETRY_TRACKING_ENDPOINT, we may be setting this variable in GitHub workflows
     delete process.env.AMPLIFY_BACKEND_TELEMETRY_TRACKING_ENDPOINT;
+  });
+
+  afterEach(() => {
     delete require.cache[require.resolve('./get_telemetry_url')];
   });
+
+  after(() => {
+    process.env.AMPLIFY_BACKEND_TELEMETRY_TRACKING_ENDPOINT =
+      originalAmplifyTelemetryEndpoint;
+  });
+
   void test('that prod URL is returned when the env for beta URL is not set', () => {
     // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
     const { getUrl } = require('./get_telemetry_url');
