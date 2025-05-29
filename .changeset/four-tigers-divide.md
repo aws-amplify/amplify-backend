3b4f18fab5c0bfdebab06b598246420e8f3d1b52
Roshane Pascual <rtpascual@users.noreply.github.com>
2025-05-29 13:05:22 -0700

    update error path processing to include current working directory for anonymization (#2832)
    * update error path processing to include current working directory for anonymization
    
    * add test logs
    
    * try this
    
    * try that
    
    * try this
    
    * hopefully fixes tests
    
    * regex...
    
    * add more logs
    
    * try that
    
    * hopefully last try
    
    * forgot some

---
+++ .changeset/four-tigers-divide.md
@@ -0,0 +1,5 @@
+---
+'@aws-amplify/platform-core': patch
+---
+
+Update error path processing to include current working directory for anonymization
--- packages/platform-core/src/usage-data/serializable_error.ts
+++ packages/platform-core/src/usage-data/serializable_error.ts
@@ -13,10 +13,10 @@ export class SerializableError {
 
   // breakdown of filePathRegex:
   // (file:/+)? -> matches optional file url prefix
-  // homedir() -> users home directory, replacing \ with /
+  // homedir()/process.cwd() -> users home directory or current working directory, replacing \ with /
   // [\\w.\\-_@\\\\/]+ -> matches nested directories and file name
   private filePathRegex = new RegExp(
-    `(file:/+)?${homedir().replaceAll('\\', '/')}[\\w.\\-_@\\\\/]+`,
+    `(file:/+)?(${homedir().replaceAll('\\', '/')}|${process.cwd().replaceAll('\\', '/')})[\\w.\\-_@\\\\/]+`,
     'g',
   );
   private stackTraceRegex =
