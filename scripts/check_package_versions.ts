74f3f806d90de53420549e4a89fe8020d935e383
Rozay Chen <rozayc@amazon.com>
2025-06-27 10:14:01 -0700

    Revert changes in check_package_version

--- scripts/check_package_versions.ts
+++ scripts/check_package_versions.ts
@@ -11,7 +11,6 @@ const packagePaths = await glob('./packages/*');
 const getExpectedMajorVersion = (packageName: string) => {
   switch (packageName) {
     case 'ampx':
-    case '@aws-amplify/storage-construct':
       return '0.';
     case '@aws-amplify/backend-deployer':
     case '@aws-amplify/cli-core':
