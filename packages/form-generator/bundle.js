9cd9e1d0c89556db5e96c788828310683b80cd5b
sobolk <5849952+sobolk@users.noreply.github.com>
2025-07-18 12:16:38 -0700

    fix build

--- packages/form-generator/bundle.js
+++ packages/form-generator/bundle.js
@@ -5,16 +5,16 @@ let pkg = JSON.parse(fs.readFileSync('package.json'));
 
 // Note: the @graphql-codegen/core dependency has been moved to root package.json
 // due to https://github.com/aws-amplify/amplify-backend/issues/2901
-let external = Object.keys(pkg.dependencies)
+let external = Object.keys(pkg.dependencies);
 
 await esbuild.build({
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    outfile: 'lib/bundled/index.js',
-    external,
-    logLevel: 'info',
-    platform: 'node',
-    target: 'node18',
-    format: 'esm',
-    sourcemap: true,
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  outfile: 'lib/bundled/index.js',
+  external,
+  logLevel: 'info',
+  platform: 'node',
+  target: 'node18',
+  format: 'esm',
+  sourcemap: true,
 });
