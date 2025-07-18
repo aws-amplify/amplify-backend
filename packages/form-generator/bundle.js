bbc6ecddb9a3c7162eadf8e2fc47213e8a4724fd
sobolk <5849952+sobolk@users.noreply.github.com>
2025-07-18 12:37:42 -0700

    and this

--- packages/form-generator/bundle.js
+++ packages/form-generator/bundle.js
@@ -8,7 +8,7 @@ let pkg = JSON.parse(fs.readFileSync('package.json'));
 // (minimal viable solution until we figure out global bundling strategy).
 // 1. @graphql-codegen/core dependency was moved to root package.json.
 //    This ensures that package managers don't attempt to install it.
-//    E.g. Yarn 4.x attempts to install any dependency, prod, dev, bundled not respectig NPM spec.
+//    E.g. Yarn 4.x attempts to install any dependency, prod, dev, bundled not respecting NPM spec.
 // 2. All other dependencies (that are not problematic at this time) are listed in 'pkg.dependencies' and are exluded from bundling.
 //    An attempt to bundle them results in 35MB bundle size and runtime errors like 'Dynamic require of \"buffer\" is not supported'.
 //   These dependencies will be installed by package managers in custmer project.
