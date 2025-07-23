432ffbf027bfedae7644f34a09a42cac16142d97
manavgurnani21 <mgurnani@amazon.com>
2025-07-23 10:05:49 -0700

    split APi definition and outputs aspects defined

--- packages/backend-geo/src/geo_access_orchestrator.ts
+++ packages/backend-geo/src/geo_access_orchestrator.ts
@@ -1,12 +1,17 @@
-import { ConstructFactoryGetInstanceProps } from '@aws-amplify/plugin-types';
+import {
+  ConstructFactoryGetInstanceProps,
+  SsmEnvironmentEntry,
+} from '@aws-amplify/plugin-types';
 import {
   GeoAccessBuilder,
   GeoAccessGenerator,
-  GeoResourceType,
+  resourceActionRecord,
 } from './types.js';
 import { roleAccessBuilder as _roleAccessBuilder } from './access_builder.js';
 import { GeoAccessPolicyFactory } from './geo_access_policy_factory.js';
 import { AmplifyUserError } from '@aws-amplify/platform-core';
+import { Policy } from 'aws-cdk-lib/aws-iam';
+import { Stack } from 'aws-cdk-lib';
 
 // this file is responsible for implementing the following:
 // 1. access orchestrator for geo
@@ -16,30 +21,36 @@ import { AmplifyUserError } from '@aws-amplify/platform-core';
  * Configures access permissions to associate them with roles.
  */
 export class GeoAccessOrchestrator {
+  private resourceStack: Stack;
+  private policies: Policy[] = [];
   /**
    * Constructs an instance of GeoAccessOrchestrator
    * @param geoAccessGenerator - access permissions defined by user for the resource
    * @param getInstanceProps - instance properties of a specific construct factory
-   * @param geoPolicyFactory - instance of GeoAccessPolicyFactory to generate policyStatements
-   * @param roleAccessBuilder - permission reader and processor
+   * @param geoStack - instance of GeoAccessPolicyFactory to generate policyStatements
+   * @param ssmEnvironmentEntries - permission reader and processor
+   * @param geoPolicyFactory -
+   * @param roleAccessBuilder -
    */
   constructor(
     private readonly geoAccessGenerator: GeoAccessGenerator,
     private readonly getInstanceProps: ConstructFactoryGetInstanceProps,
+    private readonly geoStack: Stack,
+    private readonly ssmEnvironmentEntries: SsmEnvironmentEntry[],
     private readonly geoPolicyFactory: GeoAccessPolicyFactory = new GeoAccessPolicyFactory(),
     private readonly roleAccessBuilder: GeoAccessBuilder = _roleAccessBuilder,
-  ) {}
+  ) {
+    this.resourceStack = geoStack;
+  }
 
   /**
    * Orchestrates the process of translating the customer-provided storage access rules into IAM policies and attaching those policies to the appropriate roles.
    *
-   * The high level steps are:
-   * 1. Invokes the geoAccessGenerator to produce a storageAccessDefinition
-   * 3. Organizes the storageAccessDefinition into internally managed maps to facilitate translation into allow / deny rules on IAM policies
-   * 4. Invokes the policy generator to produce a policy with appropriate allow / deny rules
-   * 5. Invokes the resourceAccessAcceptors for each entry in the geoAccessDefinition to accept the corresponding IAM policy
    */
-  orchestrateGeoAccess = (resourceArn: string) => {
+  orchestrateGeoAccess = (
+    resourceArn: string,
+    resourceIdentifier: string,
+  ): Policy[] => {
     // getting access definitions from allow calls
     const geoAccessDefinitions = this.geoAccessGenerator(
       this.roleAccessBuilder,
@@ -62,17 +73,38 @@ export class GeoAccessOrchestrator {
         },
       );
 
-      definition.userRoles.forEach((user) => {
-        this.geoPolicyFactory.attachPolicy(
-          // attaching policy statement to principal policy of each user Role provided within access definition
-          user(this.getInstanceProps),
-          this.geoPolicyFactory.createPolicyStatement(
-            definition.actions,
-            resourceArn,
-          ), // creating a policy statement for all actions provided within definition
+      // checking for valid actions for resource type
+      definition.actions.forEach((action) => {
+        if (!resourceActionRecord[resourceIdentifier].includes(action)) {
+          throw new AmplifyUserError('ActionNotFoundError', {
+            message: `Desired access action not found for the specific ${resourceIdentifier} resource.`,
+            resolution: `Please refer to specific ${resourceIdentifier} access actions for more information.`,
+          });
+        }
+      });
+
+      const roleTokens = Array.from(uniqueRoleTokenSet);
+
+      let roleIndex: number = 0; // need respective roleToken for policy generation
+      definition.getAccessAcceptors.forEach((acceptor) => {
+        // for each acceptor within auth, guest, or user groups
+
+        const policy: Policy = this.geoPolicyFactory.createPolicy(
+          definition.actions,
+          resourceArn,
+          roleTokens[roleIndex],
+          this.resourceStack,
         );
+        acceptor(this.getInstanceProps).acceptResourceAccess(
+          policy,
+          this.ssmEnvironmentEntries,
+        );
+        this.policies.push(policy);
+        roleIndex += 1;
       });
     });
+
+    return this.policies;
   };
 }
 
@@ -81,10 +113,16 @@ export class GeoAccessOrchestrator {
  * Instance Manager for Geo Access Orchestration
  */
 export class GeoAccessOrchestratorFactory {
-  private readonly resourceType: GeoResourceType;
-
   getInstance = (
     geoAccessGenerator: GeoAccessGenerator,
     getInstanceProps: ConstructFactoryGetInstanceProps,
-  ) => new GeoAccessOrchestrator(geoAccessGenerator, getInstanceProps);
+    stack: Stack,
+    ssmEnvironmentEntries: SsmEnvironmentEntry[],
+  ) =>
+    new GeoAccessOrchestrator(
+      geoAccessGenerator,
+      getInstanceProps,
+      stack,
+      ssmEnvironmentEntries,
+    );
 }
