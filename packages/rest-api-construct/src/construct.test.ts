cbe7717296fac62a1afa7820f55d4cbbee575311
Rory Hackney <roryhackney@gmail.com>
2025-07-28 12:28:01 -0700

    add unit test for creating new lambda from code param

--- packages/rest-api-construct/src/construct.test.ts
+++ packages/rest-api-construct/src/construct.test.ts
@@ -4,8 +4,8 @@ import { App, Stack } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-void describe('RestApiConstruct', () => {
-  void it('loads a function if the folder is specified', () => {
+void describe('RestApiConstruct Lambda Handling', () => {
+  void it('loads an existing local lambda function if the directory is specified', () => {
     const app = new App();
     const stack = new Stack(app);
     new RestApiConstruct(stack, 'RestApiLambdaTest', {
@@ -17,6 +17,30 @@ void describe('RestApiConstruct', () => {
           lambdaEntry: {
             runtime: lambda.Runtime.NODEJS_18_X,
             source: { path: 'packages/rest-api-construct/lib/test-assets' },
+          },
+        },
+      ],
+    });
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Handler: 'index.handler',
+    });
+  });
+
+  void it('creates a new lambda function if the code is specified', () => {
+    const app = new App();
+    const stack = new Stack(app);
+    new RestApiConstruct(stack, 'RestApiNewLambdaTest', {
+      apiName: 'RestApiNewLambda',
+      apiProps: [
+        {
+          path: 'items',
+          routes: ['GET'],
+          lambdaEntry: {
+            runtime: lambda.Runtime.NODEJS_22_X,
+            source: {
+              code: "export const handler = () => {return 'Hello World! This is a new lambda function.';};",
+            },
           },
         },
       ],
