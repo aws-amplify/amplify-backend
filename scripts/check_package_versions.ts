3ca21c9f9bc0cc6f79ed8e59fdd40f9e27c280bd
manavgurnani21 <mgurnani@amazon.com>
2025-07-28 15:51:57 -0700

    small fixes

--- scripts/check_package_versions.ts
+++ scripts/check_package_versions.ts
@@ -14,6 +14,7 @@ const getExpectedMajorVersion = (packageName: string) => {
       return '0.';
     case '@aws-amplify/backend-deployer':
     case '@aws-amplify/cli-core':
+    case '@aws-amplify/backend-geo':
     case '@aws-amplify/sandbox':
       return '2.';
     default:
