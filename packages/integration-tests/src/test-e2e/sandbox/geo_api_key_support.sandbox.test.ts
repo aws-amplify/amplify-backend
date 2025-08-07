dbe662de91d1e92462974aefe19abc9841d296d7
manavgurnani21 <mgurnani@amazon.com>
2025-08-07 13:58:58 -0700

    following naming convention of integration test

--- packages/integration-tests/src/test-e2e/sandbox/geo_api_key_support.sandbox.test.ts
+++ packages/integration-tests/src/test-e2e/sandbox/geo_api_key_support.sandbox.test.ts
@@ -1,4 +1,4 @@
 import { defineSandboxTest } from './sandbox.test.template.js';
-import { GeoAPIKeySupportTestProjectCreator } from '../../test-project-setup/geo_api_support_testing.js';
+import { GeoAPIKeySupportTestProjectCreator } from '../../test-project-setup/geo_api_key_support_testing.js';
 
 defineSandboxTest(new GeoAPIKeySupportTestProjectCreator());
