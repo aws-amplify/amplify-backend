aba0dc6a4323f1e664c4372cc4f44391513d595e
esslam-ashour <eashour@amazon.com>
2025-08-14 14:56:15 -0700

    add ai-model/runtime to API extractor exports

--- packages/ai-constructs/src/index.internal.ts
+++ packages/ai-constructs/src/index.internal.ts
@@ -5,6 +5,8 @@ import * as __export__conversation from './conversation/index.js';
 import * as __export__conversation__runtime from './conversation/runtime/index.js';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import * as __export__ai_model from './ai-model/index.js';
+// eslint-disable-next-line @typescript-eslint/naming-convention
+import * as __export__ai_model__runtime from './ai-model/runtime/index.js';
 
 /*
  Api-extractor does not ([yet](https://github.com/microsoft/rushstack/issues/1596)) support multiple package entry points
@@ -16,4 +18,5 @@ export {
   __export__conversation,
   __export__conversation__runtime,
   __export__ai_model,
+  __export__ai_model__runtime,
 };
