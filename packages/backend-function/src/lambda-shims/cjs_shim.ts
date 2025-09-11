5e440e037a1293a53e393ac25938c168eda10f0e
Vieltojarvi <lvielto@amazon.com>
2025-09-11 14:03:37 -0400

    fix tests

--- packages/backend-function/src/lambda-shims/cjs_shim.ts
+++ packages/backend-function/src/lambda-shims/cjs_shim.ts
@@ -1,6 +1,6 @@
 import { createRequire } from 'node:module';
 import path from 'node:path';
 import url from 'node:url';
-global.require = createRequire(import.meta.url);
-global.__filename = url.fileURLToPath(import.meta.url);
-global.__dirname = path.dirname(__filename);
+globalThis.require = createRequire(import.meta.url);
+globalThis.__filename = url.fileURLToPath(import.meta.url);
+globalThis.__dirname = path.dirname(__filename);
